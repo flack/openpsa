ef134df236efc17020677e31d24be7e88cbe567b
Andreas Flack <flack@contentcontrol-berlin.de>
2010-11-07 15:48:01 +0100

    Add another missing dependency

---
+++ lib/de/bitfolge/feedcreator/midcom/interfaces.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * RSS and Atom feed generator library
+ * @link http://www.bitfolge.de/rsscreator-en.html
+ * @license LGPL license
+ * @package de.bitfolge.feedcreator
+ */
+
+/** 
+ * Startup loads main class, which is used for all operations.
+ * 
+ * @package de.bitfolge.feedcreator
+ */
+class de_bitfolge_feedcreator_interface extends midcom_baseclasses_components_interface
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+        
+        $this->_component = 'de.bitfolge.feedcreator';
+        $this->_autoload_files = Array('feedcreator.php');
+    }
+    
+    function _on_initialize()
+    {
+        return true;
+    }
+}
+?>
