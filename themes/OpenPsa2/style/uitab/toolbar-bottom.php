8d4d66fdec4e441f624f7a9a3b86c20359d883f5
Andreas Flack <flack@contentcontrol-berlin.de>
2010-11-07 22:23:02 +0100

    Fix notice

--- themes/OpenPsa2/style/uitab/toolbar-bottom.php
+++ themes/OpenPsa2/style/uitab/toolbar-bottom.php
@@ -3,10 +3,12 @@ $context_id = $_MIDCOM->get_current_context();
 $back_button_name = $_MIDCOM->i18n->get_string("back" , "midcom");
 //remove the back-button
 //TODO: any better way to identify the back button ?
+
 foreach($_MIDCOM->toolbars->_toolbars[$context_id][MIDCOM_TOOLBAR_VIEW]->items as $key => $item)
 {
    if (   $item[1] == $back_button_name
-       || strpos($_SERVER['HTTP_REFERER'], $item[0]) !== false)
+       || (    array_key_exists('HTTP_REFERER', $_SERVER) 
+            && strpos($_SERVER['HTTP_REFERER'], $item[0]) !== false))
    {
        unset($_MIDCOM->toolbars->_toolbars[$context_id][MIDCOM_TOOLBAR_VIEW]->items[$key]);
    }
