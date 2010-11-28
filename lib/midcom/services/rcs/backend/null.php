2bb2fc296a2bf6911607ec3469b9433f6da07b35
Andreas Flack <flack@contentcontrol-berlin.de>
2010-11-28 22:04:40 +0100

    Move null implementation to its own file

---
+++ lib/midcom/services/rcs/backend/null.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author tarjei huse
+ * @package midcom.services.rcs
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+/**
+ * @package midcom.services.rcs
+ */
+class midcom_services_rcs_backend_null extends midcom_services_rcs_backend
+{
+    function update(&$object, $comment)
+    {
+        return true;
+    }
+}
+?>
