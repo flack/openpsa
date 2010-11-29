4fdff24922ac4b8bb427e9eee6d3bb024a94e4ce
Andreas Flack <flack@contentcontrol-berlin.de>
2010-11-29 18:00:46 +0100

    Remove unused class

--- lib/midgard/admin/asgard/handler/object/rcs.php
+++ lib/midgard/admin/asgard/handler/object/rcs.php
@@ -52,7 +52,7 @@ class midgard_admin_asgard_handler_object_rcs extends midcom_baseclasses_compone
      *
      * @var midcom_db_object
      */
-    private $_object = null;
+    public $_object = null;
 
     function __construct()
     {
