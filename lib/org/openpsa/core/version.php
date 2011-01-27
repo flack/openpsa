29fd6917c8c01e117466ded9bbf16d1dd95f32a9
Andreas Flack <flack@contentcontrol-berlin.de>
2011-01-27 22:40:02 +0100

    Change version numbers to reduce confusion with midcom/ragnaroek

--- lib/org/openpsa/core/version.php
+++ lib/org/openpsa/core/version.php
@@ -11,21 +11,21 @@
  */
 //These two constants are on purpose in here
 define('ORG_OPENPSA_CORE_VERSION_NUMBER', $_MIDCOM->componentloader->get_component_version('org.openpsa.core'));
-define('ORG_OPENPSA_CORE_VERSION_NAME', 'Leaner and Meaner');
+define('ORG_OPENPSA_CORE_VERSION_NAME', 'Off the Grid');
 
 /**
  * Returns current version of OpenPSA. Three different modes are supported:
  *  version number (version name)
  *  version number
  *  version name
- * 
+ *
  * @package org.openpsa.core
  */
 class org_openpsa_core_version
 {
     /**
      * Returns version number
-     *  
+     *
      * @return string OpenPSA version string
      */
     function get_version_number()
@@ -35,7 +35,7 @@ class org_openpsa_core_version
 
     /**
      * Returns version name
-     *  
+     *
      * @return string OpenPSA version string
      */
     function get_version_name()
@@ -45,7 +45,7 @@ class org_openpsa_core_version
 
     /**
      * Returns version number and name
-     *  
+     *
      * @return string OpenPSA version string
      */
     function get_version_both()
