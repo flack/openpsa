b950046610d02db2a0ad195b9ed48d436bd5bbe3
Andreas Flack <flack@contentcontrol-berlin.de>
2011-02-14 00:25:08 +0100

    Add test for invoice item recalculation

--- test/org/openpsa/invoices/invoiceTest.php
+++ test/org/openpsa/invoices/invoiceTest.php
@@ -15,7 +15,7 @@ require_once('rootfile.php');
  */
 class org_openpsa_invoices_invoiceTest extends openpsa_testcase
 {
-    public function testNumbering()
+    public function testCRUD()
     {
         $_MIDCOM->auth->request_sudo('org.openpsa.invoices');
         $invoice = new org_openpsa_invoices_invoice_dba();
@@ -31,6 +31,85 @@ class org_openpsa_invoices_invoiceTest extends openpsa_testcase
         $this->assertEquals($next_number, $invoice->generate_invoice_number());
 
         $_MIDCOM->auth->drop_sudo();
+    }
+
+    /**
+     * @depends testCRUD
+     */
+    public function testRecalculate_invoice_items()
+    {
+        $invoice = $this->create_object('org_openpsa_invoices_invoice_dba');
+
+        $salesproject = $this->create_object('org_openpsa_sales_salesproject_dba');
+        $deliverable_attributes = array
+        (
+            'salesproject' => $salesproject->id,
+            'pricePerUnit' => 10
+        );
+        $deliverable = $this->create_object('org_openpsa_sales_salesproject_deliverable_dba', $deliverable_attributes);
+
+        $task_attributes = array
+        (
+            'agreement' => $deliverable->id
+        );
+        $task1 = $this->create_object('org_openpsa_projects_task_dba', $task_attributes);
+        $task2 = $this->create_object('org_openpsa_projects_task_dba', $task_attributes);
+
+        $report_attributes = array
+        (
+            'invoice' => $invoice->id,
+            'hours' => 1,
+            'invoiceable' => true,
+            'task' => $task1->id
+        );
+
+        $report1 = $this->create_object('org_openpsa_projects_hour_report_dba', $report_attributes);
+        $report_attributes['task'] = $task2->id;
+        $report2 = $this->create_object('org_openpsa_projects_hour_report_dba', $report_attributes);
+
+        $_MIDCOM->auth->request_sudo('org.openpsa.invoices');
+        $invoice->_recalculate_invoice_items();
+        $invoice->refresh();
+        $this->assertEquals(20, $invoice->sum);
+        $this->assertEquals(2, $this->_count_invoice_items($invoice->id));
+
+        //Rinse and repeat to see if the result stays stable
+        $invoice->_recalculate_invoice_items();
+        $invoice->refresh();
+        $this->assertEquals(20, $invoice->sum);
+        $this->assertEquals(2, $this->_count_invoice_items($invoice->id));
+
+        $report1->hours = 2;
+        $report1->update();
+        $invoice->_recalculate_invoice_items();
+        $invoice->refresh();
+        $this->assertEquals(30, $invoice->sum);
+        $this->assertEquals(2, $this->_count_invoice_items($invoice->id));
+
+        $report2->invoiceable = false;
+        $report2->update();
+
+        $invoice->_recalculate_invoice_items(array($task1->id));
+        $invoice->refresh();
+        $this->assertEquals(30, $invoice->sum);
+        $this->assertEquals(2, $this->_count_invoice_items($invoice->id));
+
+        $invoice->_recalculate_invoice_items(array($task2->id));
+        $invoice->refresh();
+        $this->assertEquals(20, $invoice->sum);
+        $this->assertEquals(2, $this->_count_invoice_items($invoice->id));
+
+        self::delete_linked_objects('org_openpsa_invoices_invoice_item_dba', 'task', $task1->id);
+        self::delete_linked_objects('org_openpsa_invoices_invoice_item_dba', 'task', $task2->id);
+
+        $_MIDCOM->auth->drop_sudo();
+    }
+
+    private function _count_invoice_items($invoice_id)
+    {
+        $mc = org_openpsa_invoices_invoice_item_dba::new_collector('invoice', $invoice_id);
+        $mc->execute();
+        return sizeof($mc->list_keys());
     }
 }
 ?>
