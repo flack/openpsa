0f9a734fdd3262a309aad522ab0fff5610ada654
Andreas Flack <flack@contentcontrol-berlin.de>
2011-02-16 20:14:49 +0100

    Add rudimentary test for billing data

---
+++ test/org/openpsa/invoices/billing/dataTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+require_once('rootfile.php');
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_invoices_invoice_billing_dataTest extends openpsa_testcase
+{
+    protected static $_contact;
+
+    public static function setUpBeforeClass()
+    {
+        $attributes = array
+        (
+           'street' => 'TEST STREET'
+        );
+        self::$_contact = self::create_class_object('org_openpsa_contacts_person_dba');
+    }
+
+    public function testCRUD()
+    {
+        $_MIDCOM->auth->request_sudo('org.openpsa.invoices');
+        $data = new org_openpsa_invoices_billing_data_dba();
+        $data->linkGuid = self::$_contact->guid;
+        $data->useContactAddress = true;
+        $stat = $data->create();
+        $this->assertTrue($stat);
+
+        $parent = $data->get_parent();
+        $this->assertEquals($parent->guid, self::$_contact->guid);
+
+        self::$_contact->refresh();
+        $this->assertEquals(self::$_contact->street, $data->street);
+
+        $data->vat = 12;
+        $data->due = 12;
+        $stat = $data->update();
+        $this->assertTrue($stat);
+
+        self::$_contact->refresh();
+        $invoice = new org_openpsa_invoices_invoice_dba();
+        $invoice->customerContact = self::$_contact->id;
+        $invoice_data = $invoice->get_billing_data();
+
+        $this->assertEquals($data->guid, $invoice_data->guid);
+        $this->assertEquals($data->vat, $invoice->get_default_vat());
+        $this->assertEquals($data->due, $invoice->get_default_due());
+
+        $stat = $data->delete();
+        $this->assertTrue($stat);
+
+        $_MIDCOM->auth->drop_sudo();
+    }
+}
+?>
