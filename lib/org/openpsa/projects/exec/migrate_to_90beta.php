7d674978db44c9cc3331c91837ef300ab0873a85
Andreas Flack <flack@contentcontrol-berlin.de>
2011-04-01 19:06:56 +0200

    Also convert standalone salesprojects

--- lib/org/openpsa/projects/exec/migrate_to_90beta.php
+++ lib/org/openpsa/projects/exec/migrate_to_90beta.php
@@ -21,5 +21,23 @@ foreach ($projects as $project)
     $runner = new project_converter($project);
     $runner->execute();
 }
+
+/*
+ * Salesprojects that do not have associated projects need to be processed separately. For the sake of
+ * simplicity, we create a dummy project and feed them through the normal converter
+ */
+$qb = new midgard_query_builder('org_openpsa_salesproject_old');
+$salesprojects = $qb->execute();
+
+foreach ($salesprojects as $salesproject)
+{
+    $project = new org_openpsa_task_old();
+    $project->orgOpenpsaObtype = 6000;
+    $project->create();
+    $runner = new project_converter($project);
+    $runner->set_salesproject($salesproject);
+    $runner->execute();
+}
+
 echo "</pre>\n";
 ?>
