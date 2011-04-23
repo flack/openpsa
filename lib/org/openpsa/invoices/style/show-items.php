8d94f140ed50c378933c0b7bf196a8198c939890
Andreas Flack <flack@contentcontrol-berlin.de>
2011-04-23 22:38:21 +0200

    style tweaks

--- lib/org/openpsa/invoices/style/show-items.php
+++ lib/org/openpsa/invoices/style/show-items.php
@@ -5,13 +5,13 @@ $grid = $data['grid'];
 $grid->set_option('editurl', $prefix . 'invoice/itemedit/' . $data['invoice']->guid . '/');
 $grid->set_option('pager', '#p_' . $grid->get_identifier());
 
-$grid->set_column('deliverable', $_MIDCOM->i18n->get_string('agreement', 'org.openpsa.projects'));
-$grid->set_column('task', $_MIDCOM->i18n->get_string('task', 'org.openpsa.projects'));
+$grid->set_column('deliverable', $_MIDCOM->i18n->get_string('agreement', 'org.openpsa.projects'), 'width: 80');
+$grid->set_column('task', $_MIDCOM->i18n->get_string('task', 'org.openpsa.projects'), 'width: 80');
 $grid->set_column('description', $_MIDCOM->i18n->get_string('description', 'midcom'), 'editable: true, edittype: "textarea"');
 $grid->set_column('price', $data['l10n']->get('price'), 'align: "right", width: 40, formatter: "number", editable: true');
-$grid->set_column('quantity', $data['l10n']->get('quantity'), 'align: "right", width: 40, formatter: "number", editable: true');
+$grid->set_column('quantity', $data['l10n']->get('quantity'), 'align: "right", width: 30, formatter: "number", editable: true');
 $grid->set_column('sum', $data['l10n']->get('sum'), 'align: "right", width: 60, formatter: "number", summaryType: "sum"');
-$grid->set_column('actions', '',  'width: 60');
+$grid->set_column('actions', '',  'width: 60, fixed: true');
 ?>
 
 <div class="full-width">
@@ -24,8 +24,8 @@ org_openpsa_grid_editable.enable_inline("<?php echo $grid->get_identifier(); ?>"
     aftersavefunc: function (rowid, response)
     {
         var grid = jQuery("#<?php echo $grid->get_identifier(); ?>"),
-            price = grid.jqGrid('getCell', rowid, 2),
-            quantity = grid.jqGrid('getCell', rowid, 3)
+            price = grid.jqGrid('getCell', rowid, 4),
+            quantity = grid.jqGrid('getCell', rowid, 5)
             rows = grid.jqGrid('getRowData'),
             total = 0
             i = 0;
@@ -34,7 +34,7 @@ org_openpsa_grid_editable.enable_inline("<?php echo $grid->get_identifier(); ?>"
 
         for (i = 0; i < rows.length; i++)
         {
-            total += parseFloat(grid.jqGrid('getCell', rowid, 4));
+            total += parseFloat(grid.jqGrid('getCell', rowid, 6));
         }
         grid.jqGrid("footerData", "set", {sum: total});
     }
