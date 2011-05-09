36e753a6c9b273365b13e290ec2301fc601110c6
Andreas Flack <flack@contentcontrol-berlin.de>
2011-05-09 10:40:49 +0200

    Remove some duplicated code & implement visibility checks for collector

--- lib/midcom/core/collector.php
+++ lib/midcom/core/collector.php
@@ -22,89 +22,17 @@
  * If you have to do create the instance manually however, do not forget to call the
  * {@link initialize()} function after construction, or the creation callbacks will fail.
  *
- * <i>Developer's Note:</i>
- *
- * Due to the limitations of the Zend engine this class does not extend the
- * QueryBuilder but proxy to it.
- *
  * @package midcom
  * @todo Refactor the class to promote code reuse in the execution handlers.
  */
-class midcom_core_collector
+class midcom_core_collector extends midcom_core_query
 {
     /**
-     * This private helper holds the type that the application expects to retrieve
-     * from this instance.
-     *
-     * @var string
-     */
-    private $_real_class;
-
-    /**
-     * The collector instance that is internally used.
-     *
-     * @var midgard_collector
-     */
-    private $_mc;
-
-    /**
-     * The number of records to return to the client at most.
-     *
-     * @var int
-     */
-    private $_limit = 0;
-
-    /**
-     * The offset of the first record the client wants to have available.
-     *
-     * @var int
-     */
-    private $_offset = 0;
-
-    /**
-     * This is an internal count which is incremented by one each time a constraint is added.
-     * It is used to emit a warning if no constraints have been added to the QB during execution.
-     *
-     * @var int
-     */
-    private $_constraint_count = 0;
-
-    /**
-     * The number of records found by the last execute() run. This is -1 as long as no
-     * query has been executed. This member is read-only.
-     *
-     * @var int
-     */
-    var $count = -1;
-
-    /**
-     * The number of objects for which access was denied.
-     *
-     * This is especially useful for reimplementations of functions like mgd_get_article_by_name
-     * which must use the QB in the first place.
-     *
-     * @var int
-     */
-    var $denied = 0;
-
-    /**
-     * Set this element to true to hide all items which are currently invisible according
-     * to the approval/scheduling settings made using Metadata. This must be set before executing
-     * the query.
-     *
-     * NOTE: Checks not implemented yet
-     *
-     * Be aware, that this setting will currently not use the QB to filter the objects accordingly,
-     * since there is no way yet to filter against parameters. This will mean some performance
-     * impact.
-     *
-     */
-    var $hide_invisible = true;
-
-    /**
      * Keep track if $this->execute has been called
+     *
+     * @var boolean
      */
-    var $_executed = false;
+    private $_executed = false;
 
     /**
      * This private helper holds the user id for ACL checks. This is set when executing
@@ -149,10 +77,10 @@ class midcom_core_collector
             $_class_mapping_cache[$classname] = $mgdschemaclass;
         }
 
-        $this->_mc = new midgard_collector($mgdschemaclass, $domain, $value);
+        $this->_query = new midgard_collector($mgdschemaclass, $domain, $value);
 
         // MidCOM's collector always uses the GUID as the key for ACL purposes
-        $this->_mc->set_key_property('guid');
+        $this->_query->set_key_property('guid');
     }
 
     /**
@@ -204,24 +132,25 @@ class midcom_core_collector
         // Add the limit / offsets
         if ($this->_limit)
         {
-            $this->_mc->set_limit($this->_limit);
+            $this->_query->set_limit($this->_limit);
         }
         if ($this->_offset)
         {
-            $this->_mc->set_offset($this->_offset);
+            $this->_query->set_offset($this->_offset);
         }
 
-        return $this->_mc->execute();
+        $this->_add_visibility_checks();
+        
+        return $this->_query->execute();
     }
 
     /**
      * Resets some internal variables for re-execute
      */
-    private function _reset()
+    protected function _reset()
     {
         $this->_executed = false;
-        $this->count = -1;
-        $this->denied = 0;
+        parent::_reset();
     }
 
     /**
@@ -257,7 +186,7 @@ class midcom_core_collector
     private function _list_keys_and_check_privileges()
     {
         $this->_real_execute();
-        $result = $this->_mc->list_keys();
+        $result = $this->_query->list_keys();
         if (!is_array($result))
         {
             return $result;
@@ -290,7 +219,7 @@ class midcom_core_collector
     }
 
     /**
-     * implements midgard_collector::list_keys with ACL and visibility checking
+     * implements midgard_collector::list_keys with ACL checking
      */
     function list_keys()
     {
@@ -338,7 +267,7 @@ class midcom_core_collector
             midcom_connection::set_error(MGD_ERR_ACCESS_DENIED);
             return false;
         }
-        return $this->_mc->get_subkey($key, $property);
+        return $this->_query->get_subkey($key, $property);
     }
 
     function get($key)
@@ -349,130 +278,12 @@ class midcom_core_collector
             midcom_connection::set_error(MGD_ERR_ACCESS_DENIED);
             return false;
         }
-        return $this->_mc->get($key);
+        return $this->_query->get($key);
     }
 
     function destroy()
     {
-        return $this->_mc->destroy();
-    }
-
-    /**
-     * Add a constraint to the collector.
-     *
-     * @param string $field The name of the MgdSchema property to query against.
-     * @param string $operator The operator to use for the constraint, currently supported are
-     *     &lt;, &lt;=, =, &lt;&gt;, &gt;=, &gt;, LIKE. LIKE uses the percent sign ('%') as a
-     *     wildcard character.
-     * @param mixed $value The value to compare against. It should be of the same type then the
-     *     queried property.
-     * @return boolean Indicating success.
-     */
-    function add_constraint($field, $operator, $value)
-    {
-        $this->_reset();
-        // Add check against null values, Core MC is too stupid to get this right.
-        if ($value === null)
-        {
-            debug_add("Collector: Cannot add constraint on field '{$field}' with null value.", MIDCOM_LOG_WARN);
-            return false;
-        }
-
-        if (! $this->_mc->add_constraint($field, $operator, $value))
-        {
-            debug_add("Failed to execute add_constraint.", MIDCOM_LOG_ERROR);
-            debug_add("Class = '{$this->_real_class}, Field = '{$field}', Operator = '{$operator}'");
-            debug_print_r('Value:', $value);
-
-            return false;
-        }
-        $this->_constraint_count++;
-        return true;
-    }
-
-    /**
-     * Add an ordering constraint to the collector.
-     *
-     * @param string $field The name of the MgdSchema property to query against.
-     * @param string $ordering One of 'ASC' or 'DESC' indicating ascending or descending
-     *     ordering. The default is 'ASC'.
-     * @return boolean Indicating success.
-     */
-    function add_order($field, $ordering = null)
-    {
-        /**
-         * NOTE: So see also querybuilder.php when making changes here
-         */
-        if ($ordering === null)
-        {
-            $result = $this->_mc->add_order($field);
-        }
-        else
-        {
-            $result = $this->_mc->add_order($field, $ordering);
-        }
-
-        if (! $result)
-        {
-            debug_add("Failed to execute add_order: Unknown or invalid column '{$field}'.", MIDCOM_LOG_ERROR);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Creates a new logical group within the query. They are set in parentheses in the final
-     * SQL and will thus be evaluated with precedence over the normal out-of-group constraints.
-     *
-     * While the call lets you decide whether all constraints within the group are AND'ed or OR'ed,
-     * only OR constraints make logically sense in this context, which is why this proxy function
-     * sets 'OR' as the default operator.
-     *
-     * @param string $operator One of 'OR' or 'AND' denoting the logical operation with which all
-     *     constraints in the group are concatenated.
-     */
-    function begin_group($operator = 'OR')
-    {
-        $this->_mc->begin_group($operator);
-    }
-
-    /**
-     * Ends a group previously started with begin_group().
-     */
-    function end_group()
-    {
-        $this->_mc->end_group();
-    }
-
-    /**
-     * Limits the resultset to contain at most the specified number of records.
-     * Set the limit to zero to retrieve all available records.
-     *
-     * This implementation overrides the original QB implementation for the implementation
-     * of ACL restrictions.
-     *
-     * @param int $count The maximum number of records in the resultset.
-     */
-    function set_limit($limit)
-    {
-        $this->_reset();
-        $this->_limit = $limit;
-    }
-
-    /**
-     * Sets the offset of the first record to retrieve. This is a zero based index,
-     * so if you want to retrieve from the very first record, the correct offset would
-     * be zero, not one.
-     *
-     * This implementation overrides the original QB implementation for the implementation
-     * of ACL restrictions.
-     *
-     * @param int $offset The record number to start with.
-     */
-    function set_offset($offset)
-    {
-        $this->_reset();
-        $this->_offset = $offset;
+        return $this->_query->destroy();
     }
 
     function set_key_property($property, $value = null)
@@ -484,7 +295,7 @@ class midcom_core_collector
 
     function add_value_property($property)
     {
-        if (!$this->_mc->add_value_property($property))
+        if (!$this->_query->add_value_property($property))
         {
             debug_add("Failed to execute add_value_property '{$property}' for {$this->_real_class}.", MIDCOM_LOG_ERROR);
 
@@ -530,13 +341,13 @@ class midcom_core_collector
         $this->_reset();
         if ($this->_limit)
         {
-            $this->_mc->set_limit($this->_limit);
+            $this->_query->set_limit($this->_limit);
         }
         if ($this->_offset)
         {
-            $this->_mc->set_offset($this->_offset);
+            $this->_query->set_offset($this->_offset);
         }
-        return $this->_mc->count();
+        return $this->_query->count();
     }
 }
 ?>
---
+++ lib/midcom/core/query.php
@@ -0,0 +1,273 @@
+<?php
+/**
+ * @package midcom.core
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * Common base class for collector and querybuilder
+ *
+ * @package midcom.core
+ */
+abstract class midcom_core_query
+{
+    /**
+     * Set this element to true to hide all items which are currently invisible according
+     * to the approval/scheduling settings made using Metadata. This must be set before executing
+     * the query.
+     *
+     * NOTE: Approval checks not implemented in collector yet
+     *
+     * Be aware, that this setting will currently not use the QB to filter the objects accordingly,
+     * since there is no way yet to filter against parameters. This will mean some performance
+     * impact.
+     *
+     * @var boolean
+     */
+    public $hide_invisible = true;
+
+    /**
+     * Flag that tracks whether deleted visibility check have already been added
+     *
+     * @var boolean
+     */
+    protected $_visibility_checks_added = false;
+
+    /**
+     * The number of records to return to the client at most.
+     *
+     * @var int
+     */
+    protected $_limit = 0;
+
+    /**
+     * The offset of the first record the client wants to have available.
+     *
+     * @var int
+     */
+    protected $_offset = 0;
+
+    /**
+     * This is an internal count which is incremented by one each time a constraint is added.
+     * It is used to emit a warning if no constraints have been added to the QB during execution.
+     *
+     * @var int
+     */
+    protected $_constraint_count = 0;
+
+    /**
+     * This private helper holds the type that the application expects to retrieve
+     * from this instance.
+     *
+     * @var string
+     */
+    protected $_real_class;
+
+    /**
+     * The number of groups open
+     *
+     * @var int
+     */
+    protected $_groups = 0;
+
+    /**
+     * The number of records found by the last execute() run. This is -1 as long as no
+     * query has been executed. This member is read-only.
+     *
+     * @var int
+     */
+    protected $count = -1;
+
+    /**
+     * The query backend, should be set in constructor. Currently collector or querybuilder
+     *
+     * @var object
+     */
+    protected $_query;
+
+    /**
+     * The number of objects for which access was denied.
+     *
+     * This is especially useful for reimplementations of functions like mgd_get_article_by_name
+     * which must use the QB in the first place.
+     *
+     * @var int
+     */
+    protected $_denied = 0;
+
+    protected function _add_visibility_checks()
+    {
+        if ($this->_visibility_checks_added)
+        {
+            return;
+        }
+
+        if (!$this->hide_invisible)
+        {
+            $this->_visibility_checks_added = true;
+            return;
+        }
+
+        if (!$GLOBALS['midcom_config']['show_hidden_objects'])
+        {
+            $this->add_constraint('metadata.hidden', '=', false);
+            $now = strftime('%Y-%m-%d %H:%M:%S');
+            $this->begin_group('OR');
+                $this->add_constraint('metadata.schedulestart', '>', $now);
+                $this->add_constraint('metadata.schedulestart', '=', '0000-00-00 00:00:00');
+            $this->end_group();
+            $this->add_constraint('metadata.scheduleend', '<', $now);
+
+        }
+
+        $this->_visibility_checks_added = true;
+    }
+
+
+    /**
+     * Resets some internal variables for re-execute
+     */
+    protected function _reset()
+    {
+        $this->count = -1;
+        $this->denied = 0;
+    }
+
+    /**
+     * Add a constraint to the query builder.
+     *
+     * @param string $field The name of the MgdSchema property to query against.
+     * @param string $operator The operator to use for the constraint, currently supported are
+     *     <, <=, =, <>, >=, >, LIKE. LIKE uses the percent sign ('%') as a
+     *     wildcard character.
+     * @param mixed $value The value to compare against. It should be of the same type then the
+     *     queried property.
+     * @return boolean Indicating success.
+     */
+    public function add_constraint($field, $operator, $value)
+    {
+        $this->_reset();
+        // Add check against null values, Core MC is too stupid to get this right.
+        if ($value === null)
+        {
+            debug_add("Query: Cannot add constraint on field '{$field}' with null value.", MIDCOM_LOG_WARN);
+            return false;
+        }
+        if (! $this->_query->add_constraint($field, $operator, $value))
+        {
+            debug_add("Failed to execute add_constraint.", MIDCOM_LOG_ERROR);
+            debug_add("Class = '{$this->_real_class}, Field = '{$field}', Operator = '{$operator}'");
+            debug_print_r('Value:', $value);
+
+            return false;
+        }
+
+        $this->_constraint_count++;
+
+        return true;
+    }
+
+    /**
+     * Creates a new logical group within the query. They are set in parentheses in the final
+     * SQL and will thus be evaluated with precedence over the normal out-of-group constraints.
+     *
+     * While the call lets you decide whether all constraints within the group are AND'ed or OR'ed,
+     * only OR constraints make logically sense in this context, which is why this proxy function
+     * sets 'OR' as the default operator.
+     *
+     * @param string $operator One of 'OR' or 'AND' denoting the logical operation with which all
+     *     constraints in the group are concatenated.
+     */
+    function begin_group($operator = 'OR')
+    {
+        $this->_groups++;
+        try
+        {
+            @$this->_query->begin_group($operator);
+        }
+        catch (Exception $e)
+        {
+            debug_add("Failed to execute begin_group {$operator}, Midgard Exception: " . $e->getMessage(), MIDCOM_LOG_ERROR);
+            $this->_groups--;
+        }
+    }
+
+    /**
+     * Ends a group previously started with begin_group().
+     */
+    function end_group()
+    {
+        $this->_groups--;
+
+        try
+        {
+            @$this->_query->end_group();
+        }
+        catch (Exception $e)
+        {
+            debug_add("Failed to execute end_group, Midgard Exception: " . $e->getMessage(), MIDCOM_LOG_ERROR);
+        }
+    }
+
+    /**
+     * Limits the resultset to contain at most the specified number of records.
+     * Set the limit to zero to retrieve all available records.
+     *
+     * @param int $limit The maximum number of records in the resultset.
+     */
+    function set_limit($limit)
+    {
+        $this->_reset();
+        $this->_limit = $limit;
+    }
+
+    /**
+     * Sets the offset of the first record to retrieve. This is a zero based index,
+     * so if you want to retrieve from the very first record, the correct offset would
+     * be zero, not one.
+     *
+     * @param int $offset The record number to start with.
+     */
+    function set_offset($offset)
+    {
+        $this->_reset();
+        $this->_offset = $offset;
+    }
+
+
+    /**
+     * Add an ordering constraint to the query builder.
+     *
+     * @param string $field The name of the MgdSchema property to query against.
+     * @param string $ordering One of 'ASC' or 'DESC' indicating ascending or descending
+     *     ordering. The default is 'ASC'.
+     * @return boolean Indicating success.
+     */
+    function add_order($field, $ordering = null)
+    {
+        if ($ordering === null)
+        {
+            $result = $this->_query->add_order($field);
+        }
+        else
+        {
+            $result = $this->_query->add_order($field, $ordering);
+        }
+
+        if (! $result)
+        {
+            debug_add("Failed to execute add_order for column '{$field}', midgard error: " . midcom_connection::get_error_string(), MIDCOM_LOG_ERROR);
+        }
+
+        return $result;
+    }
+
+    abstract public function execute();
+
+    abstract public function count();
+
+    abstract public function count_unchecked();
+}
+?>
