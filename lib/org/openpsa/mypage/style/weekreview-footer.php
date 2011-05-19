da68e7d1a754ce68fb13637d7c917a21fdc1c573
Andreas Flack <flack@contentcontrol-berlin.de>
2011-05-19 21:14:34 +0200

    Don't round hour totals to int, since the individual items are shown as decimal as well

--- lib/org/openpsa/mypage/style/weekreview-footer.php
+++ lib/org/openpsa/mypage/style/weekreview-footer.php
@@ -1,6 +1,6 @@
 </table>
 <p>
 <?php
-echo $data['l10n']->get('totals') . ': ' . sprintf($data['l10n']->get('%d hours (%d invoiceable) reported'), $data['week_hours_total'], $data['week_hours_invoiceable']);
+echo $data['l10n']->get('totals') . ': ' . sprintf($data['l10n']->get('%02.2f hours (%02.2f invoiceable) reported'), $data['week_hours_total'], $data['week_hours_invoiceable']);
 ?>
 </p>
