25217bac8defa0e3a0f316e8f5becbf2bbf2edb1
Andreas Flack <flack@contentcontrol-berlin.de>
2011-06-02 13:29:22 +0200

    User helper_box layout

--- lib/org/openpsa/directmarketing/style/show-campaign-list-header.php
+++ lib/org/openpsa/directmarketing/style/show-campaign-list-header.php
@@ -2,6 +2,6 @@
 $nap = new midcom_helper_nav();
 $node = $nap->get_node($nap->get_current_node());
 ?>
-<div class="area">
-    <h2><?php echo $data['l10n']->get('campaigns'); ?></h2>
+<div class="area org_openpsa_helper_box">
+    <h3><?php echo $data['l10n']->get('campaigns'); ?></h3>
     <dl class="campaigns">
