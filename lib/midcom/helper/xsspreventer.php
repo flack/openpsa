1faf527f5730d0000656cb610eff7bbb14277482
Andreas Flack <flack@contentcontrol-berlin.de>
2011-06-23 20:38:49 +0200

    remove dead link and unnecessary inheritance

--- lib/midcom/helper/xsspreventer.php
+++ lib/midcom/helper/xsspreventer.php
@@ -9,11 +9,9 @@
 /**
  * Library for escaping user provided values displayed for example in input etc
  *
- * Backorted from MidCOM3 (http://repo.or.cz/w/midcom.git?a=tree;f=midcom_helper_xsspreventer;h=24b6e322e416d3d11b13a17e4b6dab2b83c99276;hb=HEAD)
- *
  * @package midcom.helper
  */
-class midcom_helper_xsspreventer extends midcom_baseclasses_components_purecode
+class midcom_helper_xsspreventer
 {
     /**
      * Escape value of an XML attribute, also adds quotes around it
