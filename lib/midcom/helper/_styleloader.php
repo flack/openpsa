66eee558830b2a8b9dfad14aec4d3d6b3cfe8012
Andreas Flack <flack@contentcontrol-berlin.de>
2011-07-02 19:20:04 +0100

    array_key_exists doesn't work with ArrayAccess objects...

--- lib/midcom/helper/_styleloader.php
+++ lib/midcom/helper/_styleloader.php
@@ -645,7 +645,7 @@ class midcom_helper__styleloader
         }
         else
         {
-            if (array_key_exists('theme', $GLOBALS['midcom_config']))
+            if (isset($GLOBALS['midcom_config']['theme']))
             {
                 $filename = preg_replace('/lib$/', 'themes', MIDCOM_ROOT) . '/' . $GLOBALS['midcom_config']['theme'] .  "/style/{$_element}.php";
                 if (file_exists($filename))
