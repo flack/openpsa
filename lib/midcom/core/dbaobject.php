8a63f9f7d4a84d2bbbdc2e25ce0857fd70723ea4
Andreas Flack <flack@contentcontrol-berlin.de>
2011-08-10 14:02:22 +0200

    Fix another strict notice

--- lib/midcom/core/dbaobject.php
+++ lib/midcom/core/dbaobject.php
@@ -821,7 +821,7 @@ abstract class midcom_core_dbaobject
     public function _on_loaded() {}
     public static function _on_prepare_exec_query_builder(&$qb) { return true; }
     public static function _on_prepare_new_query_builder(&$qb) {}
-    public function _on_process_query_result(&$result) {}
+    public static function _on_process_query_result(&$result) {}
     public static function _on_prepare_new_collector(&$mc) {}
     public static function _on_prepare_exec_collector(&$mc) { return true; }
     public function _on_process_collector_result(&$result) {}
