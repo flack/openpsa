26a9e9b8f4472fac23c3fb8c0f75c0bdad63e0c8
Andreas Flack <flack@contentcontrol-berlin.de>
2011-08-15 11:00:19 +0200

    Make sure we don't produce duplicate AT entries when runs are manually triggered

--- test/org/openpsa/sales/handler/deliverable/processTest.php
+++ test/org/openpsa/sales/handler/deliverable/processTest.php
@@ -26,7 +26,7 @@ class org_openpsa_sales_handler_deliverable_processTest extends openpsa_testcase
     public static function setUpBeforeClass()
     {
         self::$_person = self::create_user(true);
-        self::$_salesproject = self::create_class_object('org_openpsa_sales_salesproject_dba');
+        self::$_salesproject = self::create_class_object('org_openpsa_sales_salesproject_dba', array('customerContact' => self::$_person->id));
         $product_group = self::create_class_object('org_openpsa_products_product_group_dba');
         $product_attributes = array
         (
@@ -89,8 +89,9 @@ class org_openpsa_sales_handler_deliverable_processTest extends openpsa_testcase
             'salesproject' => self::$_salesproject->id,
             'product' => self::$_product->id,
             'start' => time(),
+            'continuous' => true,
             'unit' => 'q',
-            'orgOpenpsaObtype' => ORG_OPENPSA_PRODUCTS_DELIVERY_SUBSCRIPTION
+            'orgOpenpsaObtype' => ORG_OPENPSA_PRODUCTS_DELIVERY_SUBSCRIPTION,
         );
 
         $deliverable = $this->create_object('org_openpsa_sales_salesproject_deliverable_dba', $deliverable_attributes);
@@ -107,6 +108,30 @@ class org_openpsa_sales_handler_deliverable_processTest extends openpsa_testcase
         $this->assertEquals($url, 'salesproject/' . self::$_salesproject->guid . '/');
         $deliverable->refresh();
         $this->assertEquals(org_openpsa_sales_salesproject_deliverable_dba::STATUS_ORDERED, $deliverable->state);
+
+        $mc = new org_openpsa_relatedto_collector($deliverable->guid, 'midcom_services_at_entry_dba');
+        $mc->add_object_order('start', 'DESC');
+        $at_entries = $mc->get_related_objects();
+
+        $this->assertEquals(1, sizeof($at_entries));
+        $this->register_object($at_entries[0]);
+
+        $_POST = array
+        (
+            'run_cycle' => true,
+            'at_entry' => $at_entries[0]->guid
+        );
+
+        $url = $this->run_relocate_handler('org.openpsa.sales', array('deliverable', 'process', $deliverable->guid));
+        $this->assertEquals($url, 'salesproject/' . self::$_salesproject->guid . '/');
+        $deliverable->refresh();
+
+        $mc = new org_openpsa_relatedto_collector($deliverable->guid, 'midcom_services_at_entry_dba');
+        $mc->add_object_order('start', 'DESC');
+        $at_entries = $mc->get_related_objects();
+
+        $this->assertEquals(1, sizeof($at_entries));
+        $this->register_object($at_entries[0]);
 
         midcom::get('auth')->drop_sudo();
     }
