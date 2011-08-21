52189d3f51be8d0b425fb2c7ae709267df23f57d
Andreas Flack <flack@contentcontrol-berlin.de>
2011-08-21 15:26:51 +0200

    Re-add accidentally removed file

---
+++ test/org/openpsa/sales/handler/editTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+if (!defined('OPENPSA_TEST_ROOT'))
+{
+    define('OPENPSA_TEST_ROOT', dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR);
+    require_once(OPENPSA_TEST_ROOT . 'rootfile.php');
+}
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_sales_salesproject_editTest extends openpsa_testcase
+{
+    protected static $_person;
+
+    public static function setUpBeforeClass()
+    {
+        self::$_person = self::create_user(true);
+    }
+
+    public function testHandler_edit()
+    {
+        midcom::get('auth')->request_sudo('org.openpsa.sales');
+
+        $salesproject = $this->create_object('org_openpsa_sales_salesproject_dba');
+
+        $data = $this->run_handler('org.openpsa.sales', array('salesproject', 'edit', $salesproject->guid));
+        $this->assertEquals('salesproject_edit', $data['handler_id']);
+
+        midcom::get('auth')->drop_sudo();
+    }
+
+    public function testHandler_new()
+    {
+        midcom::get('auth')->request_sudo('org.openpsa.sales');
+
+        $data = $this->run_handler('org.openpsa.sales', array('salesproject', 'new'));
+        $this->assertEquals('salesproject_new', $data['handler_id']);
+
+        midcom::get('auth')->drop_sudo();
+    }
+}
+?>
