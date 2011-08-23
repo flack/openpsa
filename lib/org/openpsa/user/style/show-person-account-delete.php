31dc4797d8113df3aaedf73817b0d85f213d9438
Andreas Flack <flack@contentcontrol-berlin.de>
2011-08-23 18:56:40 +0200

    use new delete helper for account deletion

--- lib/org/openpsa/user/style/show-person-account-delete.php
+++ lib/org/openpsa/user/style/show-person-account-delete.php
@@ -1,11 +1,5 @@
 <div class="main">
-     <p><?php echo sprintf($data['l10n']->get("delete account %s %s"), $data['person']->firstname, $data['person']->lastname); ?></p>
+    <p><?php echo sprintf($data['l10n']->get("delete account %s %s"), $data['person']->firstname, $data['person']->lastname); ?></p>
 
-    <form method="post" action="<?php echo midcom_connection::get_url('uri'); ?>" class="datamanager2">
-        <div class="form_toolbar">
-            <input type="submit" id="submit_account" value="<?php echo $data['l10n_midcom']->get('delete'); ?>" name="midcom_helper_datamanager2_save" accesskey="d" class="delete" />
-            <input type="submit" value="<?php echo $data['l10n_midcom']->get('cancel'); ?>" name="midcom_helper_datamanager2_cancel" accesskey="c" class="cancel" />
-        </div>
-    </form>
-
+    <?php $data['controller']->display_form(); ?>
 </div>
