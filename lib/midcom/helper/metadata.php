f51c1aac7757e3da7abc9c1d9f22cd39b4353042
Andreas Flack <flack@contentcontrol-berlin.de>
2011-08-31 21:16:18 +0200

    Mark some properties as private

--- lib/midcom/helper/metadata.php
+++ lib/midcom/helper/metadata.php
@@ -92,34 +92,30 @@ class midcom_helper_metadata
     /**
      * Holds the values already read from the database.
      *
-     * @access private
      * @var Array
      */
-    var $_cache = Array();
+    private $_cache = Array();
 
     /**
      * The schema database URL to use for this instance.
      *
-     * @access private
      * @var string
      */
-    var $_schemadb_path = null;
+    private $_schemadb_path = null;
 
     /**
      * Datamanager instance for the given object.
      *
-     * @access private
      * @var midcom_helper_datamanager2
      */
-    var $_datamanager = null;
+    private $_datamanager = null;
 
     /**
      * Translation array for the object
      *
-     * @access private
      * @var array
      */
-    var $_translations = null;
+    private $_translations = null;
 
     /**
      * This will construct a new metadata object for an existing content object.
