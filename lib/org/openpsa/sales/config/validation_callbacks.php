284e37fd441796f4c6290659841a2d73804f4199
Andreas Flack <flack@contentcontrol-berlin.de>
2011-09-04 11:48:39 +0200

    fix validation for non-continuous deliverables

--- lib/org/openpsa/sales/config/validation_callbacks.php
+++ lib/org/openpsa/sales/config/validation_callbacks.php
@@ -1,13 +1,14 @@
 <?php
 function validate_subscription($fields)
 {
-	// check channels
-	if (   empty($fields['end'])
-	    && empty($fields['continuous']))
-	{
-	    $result['end'] = midcom::get('i18n')->get_string('select either end date or continuous', 'org.openpsa.sales');
-	    return $result;
-	}
+    // check channels
+    if (   (   empty($fields['end_date'])
+            || $fields['end_date'] == '0000-00-00')
+        && empty($fields['continuous']))
+    {
+        $result['end'] = midcom::get('i18n')->get_string('select either end date or continuous', 'org.openpsa.sales');
+        return $result;
+    }
 
     return true;
 }
