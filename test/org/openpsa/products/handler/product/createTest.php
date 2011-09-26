a8804a47e5c654304137f57912f0d6df6c9e75e7
Andreas Flack <flack@contentcontrol-berlin.de>
2011-09-26 21:29:50 +0200

    Add better test for product creation

--- test/org/openpsa/products/handler/product/createTest.php
+++ test/org/openpsa/products/handler/product/createTest.php
@@ -30,8 +30,36 @@ class org_openpsa_products_handler_product_createTest extends openpsa_testcase
     {
         midcom::get('auth')->request_sudo('org.openpsa.products');
 
-        $data = $this->run_handler('org.openpsa.products', array('product', 'create', self::$_group->code, 'default'));
+        $data = $this->run_handler('org.openpsa.products', array('product', 'create', 'default'));
         $this->assertEquals('create_product', $data['handler_id']);
+
+        $formdata = array
+        (
+            'title' => 'TEST ' . __CLASS__ . ' ' . time(),
+            'code' => 'TEST_' . __CLASS__ . '_' . time(),
+            'delivery' => (string) ORG_OPENPSA_PRODUCTS_DELIVERY_SINGLE,
+            'orgOpenpsaObtype' => (string) ORG_OPENPSA_PRODUCTS_PRODUCT_TYPE_GOODS,
+            'org_openpsa_products_productGroup_chooser_widget_selections' => array((string) self::$_group->id => '1')
+        );
+
+        $url = $this->submit_dm2_form('controller', $formdata, 'org.openpsa.products', array('product', 'create', 'default'));
+
+        $qb = org_openpsa_products_product_dba::new_query_builder();
+        $qb->add_constraint('productGroup', '=', self::$_group->id);
+        $results = $qb->execute();
+        $this->register_objects($results);
+        $this->assertEquals(1, sizeof($results));
+        $this->assertEquals('product/' . $results[0]->guid . '/', $url);
+
+        midcom::get('auth')->drop_sudo();
+    }
+
+    public function testHandler_create_group()
+    {
+        midcom::get('auth')->request_sudo('org.openpsa.products');
+
+        $data = $this->run_handler('org.openpsa.products', array('product', 'create', self::$_group->code, 'default'));
+        $this->assertEquals('create_group_product', $data['handler_id']);
 
         midcom::get('auth')->drop_sudo();
     }
