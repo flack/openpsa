c10577b0457f5d6092ce352ecb2e892ae74db1b4
Andreas Flack <flack@contentcontrol-berlin.de>
2011-10-14 13:46:54 +0200

    Fix display of declined tasks

--- lib/org/openpsa/projects/style/show-tasks-declined-item.php
+++ lib/org/openpsa/projects/style/show-tasks-declined-item.php
@@ -1,22 +1,7 @@
 <?php
 $prefix = $_MIDCOM->get_context_data(MIDCOM_CONTEXT_ANCHORPREFIX);
-$task =& $data['task'];
-echo "<dt><a href=\"{$prefix}task/{$task->guid}/\">{$task->title}</a>";
-if ($parent = $task->get_parent())
-{
-    if (is_a($parent, 'org_openpsa_projects_project'))
-    {
-        $parent_url = "{$prefix}project/{$parent->guid}/";
-    }
-    else
-    {
-        $parent_url = "{$prefix}task/{$parent->guid}/";
-    }
-    echo " <span class=\"metadata\">(<a href=\"{$parent_url}\">{$parent->title}</a>)</span>";
-}
-echo "</dt>\n";
-echo "<dd>\n";
-
+$task = $data['task'];
+echo "<tr>\n<td><a href=\"{$prefix}task/{$task->guid}/\">{$task->title}</a>";
 $task->get_members();
 if ( count($task->resources) > 0)
 {
@@ -28,5 +13,43 @@ if ( count($task->resources) > 0)
     }
     echo sprintf($data['l10n']->get("declined by %s"), $resources_string);
 }
+echo "</td>\n";
+echo "<td>\n";
+if ($parent = $task->get_parent())
+{
+    if (is_a($parent, 'org_openpsa_projects_project'))
+    {
+        $parent_url = "{$prefix}project/{$parent->guid}/";
+    }
+    else
+    {
+        $parent_url = "{$prefix}task/{$parent->guid}/";
+    }
+    echo " <a href=\"{$parent_url}\">{$parent->title}</a>\n";
+}
+echo "</td>\n";
+echo "<td>\n";
+if (   isset($data['priority_array'])
+	&& array_key_exists($task->priority, $data['priority_array']))
+{
+    echo $data['l10n']->get($data['priority_array'][$task->priority]);
+}
+echo "</td>\n";
 ?>
-</dd>
+</td>
+    <td class="numeric">
+      <span title="<?php echo $data['l10n']->get('planned hours'); ?>"><?php echo round($task->plannedHours, 2);
+    ?>
+    </span>
+    </td>
+    <td class="numeric">
+      <span title="<?php echo $data['l10n']->get('reported'); ?>"><?php echo round($task->reportedHours, 2);
+    ?>
+    </span>
+    </td>
+    <td class="numeric">
+      <span title="<?php echo $data['l10n']->get('invoiceable'); ?>"><?php echo round($task->invoiceableHours, 2);
+    ?>
+    </span>
+    </td>
+</tr>
