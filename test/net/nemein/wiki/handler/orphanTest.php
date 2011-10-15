1d74684f24f826cbe836f66fd911d1e24a779989
Andreas Flack <flack@contentcontrol-berlin.de>
2011-10-15 23:53:38 +0200

    Add basic handler tests for n.n.wiki

---
+++ test/net/nemein/wiki/handler/orphanTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+if (!defined('OPENPSA_TEST_ROOT'))
+{
+    define('OPENPSA_TEST_ROOT', dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR);
+    require_once OPENPSA_TEST_ROOT . 'rootfile.php';
+}
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class net_nemein_wiki_handler_orphanTest extends openpsa_testcase
+{
+    protected static $_topic;
+
+    public static function setUpBeforeClass()
+    {
+        $topic_attributes = array
+        (
+            'component' => 'net.nemein.wiki',
+            'name' => __CLASS__ . time()
+        );
+        self::$_topic = self::create_class_object('midcom_db_topic', $topic_attributes);
+    }
+
+    public function testHandler_orphan()
+    {
+        $data = $this->run_handler(self::$_topic, array('orphans'));
+        $this->assertEquals('orphans', $data['handler_id']);
+    }
+}
+?>
