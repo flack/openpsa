3b8e68468fd1f42a31a615d34763dc01cbcfeb02
Andreas Flack <flack@contentcontrol-berlin.de>
2011-10-27 11:25:21 +0200

    Take the default timezone setting into account

--- test/org/openpsa/mypage/viewerTest.php
+++ test/org/openpsa/mypage/viewerTest.php
@@ -26,12 +26,12 @@ class org_openpsa_mypage_viewerTest extends openpsa_testcase
         $viewer = new org_openpsa_mypage_viewer($topic, $config);
         $viewer->calculate_day('2011-10-26');
         $this->assertEquals('2011-10-26', $viewer->_request_data['this_day']);
-        $this->assertEquals(1319580000, $viewer->_request_data['day_start']);
-        $this->assertEquals(1319666399, $viewer->_request_data['day_end']);
+        $this->assertEquals(mktime(0, 0, 0, 10, 26, 2011), $viewer->_request_data['day_start']);
+        $this->assertEquals(mktime(23, 59, 59, 10, 26, 2011), $viewer->_request_data['day_end']);
         $this->assertEquals('2011-10-25', $viewer->_request_data['prev_day']);
         $this->assertEquals('2011-10-27', $viewer->_request_data['next_day']);
-        $this->assertEquals(1319407200, $viewer->_request_data['week_start']);
-        $this->assertEquals(1320015599, $viewer->_request_data['week_end']);
+        $this->assertEquals(mktime(0, 0, 0, 10, 24, 2011), $viewer->_request_data['week_start']);
+        $this->assertEquals(mktime(23, 59, 59, 10, 30, 2011), $viewer->_request_data['week_end']);
     }
 }
 ?>
