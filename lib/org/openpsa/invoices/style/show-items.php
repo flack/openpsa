3d2d9f117674afce32e82715580183658934e6e4
Andreas Flack <flack@contentcontrol-berlin.de>
2012-02-25 00:24:15 +0100

    Add missing sorttype

--- lib/org/openpsa/invoices/style/show-items.php
+++ lib/org/openpsa/invoices/style/show-items.php
@@ -8,9 +8,9 @@ $grid->set_option('pager', '#p_' . $grid->get_identifier());
 $grid->set_column('deliverable', midcom::get('i18n')->get_string('agreement', 'org.openpsa.projects'), 'width: 80');
 $grid->set_column('task', midcom::get('i18n')->get_string('task', 'org.openpsa.projects'), 'width: 80');
 $grid->set_column('description', midcom::get('i18n')->get_string('description', 'midcom'), 'editable: true, edittype: "textarea"');
-$grid->set_column('price', $data['l10n']->get('price'), 'align: "right", width: 40, formatter: "number", editable: true');
-$grid->set_column('quantity', $data['l10n']->get('quantity'), 'align: "right", width: 30, formatter: "number", editable: true');
-$grid->set_column('sum', $data['l10n']->get('sum'), 'align: "right", width: 60, formatter: "number", summaryType: "sum"');
+$grid->set_column('price', $data['l10n']->get('price'), 'align: "right", width: 40, formatter: "number", sorttype: "number", editable: true');
+$grid->set_column('quantity', $data['l10n']->get('quantity'), 'align: "right", width: 30, formatter: "number", sorttype: "number", editable: true');
+$grid->set_column('sum', $data['l10n']->get('sum'), 'align: "right", width: 60, formatter: "number", sorttype: "number", summaryType: "sum"');
 $grid->set_column('actions', '',  'width: 60, fixed: true');
 ?>
 
