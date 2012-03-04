fcac4165adacf3ac93eb1060f27933e27a3cddd3
Andreas Flack <flack@contentcontrol-berlin.de>
2012-03-04 15:42:43 +0100

    Add another handler test

--- test/net/nehmer/comments/handler/moderateTest.php
+++ test/net/nehmer/comments/handler/moderateTest.php
@@ -19,5 +19,26 @@ if (!defined('OPENPSA_TEST_ROOT'))
  */
 class net_nehmer_comments_handler_moderateTest extends openpsa_testcase
 {
+    public function testHandler_report()
+    {
+        $this->create_user(true);
+        $topic = $this->create_object('midcom_db_topic');
+        $attributes = array('objectguid' => $topic->guid);
+        $comment = $this->create_object('net_nehmer_comments_comment', $attributes);
+
+        midcom::get('auth')->request_sudo('net.nehmer.comments');
+
+        $_POST = array
+        (
+            'mark' => 'abuse'
+        );
+
+        $url = $this->run_relocate_handler('net.nehmer.comments', array('report', $comment->guid));
+        $this->assertEquals('', $url);
+
+        $comment->refresh();
+        $this->assertEquals(net_nehmer_comments_comment::ABUSE, $comment->status);
+        midcom::get('auth')->drop_sudo();
+    }
 }
 ?>
