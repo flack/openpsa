3af6e24795898fd7f437a5e3b5d1e84cd4c5bd0f
Andreas Flack <flack@contentcontrol-berlin.de>
2012-03-17 11:13:18 +0100

    Also convert photostream nodes

--- tools/gallery_converter.php
+++ tools/gallery_converter.php
@@ -14,15 +14,22 @@ class gallery_converter
         $results = $qb->execute();
         foreach ($results as $result)
         {
-            $this->_process_node($result);
+            $this->_process_gallery_node($result);
+        }
+        $qb = new midgard_query_builder('midgard_topic');
+        $qb->add_constraint('component', '=', 'org.routamc.photostream');
+        $results = $qb->execute();
+        foreach ($results as $result)
+        {
+            $this->_process_photostream_node($result);
         }
     }
 
-    private function _process_node($node)
+    private function _process_gallery_node($node)
     {
         $this->_node = $node;
 
-        $this->_output('Processing node ' . $this->_node->name);
+        $this->_output('Processing gallery node ' . $this->_node->name);
         $links = $this->_get_photolinks();
         $this->_output('Found ' . sizeof($links) . ' photolinks');
 
@@ -52,10 +59,66 @@ class gallery_converter
         $this->_output('Done.');
     }
 
+    private function _process_photostream_node($node)
+    {
+        $this->_node = $node;
+
+        $this->_output('Processing photostream node ' . $this->_node->name);
+        $photos = $this->_get_photos();
+        $this->_output('Found ' . sizeof($photos) . ' photos');
+
+        $i = 0;
+        foreach ($photos as $photo)
+        {
+            $qb = new midgard_query_builder('org_openpsa_slideshow_image');
+            $qb->add_constraint('attachment', '=', $photo->archival);
+            if ($qb->count() > 0)
+            {
+                $photo->delete();
+                continue;
+            }
+            $slide = new org_openpsa_slideshow_image();
+            $slide->title = $photo->title;
+            $slide->description = $photo->description;
+            $slide->topic = $this->_node->id;
+            $slide->attachment = $photo->archival;
+            $slide->image = $photo->main;
+            $slide->thumbnail = $photo->thumb;
+            $slide->position = $i;
+            if (!$slide->create())
+            {
+                throw new midcom_error('Could not save new image: ' . midcom_connection::get_error_string());
+            }
+
+            $i++;
+        }
+
+        if ($i > 0)
+        {
+            $this->_node->component = 'org.openpsa.slideshow';
+            if (!$this->_node->update())
+            {
+                throw new midcom_error('Could not update node: ' . midcom_connection::get_error_string());
+            }
+        }
+        else
+        {
+            $this->_node->delete();
+        }
+        $this->_output('Done.');
+    }
+
     private function _output($message)
     {
         echo $message . "\n";
         flush();
+    }
+
+    private function _get_photos()
+    {
+        $qb = new midgard_query_builder('org_routamc_photostream_photo');
+        $qb->add_constraint('node', '=', $this->_node->id);
+        return $qb->execute();
     }
 
     private function _get_photolinks()
