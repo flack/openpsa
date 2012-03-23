f498dba9b768e2643e98c247f29d2ebeb0942aac
flack <flack@contentcontrol-berlin.de>
2012-03-24 00:31:41 +0100

    Wrap functionality in class

--- quick_init.php
+++ quick_init.php
@@ -1,90 +1,117 @@
 <?php
-if (!extension_loaded('midgard2'))
-{
-    die("Midgard2 is not installed in your PHP environment.\n");
-}
-if (empty($argv[1]))
-{
-    $name = prompt('Enter project name', 'openpsa');
-}
-else
-{
-    $name = $argv[1];
-}
 
-function prompt($message, $default = null)
+class openpsa_installer
 {
-    echo $message;
-    if (null !== $default)
+    protected $_project_name;
+
+    public function __construct($args)
     {
-        echo ' [' . $default . ']';
+        if (!extension_loaded('midgard2'))
+        {
+            die("Midgard2 is not installed in your PHP environment.\n");
+        }
+        if (empty($argv[1]))
+        {
+            $this->_project_name = $this->prompt('Enter project name', 'openpsa');
+        }
+        else
+        {
+            $this->_project_name = $argv[1];
+        }
     }
-    echo ":\n";
-    $handle = fopen('php://stdin', 'r');
-    $input = trim(fgets($handle));
-    if (   empty($input)
-        && null !== $default)
+
+    public function fail($message, $error_code = 1)
     {
-        return $default;
+        $this->output($message);
+        exit($error_code);
     }
-    return $input;
-}
 
-function check_dir($directory)
-{
-    if (   !is_dir($directory)
-        && !mkdir($directory))
+    public function output($message)
     {
-        echo "Failed to create directory " . $directory;
-        exit(1);
+        echo $message . "\n";
     }
-}
 
-function link_file($path, $target, $link_directory)
-{
-    check_dir($link_directory);
-    if (   !file_exists($link_directory . '/' . $target)
-        && !link(dirname(__FILE__) . '/' . $path . '/' . $target, $link_directory . '/' . $target))
+    public function prompt($message, $default = null)
     {
-        echo "Failed to create link " . $link_directory . '/' . $target;
-        exit(1);
+        if (null !== $default)
+        {
+            $message .= ' [' . $default . ']';
+        }
+        $this->output($message . ":");
+
+        $handle = fopen('php://stdin', 'r');
+        $input = trim(fgets($handle));
+        if (   empty($input)
+            && null !== $default)
+        {
+            return $default;
+        }
+        return $input;
     }
-}
 
-$config = new midgard_config();
-if (!$config->read_file($name, false))
-{
-    check_dir('/var/lib/' . $name);
-    link_file('config', 'midgard_auth_types.xml', '/var/lib/' . $name . '/share');
-
-    // Create a config file
-    $config = new midgard_config();
-    $config->dbtype = 'SQLite';
-    $config->database = $argv[1];
-    $config->dbdir = '/var/lib/' . $name;
-    $config->blobdir = '/var/lib/' . $name . '/blobs';
-    $config->sharedir = '/var/lib/' . $name . '/share';
-    $config->vardir = '/var/lib/' . $name;
-    $config->cachedir = '/var/cache/' . $name;
-    $config->logfilename = '/var/log/' . $name . '/midgard.log';
-    $config->loglevel = 'debug';
-    if (!$config->save_file('' . $name . '', false))
+    protected function _check_dir($directory)
     {
-        echo "Failed to save Midgard2 config file to /etc/midgard2/conf.d\n";
-        exit(1);
+        if (   !is_dir($directory)
+            && !mkdir($directory))
+        {
+            $this->fail("Failed to create directory " . $directory);
+        }
     }
-    echo "Configuration file /etc/midgard2/conf.d/" . $name . " created.\n";
+
+    protected function _link_file($path, $target, $link_directory)
+    {
+        $this->_check_dir($link_directory);
+        if (   !file_exists($link_directory . '/' . $target)
+            && !link(dirname(__FILE__) . '/' . $path . '/' . $target, $link_directory . '/' . $target))
+        {
+            $this->fail("Failed to create link " . $link_directory . '/' . $target);
+        }
+    }
+
+    public function run()
+    {
+        $config = new midgard_config();
+        if (!$config->read_file($this->_project_name, false))
+        {
+            $config = $this->_create_config();
+        }
+        
+        // Open a DB connection with the config
+        $midgard = midgard_connection::get_instance();
+        if (!$midgard->open_config($config))
+        {
+            $this->fail("Failed to open Midgard database connection to {$this->_project_name}: " . $midgard->get_error_string());
+        }
+
+        require_once 'tools/bootstrap.php';
+        openpsa_prepare_database($config);
+    }
+
+    private function _create_config()
+    {
+        $this->_check_dir('/var/lib/' . $this->_project_name);
+        $this->_link_file('config', 'midgard_auth_types.xml', '/var/lib/' . $this->_project_name . '/share');
+
+        // Create a config file
+        $config = new midgard_config();
+        $config->dbtype = 'SQLite';
+        $config->database = $argv[1];
+        $config->dbdir = '/var/lib/' . $this->_project_name;
+        $config->blobdir = '/var/lib/' . $this->_project_name . '/blobs';
+        $config->sharedir = '/var/lib/' . $this->_project_name . '/share';
+        $config->vardir = '/var/lib/' . $this->_project_name;
+        $config->cachedir = '/var/cache/' . $this->_project_name;
+        $config->logfilename = '/var/log/' . $this->_project_name . '/midgard.log';
+        $config->loglevel = 'debug';
+        if (!$config->save_file('' . $this->_project_name . '', false))
+        {
+            $this->fail("Failed to save Midgard2 config file to /etc/midgard2/conf.d");
+        }
+        $this->output("Configuration file /etc/midgard2/conf.d/" . $this->_project_name . " created.");
+        return $config;
+    }
 }
 
-// Open a DB connection with the config
-$midgard = midgard_connection::get_instance();
-if (!$midgard->open_config($config))
-{
-    echo "Failed to open Midgard database connection to {$name}: " . $midgard->get_error_string() ."\n";
-    exit(1);
-}
-
-require_once 'tools/bootstrap.php';
-
-openpsa_prepare_database($config);
+$installer = new openpsa_installer($argv);
+$installer->run();
 ?>
