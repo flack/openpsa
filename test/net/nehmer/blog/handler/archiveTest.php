68087fafbb8774a18a4d0697bf0cc55a68db96f1
flack <flack@contentcontrol-berlin.de>
2012-04-07 14:39:57 +0200

    Add basic handler tests

---
+++ test/net/nehmer/blog/handler/archiveTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+if (!defined('OPENPSA_TEST_ROOT'))
+{
+    define('OPENPSA_TEST_ROOT', dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR);
+    require_once OPENPSA_TEST_ROOT . 'rootfile.php';
+}
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class net_nehmer_blog_handler_archiveTest extends openpsa_testcase
+{
+    protected static $_topic;
+    protected static $_article;
+
+    public static function setUpBeforeClass()
+    {
+        self::$_topic = self::get_component_node('net.nehmer.blog');
+        $article_properties = array
+        (
+            'topic' => self::$_topic->id,
+            'name' => __CLASS__ . time()
+        );
+        self::$_article = self::create_class_object('midcom_db_article', $article_properties);
+    }
+
+    public function testHandler_welcome()
+    {
+        midcom::get('auth')->request_sudo('net.nehmer.blog');
+
+        $data = $this->run_handler(self::$_topic, array('archive'));
+        $this->assertEquals('archive-welcome', $data['handler_id']);
+
+        $this->show_handler($data);
+        midcom::get('auth')->drop_sudo();
+    }
+
+    public function testHandler_year()
+    {
+        midcom::get('auth')->request_sudo('net.nehmer.blog');
+
+        $data = $this->run_handler(self::$_topic, array('archive', 'year', date('Y')));
+        $this->assertEquals('archive-year', $data['handler_id']);
+
+        $this->show_handler($data);
+        midcom::get('auth')->drop_sudo();
+    }
+
+    public function testHandler_month()
+    {
+        midcom::get('auth')->request_sudo('net.nehmer.blog');
+
+        $data = $this->run_handler(self::$_topic, array('archive', 'month', date('Y'), date('m')));
+        $this->assertEquals('archive-month', $data['handler_id']);
+
+        $this->show_handler($data);
+        midcom::get('auth')->drop_sudo();
+    }
+}
+?>
