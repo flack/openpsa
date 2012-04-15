b140ef5053a801f5e4e33fd5a34f82ca9b391227
flack <flack@contentcontrol-berlin.de>
2012-04-15 16:12:39 +0200

    Fix warning

--- lib/org/openpsa/projects/style/show-json-tasks.php
+++ lib/org/openpsa/projects/style/show-json-tasks.php
@@ -17,7 +17,6 @@ foreach ($tasks as $task)
         'priority' => $task['priority'],
         'priority_title' => $data['priority_array'][$task['index_priority']],
         'planned_hours' => $task['planned_hours'],
-        'invoiceable_hours' => $task['invoiceable_hours'],
         'approved_hours' => $task['approved_hours'],
         'reported_hours' => $task['reported_hours'],
         'start' => date("d.m.Y", strtotime($task['start'])),
