b7d6e86e3a28913e843066fd073052a3cfd768c7
Andreas Flack <flack@contentcontrol-berlin.de>
2012-04-22 01:28:43 +0200

    Safety against missing or unreadable person objects

--- lib/midcom/style/midcom_helper_datamanager2_unlock.php
+++ lib/midcom/style/midcom_helper_datamanager2_unlock.php
@@ -1,11 +1,20 @@
 <?php
 $metadata = $this->data['handler']->datamanager->storage->object->metadata;
-$person = new midcom_db_person($metadata->locker);
+try
+{
+    $person = new midcom_db_person($metadata->locker);
+    $name = $person->name;
+}
+catch (midcom_error $e)
+{
+    $name = $this->data['handler']->_l10n_midcom->get('unknown user');
+    $e->log();
+}
 ?>
     <div class="midcom_helper_datamanager2_unlock">
         <h2><?php echo $this->data['handler']->_l10n->get('object locked'); ?></h2>
         <p>
-            <?php echo sprintf($this->data['handler']->_l10n->get('this object was locked by %s'), $person->name); ?>.
+            <?php echo sprintf($this->data['handler']->_l10n->get('this object was locked by %s'), $name); ?>.
             <?php echo sprintf($this->data['handler']->_l10n->get('lock will expire on %s'), strftime('%x %X', ($metadata->get('locked') + ($GLOBALS['midcom_config']['metadata_lock_timeout'] * 60)))); ?>.
         </p>
 <?php
