3f4d53b2e59ff05f70d8fb9cb5b94c877e5622b8
Andreas Flack <flack@contentcontrol-berlin.de>
2012-08-03 14:44:38 +0200

    Update tinymce to 3.5.6

---
+++ static/midcom.helper.datamanager2/tinymce/imagepopup/langs/en.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('en.imagepopup',{
+title : 'Insert attachment',
+desc : 'Insert image or attachment'
+});
---
+++ static/midcom.helper.datamanager2/tinymce/imagepopup/langs/fi.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('fi.imagepopup',{
+title : 'Lis&auml;&auml; liitetiedosto',
+desc : 'Lis&auml;&auml; kuva tai liite'
+});
