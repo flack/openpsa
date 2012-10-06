a4b5f76b3f10a2fcaff324c27cc1063784ea108c
flack <flack@contentcontrol-berlin.de>
2012-10-06 15:20:37 +0200

    Small fixes for style element helper

--- lib/midgard/admin/asgard/style/midgard_admin_asgard_objecthelper_style_elementnames.php
+++ lib/midgard/admin/asgard/style/midgard_admin_asgard_objecthelper_style_elementnames.php
@@ -46,7 +46,7 @@ $prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
 <script type="text/javascript">
 // <![CDATA[
 $(document).ready(function(){
-    $(".midgard_admin_asgard_objecthelper_help").accordion({ header: 'h3', active: false });
+        $(".midgard_admin_asgard_objecthelper_help").accordion({ header: 'h3', active: false, collapsible: true });
 });
 // ]]>
 </script>
