e7c86d19b8fc3961ae0a3a6c8b091d75abd1e763
Andreas Flack <flack@contentcontrol-berlin.de>
2012-10-12 19:25:10 +0200

    Unify table structures and make batch handling available for both cases

--- lib/midgard/admin/asgard/style/midgard_admin_asgard_welcome.php
+++ lib/midgard/admin/asgard/style/midgard_admin_asgard_welcome.php
@@ -12,34 +12,34 @@ foreach ($data['schema_types'] as $schema_type)
     $type_choices[$schema_type] = $data['reflectors'][$schema_type]->get_class_label();
     asort($type_choices);
 }
-$type_choices = Array('any' => midcom::get('i18n')->get_string('any', 'midgard.admin.asgard')) + $type_choices;
+$type_choices = Array('any' => $data['l10n']->get('any')) + $type_choices;
 
 $revised_after_choices = array();
 if ($data['config']->get('enable_review_dates'))
 {
     $review_by_choices = array();
-    $revised_after_choices['any'] = midcom::get('i18n')->get_string('any', 'midgard.admin.asgard');
-    $review_by_choices['any'] = midcom::get('i18n')->get_string('any', 'midgard.admin.asgard');
+    $revised_after_choices['any'] = $data['l10n']->get('any');
+    $review_by_choices['any'] = $data['l10n']->get('any');
     // 1 week
     $date = mktime(0, 0, 0, date('m'), date('d') + 6, date('Y'));
-    $review_by_choices[$date] = midcom::get('i18n')->get_string('1 week', 'midgard.admin.asgard');
+    $review_by_choices[$date] = $data['l10n']->get('1 week');
     // 2 weeks
     $date = mktime(0, 0, 0, date('m'), date('d') + 13, date('Y'));
-    $review_by_choices[$date] = midcom::get('i18n')->get_string('2 weeks', 'midgard.admin.asgard');
+    $review_by_choices[$date] = $data['l10n']->get('2 weeks');
     // 1 month
     $date = mktime(0, 0, 0, date('m') + 1, date('d'), date('Y'));
-    $review_by_choices[$date] = midcom::get('i18n')->get_string('1 month', 'midgard.admin.asgard');
+    $review_by_choices[$date] = $data['l10n']->get('1 month');
 }
 
 // 1 day
 $date = mktime(0, 0, 0, date('m'), date('d') - 1, date('Y'));
-$revised_after_choices[$date] = midcom::get('i18n')->get_string('1 day', 'midgard.admin.asgard');
+$revised_after_choices[$date] = $data['l10n']->get('1 day');
 // 1 week
 $date = mktime(0, 0, 0, date('m'), date('d') - 6, date('Y'));
-$revised_after_choices[$date] = midcom::get('i18n')->get_string('1 week', 'midgard.admin.asgard');
+$revised_after_choices[$date] = $data['l10n']->get('1 week');
 // 1 month
 $date = mktime(0, 0, 0, date('m') - 1, date('d'), date('Y'));
-$revised_after_choices[$date] = midcom::get('i18n')->get_string('1 month', 'midgard.admin.asgard');
+$revised_after_choices[$date] = $data['l10n']->get('1 month');
 ?>
 
 <div id="latest_objects">
@@ -47,7 +47,7 @@ $revised_after_choices[$date] = midcom::get('i18n')->get_string('1 month', 'midg
     <div class="filter">
         <form name="latest_objects_filter" method="get">
             <div class="type_filter">
-                <label for="type_filter"><?php echo midcom::get('i18n')->get_string('type', 'midgard.admin.asgard'); ?></label>
+                <label for="type_filter"><?php echo $data['l10n']->get('type'); ?></label>
                 <select name="type_filter" id="type_filter">
                     <?php
                     foreach ($type_choices as $value => $label)
@@ -64,7 +64,7 @@ $revised_after_choices[$date] = midcom::get('i18n')->get_string('1 month', 'midg
                 </select>
             </div>
             <div class="revised_after">
-                <label for="revised_after"><?php echo midcom::get('i18n')->get_string('objects revised within', 'midgard.admin.asgard'); ?></label>
+                <label for="revised_after"><?php echo $data['l10n']->get('objects revised within'); ?></label>
                 <select name="revised_after" id="revised_after">
                     <?php
                     foreach ($revised_after_choices as $value => $label)
@@ -85,7 +85,7 @@ $revised_after_choices[$date] = midcom::get('i18n')->get_string('1 month', 'midg
             {
                 ?>
             <div class="review_by">
-                <label for="review_by"><?php echo midcom::get('i18n')->get_string('objects expiring within', 'midgard.admin.asgard'); ?></label>
+                <label for="review_by"><?php echo $data['l10n']->get('objects expiring within'); ?></label>
                 <select name="review_by" id="review_by">
                     <?php
                     foreach ($review_by_choices as $value => $label)
@@ -106,48 +106,48 @@ $revised_after_choices[$date] = midcom::get('i18n')->get_string('1 month', 'midg
             ?>
             <input type="checkbox" id="only_mine" name="only_mine" value="1" <?php if (isset($data['only_mine']) && $data['only_mine'] == 1) { echo ' checked="checked"'; } ?> />
             <label for="only_mine">
-                <?php echo midcom::get('i18n')->get_string('only mine', 'midgard.admin.asgard'); ?>
+                <?php echo $data['l10n']->get('only mine'); ?>
             </label>
-            <input type="submit" name="filter" value="<?php echo midcom::get('i18n')->get_string('filter', 'midgard.admin.asgard'); ?>" />
+            <input type="submit" name="filter" value="<?php echo $data['l10n']->get('filter'); ?>" />
         </form>
     </div>
 
+    <h2><?php echo midcom::get('i18n')->get_string('latest activities', 'midcom.helper.activitystream'); ?></h2>
+    <form name="latest_objects_mass_action" method="post">
+      <table class="results table_widget" id="batch_process">
+        <thead>
+          <tr>
+            <th class="selection">&nbsp;</th>
+            <th class="icon">&nbsp;</th>
+            <th class="title"><?php echo $data['l10n_midcom']->get('title'); ?></th>
+            <?php
+            if ($data['config']->get('enable_review_dates'))
+            {
+                echo "            <th class=\"review_by\">" . $data['l10n']->get('review date') . "</th>\n";
+            } ?>
+
+            <th class="revised"><?php echo midcom::get('i18n')->get_string('revised', 'midcom.admin.folder'); ?></th>
+            <th class="revisor"><?php echo midcom::get('i18n')->get_string('revisor', 'midcom.admin.folder'); ?></th>
+            <th class="approved"><?php echo $data['l10n_midcom']->get('approved'); ?></th>
+            <th class="revision"><?php echo midcom::get('i18n')->get_string('revision', 'midcom.admin.folder'); ?></th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <td colspan="5">
+              <label for="select_all">
+                <input type="checkbox" name="select_all" id="select_all" value="" onclick="jQuery(this).check_all('#batch_process tbody');" /><?php echo $data['l10n']->get('select all');?>
+              </label>
+              <label for="invert_selection">
+                <input type="checkbox" name="invert_selection" id="invert_selection" value="" onclick="jQuery(this).invert_selection('#batch_process tbody');" /><?php echo $data['l10n']->get('invert selection'); ?>
+              </label>
+            </td>
+          </tr>
+        </tfoot>
+    <tbody>
 <?php
 if (isset($_GET['type_filter']))
 {
-    echo "    <form name=\"latest_objects_mass_action\" method=\"post\">";
-    echo "<table class=\"results table_widget\" id =\"batch_process\">\n";
-    echo "    <thead>\n";
-    echo "        <tr>\n";
-    echo "            <th class=\"selection\">&nbsp;</th>\n";
-    echo "            <th class=\"icon\">&nbsp;</th>\n";
-    echo "            <th class=\"title\">" . midcom::get('i18n')->get_string('title', 'midcom') . "</th>\n";
-
-    if ($data['config']->get('enable_review_dates'))
-    {
-        echo "            <th class=\"review_by\">" . midcom::get('i18n')->get_string('review date', 'midgard.admin.asgard') . "</th>\n";
-    }
-
-    echo "            <th class=\"revised\">" . midcom::get('i18n')->get_string('revised', 'midcom.admin.folder') . "</th>\n";
-    echo "            <th class=\"revisor\">" . midcom::get('i18n')->get_string('revisor', 'midcom.admin.folder') . "</th>\n";
-    echo "            <th class=\"approved\">" . midcom::get('i18n')->get_string('approved', 'midcom') . "</th>\n";
-    echo "            <th class=\"revision\">" . midcom::get('i18n')->get_string('revision', 'midcom.admin.folder') . "</th>\n";
-    echo "        </tr>\n";
-    echo "    </thead>\n";
-    echo "    <tfoot>\n";
-    echo "            <tr>\n";
-    echo "            <td colspan=\"5\">\n";
-    echo "                <label for=\"select_all\">\n";
-    echo "                    <input type=\"checkbox\" name=\"select_all\" id=\"select_all\" value=\"\" onclick=\"jQuery(this).check_all('#batch_process tbody');\" />" . midcom::get('i18n')->get_string('select all', 'midgard.admin.asgard');
-    echo "                </label>\n";
-    echo "                <label for=\"invert_selection\">\n";
-    echo "                    <input type=\"checkbox\" name=\"invert_selection\" id=\"invert_selection\" value=\"\" onclick=\"jQuery(this).invert_selection('#batch_process tbody');\" />" . midcom::get('i18n')->get_string('invert selection', 'midgard.admin.asgard');
-    echo "                </label>\n";
-    echo "            </td>\n";
-    echo "        </tr>\n";
-    echo "    </tfoot>\n";
-    echo "    <tbody>\n";
-
     foreach ($data['revised'] as $object)
     {
         $class = get_class($object);
@@ -155,12 +155,12 @@ if (isset($_GET['type_filter']))
         $approved_str = strftime('%x %X', $approved);
         if ($approved == 0  || $approved < $object->metadata->revised)
         {
-            $approved_str = midcom::get('i18n')->get_string('not approved', 'midgard.admin.asgard');
+            $approved_str = $data['l10n']->get('not approved');
         }
         $title = substr($data['reflectors'][$class]->get_object_label($object), 0, 60);
         if (empty($title))
         {
-            $title = '[' . midcom::get('i18n')->get_string('no title', 'midgard.admin.asgard') . ']';
+            $title = '[' . $data['l10n']->get('no title') . ']';
         }
         $revisor = midcom::get('auth')->get_user($object->metadata->revisor);
 
@@ -197,32 +197,6 @@ if (isset($_GET['type_filter']))
         echo "            <td class=\"revision\">{$object->metadata->revision}</td>\n";
         echo "        </tr>\n";
     }
-
-    echo "    </tbody>\n";
-    echo "</table>\n";
-    echo "<script type=\"text/javascript\">\n";
-    echo "        // <![CDATA[\n";
-    echo "            jQuery('#batch_process').tablesorter(\n";
-    echo "            {\n ";
-    echo "                widgets: ['zebra'],\n";
-    echo "                sortList: [[2,0]]\n";
-    echo "            });\n";
-    echo "        // ]]>\n";
-    echo "    </script>\n";
-?>
-        <div class="actions">
-            <div class="action">
-                <select name="mass_action" id="mass_action">
-                    <option value=""><?php echo midcom::get('i18n')->get_string('choose action', 'midgard.admin.asgard'); ?></option>
-                    <option value="delete"><?php echo midcom::get('i18n')->get_string('delete', 'midcom'); ?></option>
-                    <option value="approve"><?php echo midcom::get('i18n')->get_string('approve', 'midcom'); ?></option>
-                </select>
-            </div>
-            <input type="submit" name="execute_mass_action" value="<?php echo midcom::get('i18n')->get_string('apply to selected', 'midgard.admin.asgard'); ?>" />
-        </div>
-    </form>
-
-<?php
 }
 else
 {
@@ -231,18 +205,6 @@ else
     {
         $reflectors = Array();
 
-        echo "<h2>" . midcom::get('i18n')->get_string('latest activities', 'midcom.helper.activitystream') . "</h2>\n";
-        echo "<table class=\"results table_widget\" id =\"last_visited\">\n";
-        echo "    <thead>\n";
-        echo "        <tr>\n";
-        echo "            <th class=\"icon\">&nbsp;</th>\n";
-        echo "            <th class=\"title\">" . midcom::get('i18n')->get_string('title', 'midcom') . "</th>\n";
-        echo "            <th class=\"revised\">" . midcom::get('i18n')->get_string('date', 'midcom') . "</th>\n";
-        echo "            <th class=\"revisor\">" . midcom::get('i18n')->get_string('actor', 'midcom.helper.activitystream') . "</th>\n";
-        echo "            <th class=\"action\">" . midcom::get('i18n')->get_string('action', 'midcom.helper.activitystream') . "</th>\n";
-        echo "        </tr>\n";
-        echo "    </thead>\n";
-        echo "    <tbody>\n";
         foreach ($activities as $activity)
         {
             try
@@ -286,26 +248,57 @@ else
                 $title = $object->guid;
             }
 
+            $approved = $object->metadata->approved;
+            $approved_str = strftime('%x %X', $approved);
+            if ($approved == 0  || $approved < $object->metadata->revised)
+            {
+                $approved_str = midcom::get('i18n')->get_string('not approved', 'midgard.admin.asgard');
+            }
+
             echo "        <tr>\n";
-            echo "            <td class=\"icon\">" . $reflectors[$class]->get_object_icon($object) . "</td>\n";
-            echo "            <td class=\"title\"><a href=\"{$prefix}__mfa/asgard/object/{$data['default_mode']}/{$object->guid}/\" title=\"{$class}\">" . $title . "</a></td>\n";
-            echo "            <td class=\"revised\">" . strftime('%x %X', $activity->metadata->published) . "</td>\n";
-            echo "            <td class=\"revisor\">{$actors[$activity->actor]->name}</td>\n";
-            echo "            <td class=\"revision\">{$activity->summary}</td>\n";
+            echo "          <td class=\"selection\"><input type=\"checkbox\" name=\"selections[]\" value=\"{$object->guid}\" /></td>\n";
+            echo "          <td class=\"icon\">" . $reflectors[$class]->get_object_icon($object) . "</td>\n";
+            echo "          <td class=\"title\"><a href=\"{$prefix}__mfa/asgard/object/{$data['default_mode']}/{$object->guid}/\" title=\"{$class}\">" . $title . "</a></td>\n";
+            if ($data['config']->get('enable_review_dates'))
+            {
+                $review_date = $object->get_parameter('midcom.helper.metadata', 'review_date');
+                if (!$review_date)
+                {
+                    echo "            <td class=\"review_by\">N/A</td>\n";
+                }
+                else
+                {
+                    echo "            <td class=\"review_by\">" . strftime('%x', $review_date) . "</td>\n";
+                }
+            }
+            echo "          <td class=\"revised\">" . strftime('%x %X', $activity->metadata->published) . "</td>\n";
+            echo "          <td class=\"revisor\">{$actors[$activity->actor]->name}</td>\n";
+            echo "          <td class=\"approved\">{$approved_str}</td>\n";
+            echo "          <td class=\"revision\">{$activity->summary}</td>\n";
             echo "        </tr>\n";
         }
-        echo "    </tbody>\n";
-        echo "</table>\n";
-        echo "<script type=\"text/javascript\">\n";
-        echo "        // <![CDATA[\n";
-        echo "            jQuery('#last_visited').tablesorter(\n";
-        echo "            {\n ";
-        echo "                widgets: ['zebra']\n";
-        echo "            });\n";
-        echo "        // ]]>\n";
-        echo "    </script>\n";
-      }
-  }
-?>
-
+    }
+}?>
+  </tbody>
+</table>
+    <script type="text/javascript">
+       // <![CDATA[
+       jQuery('#batch_process').tablesorter(
+       {
+           widgets: ['zebra'],
+           sortList: [[2,0]]
+       });
+       // ]]>
+    </script>
+    <div class="actions">
+        <div class="action">
+            <select name="mass_action" id="mass_action">
+                <option value=""><?php echo $data['l10n']->get('choose action'); ?></option>
+                <option value="delete"><?php echo $data['l10n_midcom']->get('delete'); ?></option>
+                <option value="approve"><?php echo $data['l10n_midcom']->get('approve'); ?></option>
+            </select>
+        </div>
+        <input type="submit" name="execute_mass_action" value="<?php echo $data['l10n']->get('apply to selected'); ?>" />
+    </div>
+</form>
 </div>
