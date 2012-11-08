bc7196ec42305fc0ef526cebf4d3c27eda0cbbed
Andreas Flack <flack@contentcontrol-berlin.de>
2012-11-08 12:38:45 +0100

    Add simple test for auto-init handler

---
+++ test/org/openpsa/calendar/viewerTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+if (!defined('OPENPSA_TEST_ROOT'))
+{
+    define('OPENPSA_TEST_ROOT', dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR);
+    require_once OPENPSA_TEST_ROOT . 'rootfile.php';
+}
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_calendar_viewerTest extends openpsa_testcase
+{
+    protected static $_topic;
+
+    public static function setUpBeforeClass()
+    {
+        $attributes = array
+        (
+            'component' => 'org.openpsa.calendar',
+            'name' => __CLASS__ . microtime(true)
+        );
+        midcom::get('auth')->request_sudo('org.openpsa.calendar');
+        self::$_topic = self::create_class_object('midcom_db_topic', $attributes);
+        self::$_topic->set_parameter('org.openpsa.calendar', 'calendar_root_event', 0);
+        midcom::get('auth')->drop_sudo();
+
+        self::create_user(true);
+    }
+
+    public function testHandler_notinitialized()
+    {
+        midcom::get('auth')->request_sudo('org.openpsa.calendar');
+
+        $data = $this->run_handler(self::$_topic);
+        $this->assertEquals('not_initialized', $data['handler_id']);
+        $this->assertInstanceOf('midcom_response_relocate', $data['__openpsa_testcase_response']);
+        $this->assertEquals('', $data['__openpsa_testcase_response']->url);
+    }
+
+}
+?>
