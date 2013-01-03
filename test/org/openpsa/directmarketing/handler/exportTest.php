d031991fd50e149e6530962a561bb129a4e8ce53
flack <flack@contentcontrol-berlin.de>
2013-01-03 10:10:05 +0100

    adapt tests to changed handler

--- test/org/openpsa/directmarketing/handler/exportTest.php
+++ test/org/openpsa/directmarketing/handler/exportTest.php
@@ -34,21 +34,8 @@ class org_openpsa_directmarketing_handler_exportTest extends openpsa_testcase
 
         midcom::get('auth')->request_sudo('org.openpsa.directmarketing');
 
-        $data = $this->run_handler('org.openpsa.directmarketing', array('campaign', 'export', 'csv', $campaign->guid, 'filename.csv'));
-        $this->assertEquals('export_csv1', $data['handler_id']);
-
-        midcom::get('auth')->drop_sudo();
-    }
-
-    public function testHandler_csv2()
-    {
-        $helper = new openpsa_test_campaign_helper($this);
-        $campaign = $helper->get_campaign();
-
-        midcom::get('auth')->request_sudo('org.openpsa.directmarketing');
-
-        $url = $this->run_relocate_handler('org.openpsa.directmarketing', array('campaign', 'export', 'csv', $campaign->guid));
-        $this->assertEquals('campaign/export/csv/' . $campaign->guid . '/_' . date('Y-m-d') . '.csv', $url);
+        $data = $this->run_handler('org.openpsa.directmarketing', array('campaign', 'export', 'csv', $campaign->guid));
+        $this->assertEquals('export_csv', $data['handler_id']);
 
         midcom::get('auth')->drop_sudo();
     }
