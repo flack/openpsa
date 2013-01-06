1f0e75ff69f6b1697232ab10d089dc0a6d0707dc
flack <flack@contentcontrol-berlin.de>
2013-01-06 13:38:31 +0100

    Also test actual deletion

--- test/midcom/admin/folder/handler/deleteTest.php
+++ test/midcom/admin/folder/handler/deleteTest.php
@@ -9,7 +9,7 @@
 if (!defined('OPENPSA_TEST_ROOT'))
 {
     define('OPENPSA_TEST_ROOT', dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR);
-    require_once(OPENPSA_TEST_ROOT . 'rootfile.php');
+    require_once OPENPSA_TEST_ROOT . 'rootfile.php';
 }
 
 /**
@@ -22,9 +22,24 @@ class midcom_admin_folder_handler_deleteTest extends openpsa_testcase
     public function testHandler_delete()
     {
         midcom::get('auth')->request_sudo('midcom.admin.folder');
+        $parent = self::get_component_node('net.nehmer.static');
+        $attributes = array
+        (
+            'component' => 'net.nehmer.static',
+            'parent' => $parent->id
+        );
+        $topic = $this->create_object('midcom_db_topic', $attributes);
 
-        $data = $this->run_handler('net.nehmer.static', array('__ais', 'folder', 'delete'));
+        $data = $this->run_handler($topic, array('__ais', 'folder', 'delete'));
         $this->assertEquals('____ais-folder-delete', $data['handler_id']);
+        $this->show_handler($data);
+
+        $_REQUEST = array('f_submit' => true);
+        $url = $this->run_relocate_handler($topic, array('__ais', 'folder', 'delete'));
+
+        $qb = midcom_db_topic::new_query_builder();
+        $qb->add_constraint('id', '=', $topic->id);
+        $this->assertEquals(0, $qb->count());
 
         midcom::get('auth')->drop_sudo();
     }
