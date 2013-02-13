c49192867f0cd0af3747b845efaafc19141ff67d
Andreas Flack <flack@contentcontrol-berlin.de>
2013-02-13 11:25:55 +0100

    Remove unneeded uses of hostname

--- lib/org/openpsa/widgets/ui.php
+++ lib/org/openpsa/widgets/ui.php
@@ -124,8 +124,7 @@ class org_openpsa_widgets_ui extends midcom_baseclasses_components_purecode
     public static function render_tabs($guid = null, $tabdata = array())
     {
         $uipage = self::get_config_value('ui_page');
-        $host_prefix = substr(midcom::get()->get_host_prefix(), strlen(midcom::get()->get_host_name()));
-        $prefix = $host_prefix . $uipage . '/';
+        $prefix = midcom_connection::get_url('self') . $uipage . '/';
 
         if (null !== $guid)
         {
