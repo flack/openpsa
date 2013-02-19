f29d1ca607996353c1dd184c51f21ce40dfe78d6
flack <flack@contentcontrol-berlin.de>
2013-02-19 19:49:55 +0100

    Remove workaround for problem that was since fixed upstream

--- lib/midcom/core/service/implementation/urlgeneratori18n.php
+++ lib/midcom/core/service/implementation/urlgeneratori18n.php
@@ -16,35 +16,7 @@ class midcom_core_service_implementation_urlgeneratori18n implements midcom_core
     public function from_string($string, $replacer = '-')
     {
         // TODO: sanity-check $replacer ?
-        return $this->_convert_string($string, $replacer);
-    }
-
-    private function _convert_string($string, $replacer, $r = 0)
-    {
-        if ($r > 5)
-        {
-            debug_add('$r > 5, aborting', MIDCOM_LOG_ERROR);
-            return $string;
-        }
-        if (empty($string))
-        {
-            debug_add('$string was empty(), aborting', MIDCOM_LOG_WARN);
-            return '';
-        }
-
-        // Try to transliterate non-latin strings to URL-safe format
-        $string = midgardmvc_helper_urlize::string($string, $replacer);
-
-        /**
-         * Quick and dirty workaround for http://trac.midgard-project.org/ticket/1530 by recursing
-         */
-        // Recurse until we make no changes to the string
-        if ($string === midgardmvc_helper_urlize::string($string, $replacer))
-        {
-            return $string;
-        }
-
-        return $this->_convert_string($string, $replacer, $r + 1);
+        return midgardmvc_helper_urlize::string($string, $replacer);
     }
 }
 ?>
