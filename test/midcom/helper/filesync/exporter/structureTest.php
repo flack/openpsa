e087a76fe849e2dea5f557f5b85dff4a56dd7b5b
Andreas Flack <flack@contentcontrol-berlin.de>
2013-02-21 11:23:28 +0100

    Add basic unittest

---
+++ test/midcom/helper/filesync/exporter/structureTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+if (!defined('OPENPSA_TEST_ROOT'))
+{
+    define('OPENPSA_TEST_ROOT', dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DIRECTORY_SEPARATOR);
+    require_once OPENPSA_TEST_ROOT . 'rootfile.php';
+}
+require_once OPENPSA_TEST_ROOT . 'midcom/helper/filesync/__files/fs_setup.php';
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class midcom_helper_filesync_exporter_structureTest extends openpsa_testcase
+{
+    protected static $_rootdir;
+
+    public static function setUpBeforeClass()
+    {
+        self::$_rootdir = openpsa_test_fs_setup::get_exportdir('structure');
+    }
+
+    public function test_read_structure()
+    {
+        $topic_name = 'structure_' . __CLASS__ . __FUNCTION__ . microtime(true);
+        $export_name = 'export';
+
+        $topic = $this->create_object('midcom_db_topic', array('name' => $topic_name));
+        $sub_topic = $this->create_object('midcom_db_topic', array('name' => $topic_name, 'up' => $topic->id));
+
+        $exporter = new midcom_helper_filesync_exporter_structure(self::$_rootdir);
+        midcom::get('auth')->request_sudo('midcom.helper.filesync');
+        $stat = $exporter->read_structure($topic, $export_name);
+        midcom::get('auth')->drop_sudo();
+
+        $this->assertFileExists(self::$_rootdir . $export_name . '.inc');
+        $structure = file_get_contents(self::$_rootdir . $export_name . '.inc');
+        $structure_array = eval('return array(' . $structure . ');');
+
+        $expected = array
+        (
+            'export' => array
+            (
+                'name' => 'export',
+                'title' => 'localhost',
+                'root' => array
+                (
+                    'name' => $topic_name,
+                    'title' => '',
+                    'component' => '',
+                    'style' => '',
+                    'style_inherit' => false,
+                    'parameters' => array(),
+                    'acl' => array(),
+                    'nodes' => array
+                    (
+                        $topic_name => array
+                        (
+                            'name' => $topic_name,
+                            'title' => '',
+                            'component' => '',
+                            'style' => '',
+                            'style_inherit' => false,
+                            'parameters' => array(),
+                            'acl' => array(),
+                            'nodes' => array(),
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $this->assertEquals($expected, $structure_array);
+    }
+}
+?>
