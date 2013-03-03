3547df80936a2763f4f23401f5dfdd712ea01d6d
flack <flack@contentcontrol-berlin.de>
2013-03-03 12:29:11 +0100

    Port to work with codemirror

--- lib/midgard/admin/asgard/style/midgard_admin_asgard_stylehelper_element.php
+++ lib/midgard/admin/asgard/style/midgard_admin_asgard_stylehelper_element.php
@@ -12,7 +12,7 @@ $prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
         if ($data['handler_id'] == '____mfa-asgard-object_edit')
         {
             ?>
-            <button type="button" class="copy" onclick="if (editor) { editor.setValue(this.parentNode.getElementsByTagName('textarea')[0].innerHTML); } else { document.getElementById('net_nehmer_static_value').innerHTML=this.parentNode.getElementsByTagName('textarea')[0].innerHTML;}">
+            <button type="button" class="copy">
                 <?php echo $data['l10n']->get('copy default value'); ?>
             </button>
             <?php
@@ -25,6 +25,22 @@ $prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
 // <![CDATA[
 $(document).ready(function(){
     $(".midgard_admin_asgard_stylehelper_help").accordion({ header: 'h3', active: false, collapsible: true });
+
+    $('.midgard_admin_asgard_stylehelper_help button.copy').on('click', function()
+    {
+        var field_id = $('.object_edit form.datamanager2').attr('id') + '_value',
+            inserttext = $(this).parent().find('textarea').text();
+
+        if (typeof editors[field_id] !== 'undefined')
+        {
+            editors[field_id].setValue(inserttext);
+        }
+        else
+        {
+            $('#' + field_id).val(inserttext);
+        }
+    });
+
 });
 // ]]>
 </script>
