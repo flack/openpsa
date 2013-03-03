48c2180be927c1b9c0cd459f069c990e1a059d38
flack <flack@contentcontrol-berlin.de>
2013-03-03 12:02:17 +0100

    Make sure namepicker works regardless of root topic component

--- lib/midgard/admin/asgard/style/midgard_admin_asgard_stylehelper_elementnames.php
+++ lib/midgard/admin/asgard/style/midgard_admin_asgard_stylehelper_elementnames.php
@@ -26,7 +26,7 @@ $prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
                 if ($data['handler_id'] == '____mfa-asgard-object_create')
                 {
                     // We're creating an element, on clicking a name we should input it to the form
-                    echo "<a onclick=\"jQuery('#net_nehmer_static_name').val('{$name}');\">";
+                    echo '<a class="namepicker">';
                 }
                 else
                 {
@@ -47,6 +47,12 @@ $prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
 // <![CDATA[
 $(document).ready(function(){
         $(".midgard_admin_asgard_stylehelper_help").accordion({ header: 'h3', active: false, collapsible: true });
+
+        $('.midgard_admin_asgard_stylehelper_help a.namepicker').on('click', function()
+        {
+            var form_id = $('.object_edit form.datamanager2').attr('id');
+            $('#' + form_id + '_name').val($(this).text());
+        });
 });
 // ]]>
 </script>
