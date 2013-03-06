67b21fe423cfef9599e7a38b87e7624ee2530666
flack <flack@contentcontrol-berlin.de>
2013-03-06 21:07:43 +0100

    Try to ensure we actually have a unique title

--- test/net/nemein/wiki/wikipageTest.php
+++ test/net/nemein/wiki/wikipageTest.php
@@ -16,19 +16,19 @@ class net_nemein_wiki_wikipageTest extends openpsa_testcase
     public function testCRUD()
     {
         midcom::get('auth')->request_sudo('net.nemein.wiki');
-
+        $timestamp = time();
         $page = new net_nemein_wiki_wikipage();
 
         $topic = $this->get_component_node('net.nemein.wiki');
         $page->topic = $topic->id;
-        $page->title = 'TEST TITLE';
+        $page->title = 'TEST TITLE' . $timestamp;
 
         $stat = $page->create();
         $this->assertTrue($stat, midcom_connection::get_error_string());
 
         $this->register_object($page);
         $page->refresh();
-        $this->assertEquals('test-title', $page->name);
+        $this->assertEquals('test-title' . $timestamp, $page->name);
 
         $page->title = 'Test Title 2';
         $stat = $page->update();
