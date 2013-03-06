bed18bce5882eb913d919d33e53da87cd2807500
flack <flack@contentcontrol-berlin.de>
2013-03-07 00:11:58 +0100

    Convert to midcom before further processing

--- lib/midgard/admin/asgard/handler/object/manage.php
+++ lib/midgard/admin/asgard/handler/object/manage.php
@@ -783,7 +783,7 @@ class midgard_admin_asgard_handler_object_manage extends midcom_baseclasses_comp
         {
             midcom::get('uimessages')->add($this->_l10n->get('midgard.admin.asgard'), $this->_l10n->get('copy successful, you have been relocated to the new object'));
         }
-        return $new_object;
+        return midcom::get('dbfactory')->convert_midgard_to_midcom($new_object);
     }
 
     /**
