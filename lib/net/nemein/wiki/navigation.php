75d6f415ad70455ca66f4647206fe8e62cd3e61a
Andreas Flack <flack@contentcontrol-berlin.de>
2013-03-08 12:12:39 +0100

    remove leave getter altogether, because it turns out that it isn't supposed to find anything...

--- lib/net/nemein/wiki/navigation.php
+++ lib/net/nemein/wiki/navigation.php
@@ -13,47 +13,5 @@
  */
 class net_nemein_wiki_navigation  extends midcom_baseclasses_components_navigation
 {
-    /**
-     * Get the leaves set to be displayed in navigation
-     *
-     * @return array
-     */
-    public function get_leaves()
-    {
-        $leaves = array();
-
-        // Get the required information with midgard_collector
-        $mc = net_nemein_wiki_wikipage::new_collector('topic', $this->_topic->id);
-        $mc->add_value_property('id');
-        $mc->add_value_property('name');
-        $mc->add_value_property('title');
-        // Set the order of navigation
-        $mc->add_order('metadata.score', 'DESC');
-        $mc->add_order('title');
-        $mc->add_order('name');
-        $guids = $mc->list_keys();
-
-        // Return an empty result set
-        if (count($guids) === 0)
-        {
-            return $leaves;
-        }
-
-        // Get the leaves
-        foreach ($guids as $guid => $empty)
-        {
-            $values = $mc->get($guid);
-            $leaves[$values['id']] = array
-            (
-                MIDCOM_NAV_URL => $values['name'],
-                MIDCOM_NAV_NAME => ($values['title']) ? $values['title'] : $values['name'],
-                MIDCOM_NAV_GUID => $guid,
-                MIDCOM_NAV_OBJECT => new midcom_core_dbaproxy($guid, 'net_nemein_wiki_wikipage'),
-            );
-        }
-
-        // Finally return the leaves
-        return $leaves;
-    }
 }
 ?>
