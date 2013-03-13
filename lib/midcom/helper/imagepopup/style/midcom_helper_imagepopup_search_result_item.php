6b15dd771e4321ed795bc88f3cb648f26d6b0196
flack <flack@contentcontrol-berlin.de>
2013-03-13 15:01:47 +0100

    Small cleanup

--- lib/midcom/helper/imagepopup/style/midcom_helper_imagepopup_search_result_item.php
+++ lib/midcom/helper/imagepopup/style/midcom_helper_imagepopup_search_result_item.php
@@ -3,23 +3,20 @@ $prefix = midcom_connection::get_url('self');
 $item =& $data['result'];
 $mime_icon = null;
 $item_type = "image";
+
+switch ($item->mimetype)
+{
+    case 'image/x-png':
+    case 'image/png':
+    case 'image/jpeg':
+    case 'image/pjpeg':
+    case 'image/gif':
+        break;
+    default:
+        $item_type = "attachment";
+        $mime_icon = midcom_helper_misc::get_mime_icon($item->mimetype);
+}
 ?>
-    <?php
-        switch ($item->mimetype)
-        {
-            case 'image/x-png':
-            case 'image/png':
-            case 'image/jpeg':
-            case 'image/pjpeg':
-            case 'image/gif':
-                break;
-            default:
-                $item_type = "attachment";
-                $mime_icon = midcom_helper_misc::get_mime_icon($item->mimetype);
-    ?>
-    <?php
-        }
-    ?>
 
 <li title="&(item.guid);" class="midcom_helper_imagepopup_search_result_item" rel="&(item_type);">
 <?php
