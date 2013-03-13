76b3f1cce0f775410fb85d9b0b966117e80483b3
flack <flack@contentcontrol-berlin.de>
2013-03-13 23:47:00 +0100

    Don't call nonstatic method statically

--- lib/net/nehmer/blog/viewer.php
+++ lib/net/nehmer/blog/viewer.php
@@ -47,8 +47,7 @@ class net_nehmer_blog_viewer extends midcom_baseclasses_components_request
 
         if ($this->_config->get('rss_subscription_enable'))
         {
-            midcom::get('componentloader')->load_library('net.nemein.rss');
-            $rss_switches = net_nemein_rss_manage::get_plugin_handlers();
+            $rss_switches = midcom_baseclasses_components_configuration::get('net.nemein.rss', 'routes');
             $this->_request_switch = array_merge($this->_request_switch, $rss_switches);
         }
     }
