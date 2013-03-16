80c978536ad320e030dc1be8c25648cae1729c7f
flack <flack@contentcontrol-berlin.de>
2013-03-16 11:40:29 +0100

    Make test conform to documented behavior

--- test/midcom/helper/datamanager2/schemaTest.php
+++ test/midcom/helper/datamanager2/schemaTest.php
@@ -20,13 +20,13 @@ class midcom_helper_datamanager2_schemaTest extends openpsa_testcase
         $this->assertTrue(is_array($db));
         $this->assertEquals(1, sizeof($db));
         $this->assertTrue(array_key_exists('default', $db));
-        $this->assertTrue(is_a($db['default'], 'midcom_helper_datamanager2_schema'));
+        $this->assertInstanceOf('midcom_helper_datamanager2_schema', $db['default']);
         $this->assertEquals('default', $db['default']->name);
         $this->assertEquals('invoice', $db['default']->description);
         $this->assertEquals(array(), $db['default']->customdata);
         $this->assertEquals(array(), $db['default']->validation);
         $this->assertEquals(array(), $db['default']->filters);
-        $this->assertEquals('', $db['default']->l10n_schema);
+        $this->assertInstanceOf('midcom_services_i18n_l10n', $db['default']->l10n_schema);
 
         $db['default']->validation = array('TEST');
         $db = midcom_helper_datamanager2_schema::load_database($filename);
