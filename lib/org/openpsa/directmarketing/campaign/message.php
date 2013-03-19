8069e95958250e6ffa8a4c1bc7c2bba3e6149e46
flack <flack@contentcontrol-berlin.de>
2013-03-19 21:56:54 +0100

    Remove redundant local parent getters, the same functionality is already inherited

--- lib/org/openpsa/directmarketing/campaign/message.php
+++ lib/org/openpsa/directmarketing/campaign/message.php
@@ -24,41 +24,6 @@ class org_openpsa_directmarketing_campaign_message_dba extends midcom_core_dbaob
     const FAX = 8005;
     const EMAIL_HTML = 8006;
 
-    function get_parent_guid_uncached()
-    {
-        if (empty($this->campaign))
-        {
-            return null;
-        }
-        return self::get_parent_guid_uncached_static($this->guid, __CLASS__);
-    }
-
-    public static function get_parent_guid_uncached_static($guid, $classname)
-    {
-        if (empty($guid))
-        {
-            return null;
-        }
-
-        $mc = org_openpsa_directmarketing_campaign_message_dba::new_collector('guid', $guid);
-        $result = $mc->get_values('campaign');
-        if (empty($result))
-        {
-            // error
-            return null;
-        }
-        $campaign_id = array_shift($result);
-
-        $mc2 = org_openpsa_directmarketing_campaign_dba::new_collector('id', $campaign_id);
-        $result2 = $mc2->get_values('guid');
-        if (empty($result2))
-        {
-            // error
-            return null;
-        }
-        return array_shift($result2);
-    }
-
     public function _on_created()
     {
         if (!$this->orgOpenpsaObtype)
