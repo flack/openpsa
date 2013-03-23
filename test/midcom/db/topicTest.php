f7c4e113357751a2d0b8c6f3641b6f989f08bacb
flack <flack@contentcontrol-berlin.de>
2013-03-23 12:41:56 +0100

    Add basic smoketests

---
+++ test/midcom/db/topicTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class midcom_db_topicTest extends openpsa_testcase
+{
+    protected static $_parent;
+
+    public static function setUpBeforeClass()
+    {
+        self::$_parent = self::create_class_object('midcom_db_topic');
+    }
+
+    public function testCRUD()
+    {
+        midcom::get('auth')->request_sudo('midcom.core');
+
+        $topic = new midcom_db_topic();
+        $topic->topic = self::$_parent->id;
+        $stat = $topic->create();
+        $this->assertTrue($stat, midcom_connection::get_error_string());
+
+        $this->register_object($topic);
+
+        $topic->title = 'test';
+        $stat = $topic->update();
+        $this->assertTrue($stat);
+        $this->assertEquals('test', $topic->title);
+
+        $stat = $topic->delete();
+        $this->assertTrue($stat);
+
+        midcom::get('auth')->drop_sudo();
+    }
+
+    public function test_get_parent()
+    {
+        $topic1 = new midcom_db_topic;
+        $topic1->up = self::$_parent->id;
+        $parent1 = $topic1->get_parent();
+        $this->assertEquals($parent1->guid, self::$_parent->guid);
+        $this->assertEquals(get_class($parent1), get_class(self::$_parent));
+    }
+}
+?>
