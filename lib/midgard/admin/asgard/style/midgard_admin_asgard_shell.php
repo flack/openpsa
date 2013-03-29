14e99d2500f0b2aa1e0493d683e7e524e5879a0a
flack <flack@contentcontrol-berlin.de>
2013-03-29 01:59:24 +0100

    Handle script errors a bit more gracefully

--- lib/midgard/admin/asgard/style/midgard_admin_asgard_shell.php
+++ lib/midgard/admin/asgard/style/midgard_admin_asgard_shell.php
@@ -7,6 +7,16 @@ $data['controller']->display_form();
 if (!empty($data['code']))
 { ?>
     <h3><?php echo $data['l10n']->get('script output'); ?></h3>
-    <pre id="shell-output">&(data['code']:p);<pre>
+    <pre id="shell-output"><?php
+    try
+    {
+        ?>&(data['code']:p);<?php
+    }
+    catch (Exception $e)
+    {
+        echo '<div class="shell-error">' . $e->getMessage() . '</div>';
+        echo htmlentities($e->getTraceAsString());
+    }
+    ?><pre>
 <?php }
 ?>
