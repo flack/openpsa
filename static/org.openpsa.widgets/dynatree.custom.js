b72e83b87485b90c94124ba06c99937d065d6863
flack <flack@contentcontrol-berlin.de>
2013-04-07 12:10:15 +0200

    Add possibility to override config settings

--- static/org.openpsa.widgets/dynatree.custom.js
+++ static/org.openpsa.widgets/dynatree.custom.js
@@ -1,60 +1,62 @@
 var org_openpsa_tree =
 {
-    setup: function(identifier, prefix)
+    setup: function(identifier, prefix, options)
     {
-        $('#' + identifier)
-            .css('overflow', 'auto')
-            .dynatree(
+        var default_options =
+        {
+            minExpandLevel: 1,
+            persist: true,
+            cookie: {path: prefix},
+            cookieId: identifier,
+            clickFolderMode: 2,
+            autoCollapse: false,
+            debugLevel: -1,
+
+            onActivate: function(dtnode)
             {
-                minExpandLevel: 1,
-                persist: true,
-                cookie: {path: prefix},
-                cookieId: identifier,
-                clickFolderMode: 2,
-                autoCollapse: false,
-                debugLevel: -1,
-    
-                onActivate: function(dtnode)
+                if (typeof dtnode.data.href !== 'undefined')
                 {
-                    if (typeof dtnode.data.href !== 'undefined')
-                    {
-                        window.location.href = dtnode.data.href;
-                    }
-                },
-                onClick: function(dtnode, event)
+                    window.location.href = dtnode.data.href;
+                }
+            },
+            onClick: function(dtnode, event)
+            {
+                if (   typeof dtnode.tree.activeNode !== 'undefined'
+                    && dtnode.tree.activeNode === dtnode)
                 {
-                    if (   typeof dtnode.tree.activeNode !== 'undefined'
-                        && dtnode.tree.activeNode === dtnode)
-                    {
-                        dtnode.deactivate();
-                    }
-                    return true;
-                },
-                onCustomRender: function(dtnode)
-                {
-                    var url = '#',
-                    tooltip = dtnode.data.tooltip ? " title='" + dtnode.data.tooltip + "'" : "";
+                    dtnode.deactivate();
+                }
+                return true;
+            },
+            onCustomRender: function(dtnode)
+            {
+                var url = '#',
+                tooltip = dtnode.data.tooltip ? " title='" + dtnode.data.tooltip + "'" : "";
 
-                    if (typeof dtnode.data.href !== 'undefined')
-                    {
-                        url = dtnode.data.href;
-                    }
-                    return '<a href="' + url + '" class="' + dtnode.tree.options.classNames.title + '"' + tooltip + '>' + dtnode.data.title + '</a>';
-
-                },
-                onPostInit: function(isReloading, isError)
+                if (typeof dtnode.data.href !== 'undefined')
+                {
+                    url = dtnode.data.href;
+                }
+                return '<a href="' + url + '" class="' + dtnode.tree.options.classNames.title + '"' + tooltip + '>' + dtnode.data.title + '</a>';
+            },
+            onPostInit: function(isReloading, isError)
+            {
+                org_openpsa_tree.crop_height($('#' + identifier));
+            },
+            onExpand: function(flag, dtnode)
+            {
+                if (flag === true)
                 {
                     org_openpsa_tree.crop_height($('#' + identifier));
-                },
-                onExpand: function(flag, dtnode)
-                {
-                    if (flag === true)
-                    {
-                        org_openpsa_tree.crop_height($('#' + identifier));
-                    }
                 }
-            })
-            .find('.dynatree-container').css('overflow', 'visible');
+            }
+        };
+
+        options = $.extend({}, default_options, options || {});
+
+        $('#' + identifier)
+            .css('overflow', 'auto')
+            .dynatree(options);
         $(window).bind('resize', function()
         {
             org_openpsa_tree.crop_height($('#' + identifier));
@@ -65,12 +67,12 @@ var org_openpsa_tree =
         var content_height = 0,
         tree_content_height = tree.find('.dynatree-container').outerHeight(true),
         container_height = $('#content-text').height();
-        
+
         tree.closest('#content-text > *').children(':visible').each(function()
         {
             content_height += $(this).outerHeight(true);
         });
-        
+
         var available_height = container_height - (content_height - tree_content_height);
 
         if (   available_height > tree_content_height
@@ -78,7 +80,7 @@ var org_openpsa_tree =
         {
             return;
         }
-        
+
         tree.height(container_height - (content_height - tree.height()));
     }
 };
