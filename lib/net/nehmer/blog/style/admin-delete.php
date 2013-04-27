b97a1d10636bd3fd44e685c619d3847c7e91ee37
flack <flack@contentcontrol-berlin.de>
2013-04-27 09:11:46 +0200

    Use correct style element name

--- lib/net/nehmer/blog/style/admin-delete.php
+++ lib/net/nehmer/blog/style/admin-delete.php
@@ -11,4 +11,4 @@ $data['view_article'] =& $dn_data;
   <input type="submit" name="net_nehmer_blog_deletecancel" value="<?php echo $data['l10n_midcom']->get('cancel'); ?>" />
 </form>
 
-<?php midcom_show_style('show-article'); ?>
+<?php midcom_show_style('view'); ?>
