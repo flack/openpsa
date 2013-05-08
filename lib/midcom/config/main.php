0992e8b55d984cce3d06a9de2d017d8fd02e9201
Andreas Flack <flack@contentcontrol-berlin.de>
2013-05-08 18:31:05 +0200

    Use jquery 2.0 in supported browsers

--- lib/midcom/config/main.php
+++ lib/midcom/config/main.php
@@ -482,7 +482,8 @@ class midcom_config implements arrayaccess
         'wrap_style_show_with_name' => false,
 
         // Related to JavaScript libraries
-        'jquery_version' => '1.9.1.min',
+        'jquery_version' => '2.0.0.min',
+        'jquery_version_oldie' => '1.9.1.min',
         'jquery_ui_version' => '1.10.2',
         'jquery_ui_theme' => null,
         'jquery_load_from_google' => false,
