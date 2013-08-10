ae1e6e7624a5344aeb6bfc1b21a7cb39439d9f5d
flack <flack@contentcontrol-berlin.de>
2013-08-10 19:53:24 +0200

    Remove psr-0 support from autoloader, this is handled by composer anyways

--- lib/midcom.php
+++ lib/midcom.php
@@ -150,26 +150,6 @@ class midcom
     {
         static $autoloaded = 0;
 
-        //PSR-0 part
-        $class_name = ltrim($class_name, '\\');
-        if ($last_ns_pos = strripos($class_name, '\\'))
-        {
-            $basedir = MIDCOM_ROOT . '/../vendor/';
-            $namespace = substr($class_name, 0, $last_ns_pos);
-            $class_name = substr($class_name, $last_ns_pos + 1);
-            $file_name  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-            $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
-
-            if (!file_exists($basedir . $file_name))
-            {
-                return false;
-            }
-            require $basedir . $file_name;
-            $autoloaded++;
-            return true;
-        }
-
-        //MidCOM "Classic"
         if (preg_match('/_dba?$/', $class_name))
         {
             // DBA object files are named objectname.php
