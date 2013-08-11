eb102f35809d69ffb2fa374ef7d59cbd9599874c
flack <flack@contentcontrol-berlin.de>
2013-08-11 11:25:57 +0200

    bump version

--- lib/midcom.php
+++ lib/midcom.php
@@ -16,7 +16,7 @@ class midcom
      *
      * @var string
      */
-    private static $_version = '9.0beta5';
+    private static $_version = '9.0beta5+git';
 
     /**
      * Main application singleton
