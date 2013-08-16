19c9942238d1d1875491ba376e6d4637c0601996
flack <flack@contentcontrol-berlin.de>
2013-08-16 21:31:11 +0200

    Add basic handler test

---
+++ test/org/openpsa/calendar/handler/agendaTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_calendar_handler_agendaTest extends openpsa_testcase
+{
+    public function testHandler_day()
+    {
+        $this->create_user(true);
+        midcom::get('auth')->request_sudo('org.openpsa.calendar');
+
+        $attributes = array
+        (
+            'start' => 1144056938,
+            'end' => 1144066938
+        );
+        $event = $this->create_object('org_openpsa_calendar_event_dba', $attributes);
+
+        $data = $this->run_handler('org.openpsa.calendar', array('agenda', 'day', '2006-04-03'));
+        $this->assertEquals('agenda_day', $data['handler_id']);
+
+        $this->show_handler($data);
+
+        midcom::get('auth')->drop_sudo();
+    }
+}
+?>
