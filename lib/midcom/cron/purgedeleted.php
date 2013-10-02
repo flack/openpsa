b8aaab7102ca7eb413be6955311b56ecabdcbd94
Andreas Flack <flack@contentcontrol-berlin.de>
2013-10-02 18:44:18 +0200

    remove some duplication

--- lib/midcom/cron/purgedeleted.php
+++ lib/midcom/cron/purgedeleted.php
@@ -11,10 +11,43 @@
  */
 class midcom_cron_purgedeleted extends midcom_baseclasses_components_cron_handler
 {
+    private $_cutoff;
+
+    public $quiet = true;
+
+    public function set_cutoff($days)
+    {
+        $this->_cutoff = mktime(23, 59, 59, date('n'), date('j') - $days, date('Y'));
+    }
+
+    public function get_cutoff()
+    {
+        if (empty($this->_cutoff))
+        {
+            $this->set_cutoff(midcom::get('config')->get('cron_purge_deleted_after'));
+        }
+        return $this->_cutoff;
+    }
+
+    private function _log($message, $level = MIDCOM_LOG_DEBUG)
+    {
+        debug_add(trim($message, " \n"), $level);
+
+        if (!$this->quiet)
+        {
+            if ($level == MIDCOM_LOG_ERROR)
+            {
+                $message = 'ERROR: ' . $message;
+            }
+            echo $message . "\n";
+            flush();
+        }
+    }
+
     public function _on_execute()
     {
-        debug_add('called!');
-        $cut_off = mktime(23, 59, 59, date('n'), date('j') - midcom::get('config')->get('cron_purge_deleted_after'), date('Y'));
+        $cut_off = $this->get_cutoff();
+        $this->_log('Purging entries deleted before ' . gmdate('Y-m-d H:i:s', $cut_off) . "\n");
         foreach (midcom_connection::get_schema_types() as $mgdschema)
         {
             if (substr($mgdschema, 0, 2) == '__')
@@ -26,25 +59,20 @@ class midcom_cron_purgedeleted extends midcom_baseclasses_components_cron_handle
             {
                 continue;
             }
-            debug_add("Processing class {$mgdschema}");
+            $this->_log("Processing class {$mgdschema}");
             $qb = new midgard_query_builder($mgdschema);
             $qb->add_constraint('metadata.deleted', '<>', 0);
             $qb->add_constraint('metadata.revised', '<', gmdate('Y-m-d H:i:s', $cut_off));
             $qb->include_deleted();
             $qb->set_limit(500);
             $objects = $qb->execute();
-            if (!is_array($objects))
-            {
-                debug_add("QB failed fatally on class {$mgdschema}, errstr: " . midcom_connection::get_error_string(), MIDCOM_LOG_ERROR);
-                continue;
-            }
             $found = count($objects);
             $purged = 0;
             foreach ($objects as $obj)
             {
                 if (!$obj->purge())
                 {
-                    debug_add("Failed to purge {$mgdschema} {$obj->guid}, deleted: {$obj->metadata->deleted},  revised: {$obj->metadata->revised}. errstr: " . midcom_connection::get_error_string(), MIDCOM_LOG_ERROR);
+                    $this->_log("Failed to purge {$mgdschema} {$obj->guid}, deleted: {$obj->metadata->deleted},  revised: {$obj->metadata->revised}. errstr: " . midcom_connection::get_error_string(), MIDCOM_LOG_ERROR);
                     debug_print_r('Failed object', $obj);
                     continue;
                 }
@@ -52,11 +80,11 @@ class midcom_cron_purgedeleted extends midcom_baseclasses_components_cron_handle
             }
             if ($found > 0)
             {
-                debug_add("Found {$found} {$mgdschema} objects deleted before " . date('Y-m-d H:i:s', $cut_off) . ", purged {$purged}", MIDCOM_LOG_INFO);
+                $this->_log("  Found {$found} deleted {$mgdschema} objects, purged {$purged}\n", MIDCOM_LOG_INFO);
             }
             else
             {
-                debug_add("No {$mgdschema} objects deleted before " . date('Y-m-d H:i:s', $cut_off) . " found");
+                $this->_log("  No {$mgdschema} objects deleted before cutoff date found\n");
             }
         }
     }
