b0d90de1a25f7132c115c0075fbb298a09687c5a
Andreas Flack <flack@contentcontrol-berlin.de>
2014-03-31 11:54:21 +0200

    Fix wrong function call

--- lib/net/nehmer/blog/navigation.php
+++ lib/net/nehmer/blog/navigation.php
@@ -98,12 +98,23 @@ class net_nehmer_blog_navigation extends midcom_baseclasses_components_navigatio
         {
             $leaves[$article->id] = array
             (
-                MIDCOM_NAV_URL => $this->_master->get_url($article),
+                MIDCOM_NAV_URL => $this->_get_url($article),
                 MIDCOM_NAV_NAME => $article->title ?: $article->name,
                 MIDCOM_NAV_GUID => $article->guid,
                 MIDCOM_NAV_OBJECT => $article,
             );
         }
+    }
+
+    private function _get_url(midcom_db_article $article)
+    {
+        $view_url = $article->name ?: $article->guid;
+
+        if ($this->_config->get('view_in_url'))
+        {
+            $view_url = 'view/' . $view_url;
+        }
+        return $view_url . '/';
     }
 
     private function _add_pseudo_leaves(array &$leaves)
