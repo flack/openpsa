0d54ebdf3e57cdb338c0df9507243eb46e8963cd
flack <flack@contentcontrol-berlin.de>
2014-05-14 00:02:57 +0200

    Install pake via composer, since pyrus doesn't seem to be working anymorw

--- test/travis_midgard2.sh
+++ test/travis_midgard2.sh
@@ -8,9 +8,7 @@ sudo apt-get install rcs
 if [ "$MIDGARD_EXT_VERSION" != "portable" ]
 then
     # Install Pake
-    pyrus channel-discover pear.indeyets.ru
-    pyrus channel-discover pear.symfony-project.com
-    pyrus install -f http://pear.indeyets.ru/get/pake-1.7.2.tgz
+    ${COMPOSER} install indeyets/pake
 
     # Install Midgard2 library dependencies
     sudo apt-get install -y dbus libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev
@@ -24,7 +22,7 @@ then
     # Build and install Midgard2 PHP extension
     wget -q https://github.com/midgardproject/midgard-php5/tarball/${MIDGARD_EXT_VERSION}
     tar zxf ${MIDGARD_EXT_VERSION}
-    sh -c "cd midgardproject-midgard-php5-*&&php `pyrus get php_dir|tail -1`/pake.php install"
+    sh -c "cd midgardproject-midgard-php5-*&& ../pake/bin/pake install"
     echo "extension=midgard2.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 else
     ${COMPOSER} require openpsa/midgard-portable:dev-master
