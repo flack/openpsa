bb931b5072788afb4943b2fc53ac3e1cc7ffa78e
flack <flack@contentcontrol-berlin.de>
2014-05-18 17:09:09 +0200

    Remove unused list handler

--- lib/org/openpsa/projects/style/show-project-list-status-summary.php
+++
@@ -1,9 +0,0 @@
-<?php
-$prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
-?>
-<div class="area">
-    <?php echo "<h2><a href=\"{$prefix}project/list/{$data['project_list_status']}/\">" . sprintf($data['l10n']->get('%s projects'), $data['l10n']->get($data['project_list_status'])) . "</a></h2>\n"; ?>
-    <?php
-    echo sprintf($data['l10n']->get('%d %s projects'), count($data['project_list_items']), $data['l10n']->get($data['project_list_status']));
-    ?>
-</div>
