04038eb60fbe59ed32eb2af2f117b2fc6a391622
flack <flack@contentcontrol-berlin.de>
2014-08-10 20:42:47 +0200

    Add file missing from last commit

---
+++ lib/org/openpsa/reports/style/sales_report-deliverable-grid.php
@@ -0,0 +1,59 @@
+<?php
+$sales_l10n = midcom::get('i18n')->get_l10n('org.openpsa.sales');
+$invoices_l10n = midcom::get('i18n')->get_l10n('org.openpsa.invoices');
+$grid = $data['grid'];
+$grid_id = $grid->get_identifier();
+$footer_data = array('invoice' => $data['l10n']->get('totals'));
+$grid->set_option('loadonce', true)
+->set_option('grouping', true)
+->set_option('groupingView', array
+(
+    'groupField' => array('salesproject'),
+    'groupColumnShow' => array(false),
+    'groupText' => array('<strong>{0}</strong> ({1})'),
+    'groupOrder' => array('asc'),
+    'groupSummary' => array(true),
+    'showSummaryOnHide' => true
+));
+
+$grid->set_column('invoice', $invoices_l10n->get('invoice number'), 'align: "center", fixed: true', 'integer');
+if ($data['handler_id'] != 'deliverable_report')
+{
+    $grid->set_column('owner', $sales_l10n->get('owner'), '', 'string');
+}
+$grid->set_column('customer', $sales_l10n->get('customer'));
+$grid->set_column('salesproject', $sales_l10n->get('salesproject'));
+$grid->set_column('product', $sales_l10n->get('product'));
+$grid->set_column('item', $invoices_l10n->get('invoice items'));
+$grid->set_column('amount', $invoices_l10n->get('sum'), 'width: 70, align: "right", sorttype: "number", formatter: "number", summaryType: "sum", fixed: true');
+
+$grid->set_footer_data($footer_data);
+?>
+<div class="grid-filters">
+<?php
+echo ' ' . midcom::get()->i18n->get_string('group by', 'org.openpsa.core') . ': ';
+echo '<select id="chgrouping_' . $grid_id . '">';
+if ($data['handler_id'] != 'deliverable_report')
+{
+    echo '<option value="owner">' . $sales_l10n->get('owner') . "</option>\n";
+}
+
+echo '<option value="customer">' . $sales_l10n->get('customer') . "</option>\n";
+echo '<option value="salesproject">' . $sales_l10n->get('salesproject') . "</option>\n";
+echo '<option value="product">' . $sales_l10n->get('product') . "</option>\n";
+echo '<option value="clear">' . midcom::get()->i18n->get_string('no grouping', 'org.openpsa.core') . "</option>\n";
+echo '</select>';
+?>
+</div>
+
+<div class="org_openpsa_reports full-width fill-height">
+
+<?php $grid->render(); ?>
+
+</div>
+
+<script type="text/javascript">
+org_openpsa_grid_helper.bind_grouping_switch('&(grid_id);');
+
+org_openpsa_grid_footer.set_field('&(grid_id);', 'amount', 'sum');
+</script>
