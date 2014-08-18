ea18bd6c96ca89d5eecb5d8f99d15a96b7974023
Andreas Flack <flack@contentcontrol-berlin.de>
2014-08-18 15:12:26 +0200

    Make sure deliverable contains data usable for calculations

--- lib/org/openpsa/sales/validator.php
+++ lib/org/openpsa/sales/validator.php
@@ -15,12 +15,24 @@ class org_openpsa_sales_validator
 {
     public function validate_subscription(array $fields)
     {
-        // check channels
         if (   (   empty($fields['end_date'])
                 || $fields['end_date'] == '0000-00-00')
             && empty($fields['continuous']))
         {
             $result['end'] = midcom::get()->i18n->get_string('select either end date or continuous', 'org.openpsa.sales');
+            return $result;
+        }
+
+        return true;
+    }
+
+    public function validate_single_delivery(array $fields)
+    {
+        // check channels
+        if (   empty($fields['invoiceByActualUnits'])
+            && empty($fields['plannedUnits']))
+        {
+            $result['plannedUnits'] = midcom::get()->i18n->get_string('select either planned units or invoice by actual units', 'org.openpsa.sales');
             return $result;
         }
 
