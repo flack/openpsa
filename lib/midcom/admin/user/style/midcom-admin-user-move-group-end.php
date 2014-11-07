68e2a95680acda9e56910fb86e0b725225ec79fc
Andreas Flack <flack@contentcontrol-berlin.de>
2014-11-07 11:16:10 +0100

    Move elements to correct component

---
+++ lib/midcom/admin/user/style/midcom-admin-user-move-group-end.php
@@ -0,0 +1,6 @@
+    </div>
+    <div class="form_toolbar">
+        <input type="submit" name="f_submit" value="<?php echo $data['l10n_midcom']->get('save'); ?>" class="save" />
+        <input type="submit" name="f_cancel" value="<?php echo $data['l10n_midcom']->get('cancel'); ?>" class="cancel" />
+    </div>
+</form>
---
+++ lib/midcom/admin/user/style/midcom-admin-user-move-group-start.php
@@ -0,0 +1,2 @@
+<form method="post" action="<?php echo midcom_connection::get_url('uri'); ?>" class="datamanager datamanager2">
+    <div class="form">
