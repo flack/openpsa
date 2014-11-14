93c5ee422e20dd39e00baec762124e7d2a7846f0
Andreas Flack <flack@contentcontrol-berlin.de>
2014-11-14 11:24:07 +0100

    Fix refactoring oversight

---
+++ test/org/openpsa/sales/handler/deliverable/listTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_sales_salesproject_deliverable_listTest extends openpsa_testcase
+{
+    protected static $_person;
+    protected static $_salesproject;
+
+    public static function setUpBeforeClass()
+    {
+        self::$_person = self::create_user(true);
+    }
+
+    public function testHandler_list()
+    {
+        midcom::get()->auth->request_sudo('org.openpsa.sales');
+        $product_group = self::create_class_object('org_openpsa_products_product_group_dba');
+        $product_attributes = array
+        (
+            'productGroup' => $product_group->id,
+            'name' => 'TEST_' . __CLASS__ . '_' . time(),
+        );
+        $product = self::create_class_object('org_openpsa_products_product_dba', $product_attributes);
+
+        $data = $this->run_handler('org.openpsa.sales', array('deliverable', 'list', 'product', $product->guid));
+        $this->assertEquals('deliverable_list_product', $data['handler_id']);
+
+        midcom::get()->auth->drop_sudo();
+    }
+}
