1338e8c3aedb1fc4352e6a7be9d8dd6891f852bf
flack <flack@contentcontrol-berlin.de>
2014-11-22 12:45:01 +0100

    improve grid usability

--- lib/org/openpsa/sales/style/show-deliverable-grid.php
+++ lib/org/openpsa/sales/style/show-deliverable-grid.php
@@ -14,18 +14,18 @@ if ($data['product']->delivery == org_openpsa_products_product_dba::DELIVERY_SUB
 $grid->set_column('type', midcom::get()->i18n->get_string('type', 'midgard.admin.asgard'), 'width: 100');
 $grid->set_column('pricePerUnit', $data['l10n']->get('price per unit'), 'width: 50, classes: "numeric", sorttype: "number", formatter: "number"');
 $grid->set_column('units', $data['l10n']->get('units'), 'width: 40, classes: "numeric", sorttype: "number", formatter: "number"');
-$grid->set_column('invoiced', $data['l10n']->get('invoiced'), 'width: 50, classes: "numeric", sorttype: "number", formatter: "number"');
+$grid->set_column('invoiced', $data['l10n']->get('invoiced'), 'width: 50, align: "right", sorttype: "number", formatter: "number", summaryType: "sum"');
 
 $grid->set_option('loadonce', true)
     ->set_option('grouping', true)
     ->set_option('groupingView', array
     (
-        'groupField' => array('salesproject'),
+        'groupField' => array('state'),
         'groupColumnShow' => array(false),
         'groupText' => array('<strong>{0}</strong> ({1})'),
         'groupOrder' => array('asc'),
-        'groupSummary' => array(false),
-        'showSummaryOnHide' => false
+        'groupSummary' => array(true),
+        'showSummaryOnHide' => true
     ));
 
 $grid->render();
