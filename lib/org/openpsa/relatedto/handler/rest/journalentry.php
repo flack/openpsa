3a43026aebc9dfe3030dac7f801181afcda34d60
flack <flack@contentcontrol-berlin.de>
2015-03-22 10:47:47 +0100

    Add rest api for journal entries

---
+++ lib/org/openpsa/relatedto/handler/rest/journalentry.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package org.openpsa.relatedto
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * rest entry handler
+ *
+ * @package org.openpsa.relatedto
+ */
+class org_openpsa_relatedto_handler_rest_journalentry extends midcom_baseclasses_components_handler_rest
+{
+    public function get_object_classname()
+    {
+        return "org_openpsa_relatedto_journal_entry_dba";
+    }
+}
