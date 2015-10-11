e1c8b0f3301fcb0114541a673b7082d9d7c095d2
flack <flack@contentcontrol-berlin.de>
2015-10-11 15:21:07 +0200

    Add some basic smoketests

---
+++ test/net/nemein/redirector/handler/tinyurlTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class net_nemein_redirector_handler_tinyurlTest extends openpsa_testcase
+{
+    protected static $_topic;
+
+    public static function setUpBeforeClass()
+    {
+        $topic_attributes = array
+        (
+            'component' => 'net.nemein.redirector',
+            'name' => __CLASS__ . time()
+        );
+        self::$_topic = self::create_class_object('midcom_db_topic', $topic_attributes);
+    }
+
+    public function testHandler_create()
+    {
+        midcom::get()->auth->request_sudo('net.nemein.redirector');
+
+        $data = $this->run_handler(self::$_topic, array('create'));
+        $this->assertEquals('create', $data['handler_id']);
+
+        $this->show_handler($data);
+        midcom::get()->auth->drop_sudo();
+    }
+
+    public function testHandler_edit()
+    {
+        midcom::get()->auth->request_sudo('net.nemein.redirector');
+        $attributes = array
+        (
+            'node' => self::$_topic->id,
+            'name' => net_nemein_redirector_tinyurl_dba::generate()
+        );
+        $tinyurl = $this->create_object('net_nemein_redirector_tinyurl_dba', $attributes);
+
+        $data = $this->run_handler(self::$_topic, array('edit', $tinyurl->name));
+        $this->assertEquals('edit', $data['handler_id']);
+
+        $this->show_handler($data);
+        midcom::get()->auth->drop_sudo();
+    }
+}
---
+++ test/net/nemein/redirector/tinyurlTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class net_nemein_redirector_tinyurlTest extends openpsa_testcase
+{
+    public function testCRUD()
+    {
+        midcom::get()->auth->request_sudo('net.nemein.redirector');
+        $tinyurl = new net_nemein_redirector_tinyurl_dba();
+        $tinyurl->_use_activitystream = false;
+        $tinyurl->_use_rcs = false;
+        $name = net_nemein_redirector_tinyurl_dba::generate();
+
+        $topic = $this->get_component_node('net.nemein.redirector');
+        $tinyurl->node = $topic->id;
+        $tinyurl->name = $name;
+
+        $stat = $tinyurl->create();
+        $this->assertTrue($stat, midcom_connection::get_error_string());
+
+        $this->register_object($tinyurl);
+        $tinyurl->refresh();
+        $this->assertEquals($name, $tinyurl->name);
+
+        $tinyurl2 = new net_nemein_redirector_tinyurl_dba();
+        $tinyurl2->node = $topic->id;
+        $tinyurl2->name = $name;
+        $this->assertFalse($tinyurl2->create());
+        
+        $name2 = net_nemein_redirector_tinyurl_dba::generate();
+        $tinyurl->name = $name2;
+        $stat = $tinyurl->update();
+        $this->assertTrue($stat);
+        $tinyurl->refresh();
+        $this->assertEquals($name2, $tinyurl->name);
+
+        $stat = $tinyurl->delete();
+        $this->assertTrue($stat);
+
+        midcom::get()->auth->drop_sudo();
+    }
+}
