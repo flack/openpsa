4a50f98d23174b4c5b7b3221c375416b8a0fbf8d
flack <flack@contentcontrol-berlin.de>
2015-10-17 12:54:18 +0200

    Update jquery-ui

--- lib/midcom/config/main.php
+++ lib/midcom/config/main.php
@@ -483,7 +483,7 @@ class midcom_config implements arrayaccess
         // Related to JavaScript libraries
         'jquery_version' => '2.1.4.min',
         'jquery_version_oldie' => '1.11.3.min',
-        'jquery_ui_version' => '1.11.2',
+        'jquery_ui_version' => '1.11.4',
         'jquery_ui_theme' => null,
         'jquery_load_from_google' => false,
         'enable_ajax_editing' => false,
