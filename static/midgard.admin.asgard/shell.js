a7c83931fb88ea7ee8449b43b802171cba0a1732
flack <flack@contentcontrol-berlin.de>
2015-10-17 15:18:10 +0200

    Use DM codemirror init instead of duplicating it

--- static/midgard.admin.asgard/shell.js
+++ static/midgard.admin.asgard/shell.js
@@ -1,49 +1,6 @@
-$(document).ready(function()
+$(window).load(function()
 {
-    var editor = CodeMirror.fromTextArea(document.getElementById(window.midgard_admin_asgard_shell_identifier), {
-        mode: "application/x-httpd-php",
-        lineNumbers: true,
-        theme: "eclipse",
-        lineWrapping: true,
-        matchBrackets: true,
-        indentUnit: 4,
-        indentWithTabs: false,
-        enterMode: "keep",
-        tabMode: "shift",
-        readOnly: false,
-        extraKeys: {
-            "F11": function() {
-                var scroller = editor.getScrollerElement();
-                if (scroller.className.search(/\bCodeMirror-fullscreen\b/) === -1) {
-                    scroller.className += " CodeMirror-fullscreen";
-                    scroller.style.height = "100%";
-                    scroller.style.width = "100%";
-                    editor.refresh();
-                } else {
-                    scroller.className = scroller.className.replace(" CodeMirror-fullscreen", "");
-                    scroller.style.height = '';
-                    scroller.style.width = '';
-                    editor.refresh();
-                }
-            },
-            "Esc": function() {
-                var scroller = editor.getScrollerElement();
-                if (scroller.className.search(/\bCodeMirror-fullscreen\b/) !== -1) {
-                    scroller.className = scroller.className.replace(" CodeMirror-fullscreen", "");
-                    scroller.style.height = '';
-                    scroller.style.width = '';
-                    editor.refresh();
-                }
-            }
-        }
-    });
-    var hlLine = editor.addLineClass(0, 'background', "activeline");
-
-    editor.on('cursorActivity', function(instance)
-    {
-        instance.removeLineClass(hlLine, 'background', 'activeline');
-        hlLine = instance.addLineClass(instance.getCursor().line, 'background', "activeline");
-    });
+    var editor = window.editors[window.midgard_admin_asgard_shell_identifier];
 
     var storage_available = (typeof window.localStorage !== 'undefined' && window.localStorage)
     if (storage_available)
