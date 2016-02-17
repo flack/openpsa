f0d1854bce3d178cd67197018d53628fb50ff71e
flack <flack@contentcontrol-berlin.de>
2016-02-17 22:59:29 +0100

    merge view and connector handler since they both deal with the same thing anyway

--- lib/org/openpsa/documents/handler/finder.php
+++ lib/org/openpsa/documents/handler/finder.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * @package org.openpsa.documents
- * @author Nemein Oy http://www.nemein.com/
- * @copyright Nemein Oy http://www.nemein.com/
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
  */
 
 /**
- * org.openpsa.documents document handler and viewer class.
+ * org.openpsa.documents elfinder controller.
  *
  * @package org.openpsa.documents
  */
-class org_openpsa_documents_handler_directory_view extends midcom_baseclasses_components_handler
+class org_openpsa_documents_handler_finder extends midcom_baseclasses_components_handler
 {
     public function _on_initialize()
     {
@@ -128,5 +128,45 @@ class org_openpsa_documents_handler_directory_view extends midcom_baseclasses_co
     public function _show_view($handler_id, array &$data)
     {
         midcom_show_style('show-directory');
+    }
+
+    /**
+     * @param mixed $handler_id The ID of the handler.
+     * @param array $args The argument list.
+     * @param array &$data The local request data.
+     */
+    public function _handler_connector($handler_id, array $args, array &$data)
+    {
+        $options = array
+        (
+            'roots' => array
+            (
+                array
+                (
+                    'driver' => 'Openpsa',
+                    'path' => $this->_topic->guid
+                )
+            )
+        );
+
+        $connector = new elFinderConnector(new elFinder($options));
+        $connector->run();
+    }
+
+    /**
+     * @param mixed $handler_id The ID of the handler.
+     * @param array $args The argument list.
+     * @param array &$data The local request data.
+     */
+    public function _handler_goto($handler_id, array $args, array &$data)
+    {
+        $parts = explode('_', $args[0]);
+        $guid = base64_decode($parts[1]);
+        $url = midcom::get()->permalinks->resolve_permalink($guid);
+        if (!$url)
+        {
+            throw new midcom_error_notfound('Could not resolve URL for ' . $guid);
+        }
+        return new midcom_response_relocate($url);
     }
 }
