0de181e07fddaefc5b34bf8a9dfcdf308d1dc0b6
flack <flack@contentcontrol-berlin.de>
2016-03-03 23:14:56 +0100

    Fix JS error when called from task view

--- lib/org/openpsa/calendar/handler/create.php
+++ lib/org/openpsa/calendar/handler/create.php
@@ -127,7 +127,7 @@ implements midcom_helper_datamanager2_interfaces_create
                 $indexer->index($data['controller']->datamanager);
                 //FALL-THROUGH
             case 'cancel':
-                midcom::get()->head->add_jsonload("window.opener.openpsa_calendar_instance.fullCalendar('refetchEvents');");
+                midcom::get()->head->add_jsonload("if (window.opener.openpsa_calendar_instance) window.opener.openpsa_calendar_instance.fullCalendar('refetchEvents');");
                 midcom::get()->head->add_jsonload('window.close();');
                 break;
         }
