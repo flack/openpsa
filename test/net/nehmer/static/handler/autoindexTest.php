bb413306d24cbdb1e08f2f2671e5ad7a5c4a913c
flack <flack@contentcontrol-berlin.de>
2016-11-08 19:39:02 +0100

    Add basic smoketest

---
+++ test/net/nehmer/static/handler/autoindexTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class net_nehmer_static_handler_autoindexTest extends openpsa_testcase
+{
+    public function testHandler_autoindex()
+    {
+        midcom::get()->auth->request_sudo('net.nehmer.static');
+        $data = array
+        (
+            'component' => 'net.nehmer.static',
+            'name' => __CLASS__ . time()
+        );
+        $topic = $this->create_object('midcom_db_topic', $data);
+        $topic->set_parameter('net.nehmer.static', 'autoindex', true);
+        $article_properties = array
+        (
+            'topic' => $topic->id,
+            'name' => 'dummy'
+        );
+        $this->create_object('midcom_db_article', $article_properties);
+
+        $data = $this->run_handler($topic);
+        $this->assertEquals('autoindex', $data['handler_id']);
+
+        midcom::get()->auth->drop_sudo();
+    }
+}
