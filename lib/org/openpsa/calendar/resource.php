4c1e718e2f80b040d625544e4188bd9c9fa88df3
flack <flack@contentcontrol-berlin.de>
2016-11-19 00:11:00 +0100

    these checks are already done in the baseclass, no need to repeat them

--- lib/org/openpsa/calendar/resource.php
+++ lib/org/openpsa/calendar/resource.php
@@ -16,41 +16,6 @@ class org_openpsa_calendar_resource_dba extends  midcom_core_dbaobject
     public $__midcom_class_name__ = __CLASS__;
     public $__mgdschema_class_name__ = 'org_openpsa_calendar_resource';
 
-    public function _on_creating()
-    {
-        if ($this->_check_duplicates($this->name)) {
-            midcom_connection::set_error(MGD_ERR_OBJECT_NAME_EXISTS);
-            return false;
-        }
-        return true;
-    }
-
-    public function _on_updating()
-    {
-        if ($this->_check_duplicates($this->name)) {
-            midcom_connection::set_error(MGD_ERR_OBJECT_NAME_EXISTS);
-            return false;
-        }
-        return true;
-    }
-
-    private function _check_duplicates($name)
-    {
-        if ($name == '') {
-            return false;
-        }
-
-        // Check for duplicates
-        $qb = org_openpsa_calendar_resource_dba::new_query_builder();
-        $qb->add_constraint('name', '=', $name);
-
-        if ($this->id) {
-            $qb->add_constraint('id', '<>', $this->id);
-        }
-
-        return ($qb->count() > 0);
-    }
-
     function get_reservations($from, $to)
     {
         $qb = org_openpsa_calendar_event_resource_dba::new_query_builder();
