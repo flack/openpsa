f7918c472158ce4f44c3f580a0735f33a463998a
flack <flack@contentcontrol-berlin.de>
2016-11-20 14:54:10 +0100

    Use sf dom-crawler instead of custom regexes

--- lib/org/openpsa/httplib/helpers.php
+++ lib/org/openpsa/httplib/helpers.php
@@ -6,6 +6,8 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
 
+use Symfony\Component\DomCrawler\Crawler;
+
 /**
  * Helpers for HTTP content fetching and handling
  *
@@ -20,22 +22,12 @@ class org_openpsa_httplib_helpers extends midcom_baseclasses_components_purecode
      *
      * @param string $html HTML to parse
      * @param string $name Name of the meta tag to fetch
-     * @param array Content of the meta tag
+     * @return string Content of the meta tag
      */
     public static function get_meta_value($html, $name)
     {
-        $quotes = self::$_quotes;
-        $regex_metatag = "/<html.*?>.*?<head.*?>.*?(<meta[^>]*?name=([{$quotes}]){$name}\\2[^>]*?>).*?<\/head>/msi";
-        $regex_value = "/content=([{$quotes}])(.*?)\\1/i";
-        if (!preg_match($regex_metatag, $html, $tag_matches)) {
-            debug_add("Failed to find meta tag for name \"{$name}\"");
-            return '';
-        }
-        if (!preg_match($regex_value, $tag_matches[1], $value_matches)) {
-            debug_add("Failed to find meta tag value for tag name \"{$name}\"");
-            return '';
-        }
-        return $value_matches[2];
+        $crawler = new Crawler($html);
+        return (string) $crawler->filter('head meta[name="' . $name . '"]')->attr('content');
     }
 
     /**
@@ -48,47 +40,22 @@ class org_openpsa_httplib_helpers extends midcom_baseclasses_components_purecode
      */
     public static function get_link_values($html, $relation, $type = null)
     {
-        $quotes = self::$_quotes;
-        $values = array();
-        $regex_linktags = "/<html.*?>.*?<head.*?>.*?(<link[^>]*?rel=([{$quotes}]){$relation}\\2[^>]*?>).*?<\/head>/msi";
-        if (!preg_match_all($regex_linktags, $html, $tag_matches)) {
-            debug_add("Failed to find link tag values for relation \"{$relation}\"");
-            return $values;
-        }
+        $crawler = new Crawler($html);
+        $nodes = $crawler->filter('head link[rel="' . $relation . '"]');
+
         if (!is_null($type)) {
-            $regex_type_filter = "/type=([{$quotes}]){$type}\\1/i";
-            foreach ($tag_matches[1] as $k => $tagcode) {
-                // Type filter not matches, clear from resultset
-                if (!preg_match($regex_type_filter, $tagcode)) {
-                    unset($tag_matches[0][$k]);
-                    unset($tag_matches[1][$k]);
-                    unset($tag_matches[2][$k]);
-                }
-            }
-        }
-        $regex_properties = "/(title|href|hreflang)=([{$quotes}])(.*?)\\2/i";
-        foreach ($tag_matches[1] as $tagcode) {
-            $tag = array(
-                'title' => false,
-                'href' => false,
-                'hreflang' => false,
-            );
-            if (!preg_match_all($regex_properties, $tagcode, $property_matches)) {
-                // Could not read properties
-                continue;
-            }
-            foreach ($property_matches[1] as $k => $property) {
-                $value = $property_matches[3][$k];
-                if (!array_key_exists($property, $tag)) {
-                    // WTF unsupported property ??
-                    continue;
-                }
-                $tag[$property] = $value;
-            }
-            $values[] = $tag;
+            $nodes = $nodes->filter('[type="' . $type . '"]');
         }
 
-        return $values;
+        return $nodes->each(function(Crawler $node, $i) {
+            $tag = array('title' => false, 'href' => false, 'hreflang' => false);
+            foreach ($tag as $property => &$value) {
+                if ($node->attr($property) !== null) {
+                    $value = $node->attr($property);
+                }
+            }
+            return $tag;
+        });
     }
 
     /**
@@ -100,38 +67,15 @@ class org_openpsa_httplib_helpers extends midcom_baseclasses_components_purecode
      */
     public static function get_anchor_values($html, $relation)
     {
-        $quotes = self::$_quotes;
-        $values = array();
-        $regex_atags = "/(<a[^>]*?rel=([{$quotes}]){$relation}\\2[^>]*?>)((.*?)<\/a>)?/msi";
-        if (!preg_match_all($regex_atags, $html, $tag_matches)) {
-            debug_add("Failed to find anchor tag values for relation \"{$relation}\"");
-            return $values;
-        }
-        $regex_properties = "/(title|href)=([{$quotes}])(.*?)\\2/i";
-        foreach ($tag_matches[1] as $key => $tagcode) {
-            $tag = array(
-                'title' => false,
-                'href' => false,
-                'value' => false,
-            );
-            if (!preg_match_all($regex_properties, $tagcode, $property_matches)) {
-                // Could not read properties
-                continue;
-            }
-            if (isset($tag_matches[4][$key])) {
-                $tag['value'] = $tag_matches[4][$key];
-            }
-            foreach ($property_matches[1] as $k => $property) {
-                $value = $property_matches[3][$k];
-                if (!array_key_exists($property, $tag)) {
-                    // WTF unsupported property ??
-                    continue;
-                }
-                $tag[$property] = $value;
-            }
-            $values[] = $tag;
-        }
+        $crawler = new Crawler($html);
+        $nodes = $crawler->filter('a[rel="' . $relation . '"]');
 
-        return $values;
+        return $nodes->each(function(Crawler $node, $i) {
+            return array(
+                'title' => ($node->attr('title') !== null) ? $node->attr('title') : false,
+                'href' => ($node->attr('href') !== null) ? $node->attr('href') : false,
+                'value' => ($node->text() !== null) ? $node->text() : false,
+            );
+        });
     }
 }
---
+++ test/org/openpsa/httplib/helpersTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_httplib_helpersTest extends openpsa_testcase
+{
+    public function test_get_link_values()
+    {
+        $html = '<html><head><link rel="alternate" title="alt title" href="alt-link"></head></html>';
+        $expected = array(
+            array(
+                'title' => 'alt title',
+                'href' => 'alt-link',
+                'hreflang' => false,
+            )
+        );
+        $ret = org_openpsa_httplib_helpers::get_link_values($html, 'alternate');
+        $this->assertEquals($expected, $ret);
+    }
+
+    public function test_get_meta_value()
+    {
+        $html = '<html><head><meta name="icbm" content="1,1"></head></html>';
+        $ret = org_openpsa_httplib_helpers::get_meta_value($html, 'icbm');
+        $this->assertEquals('1,1', $ret);
+    }
+
+    public function test_get_anchor_values()
+    {
+        $html = '<a rel="tag" title="tag title" href="tag-link" class="test">dummy</a>';
+        $expected = array(
+            array(
+                'title' => 'tag title',
+                'href' => 'tag-link',
+                'value' => 'dummy',
+            )
+        );
+
+        $ret = org_openpsa_httplib_helpers::get_anchor_values($html, 'tag');
+        $this->assertEquals($expected, $ret);
+    }
+}
