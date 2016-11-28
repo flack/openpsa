66b7d3c2b095170089abfeaaacd5e91e8e291943
flack <flack@contentcontrol-berlin.de>
2016-11-28 10:48:00 +0100

    Remove duplicate slash

--- lib/org/openpsa/products/style/group-tree.php
+++ lib/org/openpsa/products/style/group-tree.php
@@ -3,7 +3,7 @@ $tree = new org_openpsa_widgets_tree('org_openpsa_products_product_group_dba', '
 $tree->title_fields = array('title', 'code');
 $tree->link_callback = function($guid) {
     $prefix = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_ANCHORPREFIX);
-    return $prefix . '/' . $guid . '/';
+    return $prefix . $guid . '/';
 };
 ?>
 <div class="area org_openpsa_helper_box">
