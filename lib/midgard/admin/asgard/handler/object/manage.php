94469c4f6603e6037c633e2f9f65e96daa949b83
flack <flack@contentcontrol-berlin.de>
2016-12-14 14:50:28 +0100

    remove useless semicolon

--- lib/midgard/admin/asgard/handler/object/manage.php
+++ lib/midgard/admin/asgard/handler/object/manage.php
@@ -232,7 +232,7 @@ class midgard_admin_asgard_handler_object_manage extends midcom_baseclasses_comp
         $this->_new_object = new $create_type();
         $mgd_type = midcom::get()->dbclassloader->get_mgdschema_class_name_for_midcom_class($create_type);
 
-        if ($parent_property = midgard_object_class::get_property_parent($mgd_type));
+        if ($parent_property = midgard_object_class::get_property_parent($mgd_type))
         {
             $this->_new_object->$parent_property = $controller->formmanager->get_value($parent_property);
         }
