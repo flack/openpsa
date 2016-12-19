04d3f28c8896b5ae14285cbba9829b777121d64d
flack <flack@contentcontrol-berlin.de>
2016-12-19 16:53:29 +0100

    remove mgd1-only property

--- lib/midcom/admin/help/help.php
+++ lib/midcom/admin/help/help.php
@@ -352,7 +352,6 @@ class midcom_admin_help_help extends midcom_baseclasses_components_plugin
             foreach ($class_props as $prop) {
                 switch ($prop) {
                     case 'action':
-                    case 'sid':
                         // Midgard-internal properties, skip
                         break;
                     case 'guid':
