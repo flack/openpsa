78e624776a54025bb1439e033ac0ffdeb8e1c20c
flack <flack@contentcontrol-berlin.de>
2017-03-11 19:27:19 +0100

    more phpunit 6+ compat

--- test/utilities/bootstrap.php
+++ test/utilities/bootstrap.php
@@ -13,6 +13,8 @@
 if (   !class_exists('\PHPUnit_Framework_TestCase')
     && class_exists('\PHPUnit\Framework\TestCase')) {
     class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+    class_alias('\PHPUnit\Framework\Constraint\IsEqual', '\PHPUnit_Framework_Constraint_IsEqual');
+    class_alias('\PHPUnit\Util\InvalidArgumentHelper', '\PHPUnit_Util_InvalidArgumentHelper');
 }
 
 require_once __DIR__ . '/testcase.php';
