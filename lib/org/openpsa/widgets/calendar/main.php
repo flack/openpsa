78c994951553a5e6dba3c9e60c014896f021a10f
flack <flack@contentcontrol-berlin.de>
2017-05-30 17:08:27 +0200

    Update fullcalendar to latest

--- lib/org/openpsa/widgets/calendar/main.php
+++ lib/org/openpsa/widgets/calendar/main.php
@@ -16,7 +16,7 @@ class org_openpsa_widgets_calendar extends midcom_baseclasses_components_purecod
     public static function add_head_elements()
     {
         $head = midcom::get()->head;
-        $prefix = '/org.openpsa.widgets/fullcalendar-3.2.0/';
+        $prefix = '/org.openpsa.widgets/fullcalendar-3.4.0/';
         $head->add_jsfile(MIDCOM_STATIC_URL . $prefix . 'lib/moment.min.js');
         $head->add_jsfile(MIDCOM_STATIC_URL . $prefix . 'fullcalendar.min.js');
 
@@ -33,7 +33,7 @@ class org_openpsa_widgets_calendar extends midcom_baseclasses_components_purecod
         }
 
         $head->add_stylesheet(MIDCOM_STATIC_URL . $prefix . 'fullcalendar.min.css');
-        $head->add_stylesheet(MIDCOM_STATIC_URL . $prefix . 'fullcalendar.print.css', 'print');
+        $head->add_stylesheet(MIDCOM_STATIC_URL . $prefix . 'fullcalendar.print.min.css', 'print');
         $head->add_stylesheet(MIDCOM_STATIC_URL . '/org.openpsa.calendar/calendar.css');
 
         $head->add_jsfile(MIDCOM_STATIC_URL . '/jQuery/history.js-1.8.0/jquery.history.js');
