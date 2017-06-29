04ca04a598cc08c402fe66e624340f53fab30f63
flack <flack@contentcontrol-berlin.de>
2017-06-29 23:21:13 +0200

    Convert to new dm

--- lib/midcom/baseclasses/components/handler/configuration.php
+++ lib/midcom/baseclasses/components/handler/configuration.php
@@ -6,16 +6,18 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
 
+use midcom\datamanager\datamanager;
+
 /**
- * midcom.helper.datamanager2 based configuration
+ * midcom.datamanager based configuration
  *
  * Usage:
  *
- * 1. Write a midcom_helper_datamanager2_schema compatible configuration
+ * 1. Write a datamanager schema compatible configuration
  *    schema and place it among your component files
  * 2. Point a configuration key 'schemadb_config' to it within your
  *    component configuration (_config/config.inc_)
- * 3. Refer to DM2 component configuration helper with a request handler,
+ * 3. Refer to the component configuration helper with a request handler,
  *    e.g.
  *
  * <code>
@@ -29,30 +31,14 @@
  * @package midcom.baseclasses
  */
 class midcom_baseclasses_components_handler_configuration extends midcom_baseclasses_components_handler
-implements midcom_helper_datamanager2_interfaces_edit
 {
-    /**
-     * DM2 controller instance
-     *
-     * @var midcom_helper_datamanager2_controller $_controller
-     */
-    private $_controller;
-
     public function __construct()
     {
         $this->_component = midcom_core_context::get()->get_key(MIDCOM_CONTEXT_COMPONENT);
         parent::__construct();
     }
 
-    public function get_schema_name()
-    {
-        if (isset($this->_master->_handler['schema'])) {
-            return $this->_master->_handler['schema'];
-        }
-        return 'config';
-    }
-
-    public function load_schemadb()
+    private function load_controller()
     {
         if (isset($this->_master->_handler['schemadb'])) {
             $schemadb_path = $this->_master->_handler['schemadb'];
@@ -62,13 +48,11 @@ implements midcom_helper_datamanager2_interfaces_edit
             throw new midcom_error("No configuration schema defined");
         }
 
-        $schemadb = midcom_helper_datamanager2_schema::load_database($schemadb_path);
+        $schema = (isset($this->_master->_handler['schema'])) ? $this->_master->_handler['schema'] : 'config';
 
-        if (empty($schemadb)) {
-            throw new midcom_error('Failed to load configuration schemadb');
-        }
-
-        return $schemadb;
+        return datamanager::from_schemadb($schemadb_path)
+            ->set_storage($this->_topic, $schema)
+            ->get_controller();
     }
 
     /**
@@ -80,16 +64,13 @@ implements midcom_helper_datamanager2_interfaces_edit
      */
     public function _handler_config($handler_id, array $args, array &$data)
     {
-        // Require corresponding ACL's
+        // Require corresponding ACLs
         $this->_topic->require_do('midgard:update');
         $this->_topic->require_do('midcom:component_config');
 
-        // Load the midcom_helper_datamanager2_controller for form processing
-        $this->_controller = $this->get_controller('simple', $this->_topic);
-
         midcom::get()->head->set_pagetitle($this->_l10n_midcom->get('component configuration'));
 
-        $workflow = $this->get_workflow('datamanager2', ['controller' => $this->_controller]);
+        $workflow = $this->get_workflow('datamanager', ['controller' => $this->load_controller()]);
         if (   method_exists($this, '_load_datamanagers')
             && method_exists($this, '_load_objects')) {
             $workflow->add_post_button('config/recreate/', $this->_l10n_midcom->get('recreate images'), [
