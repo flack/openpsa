1b91c6673bf99d091455a36183be02785320c878
flack <flack@contentcontrol-berlin.de>
2017-07-03 17:26:06 +0200

    Add support for rcsmessage storage

---
+++ src/midcom/datamanager/storage/rcsmessage.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright CONTENT CONTROL GmbH, http://www.contentcontrol-berlin.de
+ */
+
+namespace midcom\datamanager\storage;
+
+/**
+ * Experimental storage class
+ */
+class rcsmessage extends dbanode
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get_value()
+    {
+        return $this->object->get_rcs_message();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set_value($value)
+    {
+        $this->object->set_rcs_message($value);
+    }
+
+    public function save()
+    {
+        return true;
+    }
+}
