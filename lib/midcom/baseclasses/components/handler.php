e095067f7794327a071505fe17491e2f906724d9
flack <flack@contentcontrol-berlin.de>
2017-07-12 12:16:27 +0200

    Remove helper functions that are no longer needed

--- lib/midcom/baseclasses/components/handler.php
+++ lib/midcom/baseclasses/components/handler.php
@@ -148,50 +148,6 @@ abstract class midcom_baseclasses_components_handler extends midcom_baseclasses_
     }
 
     /**
-     * Helper function for quick access to diverse datamanager controllers.
-     *
-     * For this to work, the handler has to implement the respective DM2 interface
-     *
-     * @todo Maybe we should do a class_implements check here
-     * @param string $type The controller type
-     * @param midcom_core_dbaobject $object The object, if any
-     * @return midcom_helper_datamanager2_controller The initialized controller
-     */
-    public function get_controller($type, $object = null)
-    {
-        switch ($type) {
-            case 'simple':
-                return midcom_helper_datamanager2_handler::get_simple_controller($this, $object);
-            case 'nullstorage':
-                return midcom_helper_datamanager2_handler::get_nullstorage_controller($this);
-            case 'create':
-                return midcom_helper_datamanager2_handler::get_create_controller($this);
-            default:
-                throw new midcom_error("Unsupported controller type: {$type}");
-        }
-    }
-
-    /**
-     * Default helper function for DM2 schema-related operations
-     *
-     * @return string The default DM2 schema name
-     */
-    public function get_schema_name()
-    {
-        return 'default';
-    }
-
-    /**
-     * Default helper function for DM2 schema-related operations
-     *
-     * @return array The schema defaults
-     */
-    public function get_schema_defaults()
-    {
-        return [];
-    }
-
-    /**
      * Binds the current page view to a particular object. This will automatically connect such things like
      * metadata and toolbars to the correct object.
      *
