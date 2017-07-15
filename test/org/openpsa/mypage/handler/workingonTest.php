f0e12e78656aa3cd6de9d666bbf84a202ac74c0e
flack <flack@contentcontrol-berlin.de>
2017-07-15 21:09:48 +0200

    Increase coverage

--- test/org/openpsa/mypage/handler/workingonTest.php
+++ test/org/openpsa/mypage/handler/workingonTest.php
@@ -13,25 +13,28 @@
  */
 class org_openpsa_mypage_handler_workingonTest extends openpsa_testcase
 {
+    private static $task;
+
     public static function setUpBeforeClass()
     {
         self::create_user(true);
+        $project = self::create_class_object('org_openpsa_projects_project');
+        self::$task = self::create_class_object('org_openpsa_projects_task_dba', ['project' => $project->id]);
     }
 
     public function testHandler_workingon()
     {
+        $this->create_object('org_openpsa_projects_hour_report_dba', ['task' => self::$task->id]);
         $data = $this->run_handler('org.openpsa.mypage', ['workingon']);
         $this->assertEquals('workingon', $data['handler_id']);
     }
 
     public function testHandler_weekreview_redirect()
     {
-        $project = $this->create_object('org_openpsa_projects_project');
-        $task = $this->create_object('org_openpsa_projects_task_dba', ['project' => $project->id]);
 
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $_POST = [
-            'task' => $task->id,
+            'task' => self::$task->id,
             'description' => 'test',
             'action' => 'stop',
         ];
