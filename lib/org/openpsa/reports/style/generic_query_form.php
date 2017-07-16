67bc3e39a76dc42d96e608bc23d890cf22399970
flack <flack@contentcontrol-berlin.de>
2017-07-16 13:06:42 +0200

    Small simplification

--- lib/org/openpsa/reports/style/generic_query_form.php
+++ lib/org/openpsa/reports/style/generic_query_form.php
@@ -2,13 +2,6 @@
     <?php  $data['controller']->display_form();  ?>
     <!-- To open the report in new window we need to set the target via JS -->
     <script type="text/javascript">
-    $('.datamanager2 button[type="submit"]').on('click', function()
-    {
-        if ($(this).hasClass('save')) {
-            $(this).closest('form').attr('target', '_blank');
-        } else {
-            $(this).closest('form').removeAttr('target');
-        }
-    });
+    $('.datamanager2 button[type="submit"].save').attr('formtarget', '_blank');
     </script>
 </div>
