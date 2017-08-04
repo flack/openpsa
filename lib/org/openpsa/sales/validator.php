86a65fc2cd3b6f930a45f699d77a3bb3f47f5456
flack <flack@contentcontrol-berlin.de>
2017-08-04 18:17:57 +0200

    Adapt to changed data structure

--- lib/org/openpsa/sales/validator.php
+++ lib/org/openpsa/sales/validator.php
@@ -20,8 +20,7 @@ class org_openpsa_sales_validator
         if ($result === true) {
             $result = [];
         }
-        if (   (   empty($fields['end_date'])
-                || $fields['end_date'] == '0000-00-00')
+        if (   empty($fields['end'])
             && empty($fields['continuous'])) {
             $result['end'] = midcom::get()->i18n->get_string('select either end date or continuous', 'org.openpsa.sales');
         }
