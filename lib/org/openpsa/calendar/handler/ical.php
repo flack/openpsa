a0d4f95461078507d9afb33fbc5fd8a21934aa5f
flack <flack@contentcontrol-berlin.de>
2017-09-15 10:38:05 +0200

    Add rudimentary create/update support for ical

--- lib/org/openpsa/calendar/handler/ical.php
+++ lib/org/openpsa/calendar/handler/ical.php
@@ -6,6 +6,8 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
 
+use Sabre\VObject\Reader;
+
 /**
  * Calendar ical handler
  *
@@ -63,10 +65,53 @@ class org_openpsa_calendar_handler_ical extends midcom_baseclasses_components_ha
 
         $username = $this->_strip_extension($args[0]);
         $data['person'] = $this->_find_person_by_name($username);
+        if (!empty($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'PUT') {
+            $this->update(file_get_contents('php://input'));
+        }
 
         $this->_get_events();
 
         $this->_content_type();
+    }
+
+    private function update($input)
+    {
+        debug_add($input, 0);
+
+        $vcalendar = Reader::read($input);
+        if (!$vcalendar->select('VEVENT')) {
+            return;
+        }
+        foreach ($vcalendar->select('VEVENT') as $vevent) {
+            $uid = $vevent->UID->getValue();
+            if (substr($uid, -12, 12) === '-midgardGuid') {
+                $event = new org_openpsa_calendar_event_dba(substr($uid, 0, -12));
+            } else {
+                $event = new org_openpsa_calendar_event_dba;
+                $event->externalGuid = $uid;
+                $root_event = org_openpsa_calendar_interface::find_root_event();
+                $root_event->require_do('midgard:create');
+                $event->up = $root_event->id;
+            }
+            $event->title = $vevent->SUMMARY->getValue();
+            $event->description = $vevent->DESCRIPTION->getValue();
+            $event->location = $vevent->LOCATION ? $vevent->LOCATION->getValue() : '';
+            $event->busy = $vevent->TRANSP->getValue() == 'OPAQUE';
+            $start = new DateTime($vevent->DTSTART->getValue());
+            $event->start = (int) $start->format('U');
+            $end = new DateTime($vevent->DTEND->getValue());
+            $event->end = (int) $end->format('U');
+
+            if ($event->id) {
+                $event->update();
+            } else {
+                $event->create();
+                $member = new org_openpsa_calendar_event_member_dba;
+                $member->eid = $event->id;
+                $member->uid = $this->_request_data['person']->id;
+                $member->create();
+            }
+        }
     }
 
     /**
