d1670c2165cadb7b1f3aa2927a07e7210194e1b0
flack <flack@contentcontrol-berlin.de>
2017-09-18 15:43:35 +0200

    use a bit more semantically meaningful markup

--- lib/midgard/admin/asgard/style/midgard_admin_asgard_component_configuration_item.php
+++ lib/midgard/admin/asgard/style/midgard_admin_asgard_component_configuration_item.php
@@ -1,15 +1,15 @@
-<tr class="odd">
-<td class="key" colspan="2"><span><?php echo midcom::get()->i18n->get_string($data['key'], $data['name']); ?></span></td>
+<tr>
+<th class="key" colspan="2"><span><?php echo midcom::get()->i18n->get_string($data['key'], $data['name']); ?></span></th>
 </tr>
-<tr class="even">
+<tr>
 <?php if ($data['local'] == '<strong>N/A</strong>') {
     ?>
     <td class="global" colspan="2">&(data['global']:h);</td>
-<?php 
+<?php
 } else {
     ?>
     <td class="global">&(data['global']:h);</td>
     <td class="local">&(data['local']:h);</td>
-<?php 
+<?php
 } ?>
 </tr>
