2f93f0248dfd19f9140c432e35bc4d25a6cd7b3b
flack <flack@contentcontrol-berlin.de>
2017-09-26 16:15:13 +0200

    Add missing dependency

--- lib/midcom/admin/folder/management.php
+++ lib/midcom/admin/folder/management.php
@@ -1,11 +1,12 @@
 <?php
-use Symfony\Component\Finder\Finder;
 /**
  * @package midcom.admin.folder
  * @author The Midgard Project, http://www.midgard-project.org
  * @copyright The Midgard Project, http://www.midgard-project.org
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
+
+use Symfony\Component\Finder\Finder;
 
 /**
  * Folder management class.
