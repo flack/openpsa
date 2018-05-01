35911ad3f12611da2306aaf5e36e728bccc88f1a
flack <flack@contentcontrol-berlin.de>
2018-05-01 14:12:58 +0200

    CS cleanups

--- static/org.openpsa.mypage/mypage.js
+++ static/org.openpsa.mypage/mypage.js
@@ -1,23 +1,20 @@
 
-function show_loading()
-{
+function show_loading() {
     $('#org_openpsa_mypage_workingon_widget .org_openpsa_mypage_workingon').hide();
     $('#org_openpsa_mypage_workingon_loading').show();
 }
 
-function send_working_on(action)
-{
+function send_working_on(action) {
     var description = $("#working_description").serialize(),
-    task = $("#working_task_selection").val().replace(/[\[|"|\]]/g, ''),
-    invoiceable = $('#working_invoiceable').is(':checked'),
-    send_url = MIDCOM_PAGE_PREFIX + "workingon/set/";
+        task = $("#working_task_selection").val().replace(/[\[|"|\]]/g, ''),
+        invoiceable = $('#working_invoiceable').is(':checked'),
+        send_url = MIDCOM_PAGE_PREFIX + "workingon/set/";
 
     $.ajax({
         type: "POST",
         url: send_url,
         data: description + "&task=" + task + "&invoiceable=" + invoiceable + '&action=' + action,
-        success: function(msg)
-        {
+        success: function(msg) {
             $("#org_openpsa_mypage_workingon_widget").html(msg);
         },
         error: function() {
@@ -29,44 +26,28 @@ function send_working_on(action)
     });
 }
 
-var org_openpsa_workingon =
-{
-    setup_widget: function()
-    {
+var org_openpsa_workingon = {
+    setup_widget: function() {
         $('#org_openpsa_mypage_workingon_loading').hide();
         $('#org_openpsa_mypage_workingon_start')
             .prop("disabled", true)
-            .on('click', function()
-            {
+            .on('click', function() {
                 send_working_on('start');
             });
 
         $('#org_openpsa_mypage_workingon_stop')
-            .on('click', function()
-            {
+            .on('click', function() {
                 send_working_on('stop');
             });
 
-        if ($('#working_task_selection').val() === '')
-        {
+        if ($('#working_task_selection').val() === '') {
             $('#org_openpsa_mypage_workingon_stop').hide();
-        }
-        else
-        {
+        } else {
             $('#org_openpsa_mypage_workingon_start').hide();
         }
     },
-    select: function(event, ui)
-    {
+    select: function(event, ui) {
         midcom_helper_datamanager2_autocomplete.select(event, ui);
-        if ($('#working_task_selection').val() !== '')
-        {
-            $('#org_openpsa_mypage_workingon_start').prop("disabled", false);
-        }
-        else
-        {
-            $('#org_openpsa_mypage_workingon_start').prop("disabled", true);
-        }
+        $('#org_openpsa_mypage_workingon_start').prop("disabled", $('#working_task_selection').val() === '');
     }
-
 }
