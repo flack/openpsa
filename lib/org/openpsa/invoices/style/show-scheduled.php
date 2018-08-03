cca8e9ffb722d475ccbfe8fd3b332587bbf12da5
flack <flack@contentcontrol-berlin.de>
2018-08-03 10:57:14 +0200

    More logical column order

--- lib/org/openpsa/invoices/style/show-scheduled.php
+++ lib/org/openpsa/invoices/style/show-scheduled.php
@@ -26,9 +26,9 @@ $grid->set_option('loadonce', true)
 
 $grid->set_column('time', $data['l10n_midcom']->get('date'), 'width: 80, fixed: true, align: "right", formatter: "date"')
     ->set_column('month', '', 'hidden: true', 'string')
+    ->set_column('deliverable', $i18n->get_l10n('org.openpsa.sales')->get('deliverable'), 'width: 100, classes: "ui-ellipsis"', 'string')
     ->set_column('customer', $data['l10n']->get('customer'), 'width: 100, classes: "ui-ellipsis"', 'string')
     ->set_column('customerContact', $data['l10n']->get('customer contact'), 'width: 100, classes: "ui-ellipsis"', 'string')
-    ->set_column('deliverable', $i18n->get_l10n('org.openpsa.sales')->get('deliverable'), 'width: 100, classes: "ui-ellipsis"', 'string')
     ->set_column('type', $i18n->get_l10n('midgard.admin.asgard')->get('type'), 'width: 80, classes: "ui-ellipsis"')
     ->set_column('sum', $data['l10n']->get('amount'), 'width: 80, fixed: true, align: "right", title: false, classes: "sum", sorttype: "number", formatter: "number", summaryType:"sum"');
 
