4f85a26ad4a56b507b9ce07e34fb064142d38715
flack <flack@contentcontrol-berlin.de>
2018-12-19 21:31:52 +0100

    Move code to where it is actually used

--- src/midcom/datamanager/extension/type/formType.php
+++ src/midcom/datamanager/extension/type/formType.php
@@ -11,8 +11,13 @@ use Symfony\Component\Form\Extension\Core\Type\FormType as base;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Callback;
 use midcom\datamanager\validation\callback as cb_wrapper;
-use midcom\datamanager\extension\compat;
 use midcom\datamanager\schema;
+use midcom\datamanager\storage\container\dbacontainer;
+use midcom;
+use midcom_error;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * Experimental form type
@@ -69,7 +74,7 @@ class formType extends base
                 continue;
             }
 
-            $builder->add($field, compat::get_type_name($config['widget']), compat::get_settings($config, $storage));
+            $builder->add($field, $this->get_type_name($config['widget']), $this->get_settings($config, $storage));
         }
     }
 
@@ -79,5 +84,92 @@ class formType extends base
     public function getParent()
     {
         return base::class;
+    }
+
+    /**
+     * Provide fully qualified type names
+     *
+     * @param string $shortname
+     * @return string
+     */
+    private function get_type_name($shortname)
+    {
+        if (class_exists('midcom\datamanager\extension\type\\' . $shortname . 'Type')) {
+            return 'midcom\datamanager\extension\type\\' . $shortname . 'Type';
+        }
+        if (class_exists('Symfony\Component\Form\Extension\Core\Type\\' . ucfirst($shortname) . 'Type')) {
+            return 'Symfony\Component\Form\Extension\Core\Type\\' . ucfirst($shortname) . 'Type';
+        }
+        return $shortname;
+    }
+
+    /**
+     * Convert schema config to type settings
+     *
+     * @param array $config
+     * @param mixed $storage
+     * @return array
+     */
+    private function get_settings(array $config, $storage)
+    {
+        if ($config['write_privilege'] !== null) {
+            if (   array_key_exists('group', $config['write_privilege'])
+                && !midcom::get()->auth->is_group_member($config['write_privilege']['group'])) {
+                $config['readonly'] = true;
+            }
+            if (   array_key_exists('privilege', $config['write_privilege'])
+                && $storage instanceof dbacontainer
+                && !$storage->get_value()->can_do($config['write_privilege']['privilege'])) {
+                $config['readonly'] = true;
+            }
+        }
+
+        $settings = $config;
+        $settings['label'] = $config['title'];
+        $settings['dm2_type'] = $config['type'];
+        $settings['dm2_storage'] = $config['storage'];
+        $settings['attr']['readonly'] = $config['readonly'];
+        $settings['constraints'] = $this->build_constraints($config);
+        $settings['storage'] = $storage;
+
+        unset($settings['readonly']);
+        unset($settings['type']);
+        unset($settings['customdata']);
+        unset($settings['default']);
+        unset($settings['description']);
+        unset($settings['title']);
+        unset($settings['validation']);
+        unset($settings['widget']);
+        unset($settings['write_privilege']);
+
+        return $settings;
+    }
+
+    private function build_constraints($config)
+    {
+        $constraints = !empty($config['constraints']) ? $config['constraints'] : [];
+
+        foreach ((array) $config['validation'] as $rule) {
+            if (is_object($rule)) {
+                $constraints[] = $rule;
+                continue;
+            }
+            if ($rule['type'] === 'email') {
+                $constraints[] = new Email();
+            } elseif ($rule['type'] === 'regex') {
+                $r_options = ['pattern' => $rule['format']];
+                if (!empty($rule['message'])) {
+                    $r_options['message'] = $rule['message'];
+                }
+                $constraints[] = new Regex($r_options);
+            } else {
+                throw new midcom_error($rule['type'] . ' validation not implemented yet');
+            }
+        }
+        if ($config['required']) {
+            array_unshift($constraints, new NotBlank());
+        }
+
+        return $constraints;
     }
 }
