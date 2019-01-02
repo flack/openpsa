2387a19b466bf8d83c859fffe04087a55859afa7
flack <flack@contentcontrol-berlin.de>
2019-01-02 22:39:38 +0100

    Untabify

--- static/org.openpsa.relatedto/related_to.js
+++ static/org.openpsa.relatedto/related_to.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
     var ooRelatedInfoFetched = {};
     $('.relatedto_toolbar')
-	.on('click', '.button.info', function() {
-	    var guid = $(this).closest('.relatedto_toolbar').data('other-guid'),
-		target_field = $('#org_openpsa_relatedto_details_' + guid);
-	    if (!target_field.is('visible')) {
-		if (ooRelatedInfoFetched[guid] != true) {
-		    var urlField = $('#org_openpsa_relatedto_details_url_' + guid);
+        .on('click', '.button.info', function() {
+            var guid = $(this).closest('.relatedto_toolbar').data('other-guid'),
+                target_field = $('#org_openpsa_relatedto_details_' + guid);
+            if (!target_field.is('visible')) {
+                if (ooRelatedInfoFetched[guid] != true) {
+                    var urlField = $('#org_openpsa_relatedto_details_url_' + guid);
 
-		    if (urlField.length > 0) {
-		        target_field.load(urlField.attr('title'), function() {
-		            target_field.show('slow');
-		        });
-		    }
-		    ooRelatedInfoFetched[guid] = true;
-		} else {
-		    target_field.show('slow');
-		}
-	    } else {
-		target_field.hide('slow');
-	    }
-	})
-	.on('click', '.button.confirm, .button.deny', function() {
-	    var container = $(this).closest('.relatedto_toolbar'),
-		guid = container.data('link-guid'),
-		mode = $(this).hasClass('confirm') ? 'confirm' : 'deny',
-		url = MIDCOM_PAGE_PREFIX + '__mfa/org.openpsa.relatedto/ajax/' + mode + '/' + guid + '/';
-	    $.post(url, {}, function() {
-		if (mode == 'deny') {
-		    $('#org_openpsa_relatedto_line_' + guid).hide('slow');
-		} else {
-		    $(container).find('.button.confirm, .button.deny').parent().hide('slow');
-		}
-	    });
-	});
+                    if (urlField.length > 0) {
+                        target_field.load(urlField.attr('title'), function() {
+                            target_field.show('slow');
+                        });
+                    }
+                    ooRelatedInfoFetched[guid] = true;
+                } else {
+                    target_field.show('slow');
+                }
+            } else {
+                target_field.hide('slow');
+            }
+        })
+        .on('click', '.button.confirm, .button.deny', function() {
+            var container = $(this).closest('.relatedto_toolbar'),
+                guid = container.data('link-guid'),
+                mode = $(this).hasClass('confirm') ? 'confirm' : 'deny',
+                url = MIDCOM_PAGE_PREFIX + '__mfa/org.openpsa.relatedto/ajax/' + mode + '/' + guid + '/';
+            $.post(url, {}, function() {
+                if (mode == 'deny') {
+                    $('#org_openpsa_relatedto_line_' + guid).hide('slow');
+                } else {
+                    $(container).find('.button.confirm, .button.deny').parent().hide('slow');
+                }
+            });
+        });
 });
