958566984de391e6435c5abb2aac3953421574cd
flack <flack@contentcontrol-berlin.de>
2019-02-13 23:35:17 +0100

    Layout improvements

--- lib/midcom/admin/help/style/midcom_admin_help_mgdschemas.php
+++ lib/midcom/admin/help/style/midcom_admin_help_mgdschemas.php
@@ -1,6 +1,4 @@
 <?php
-use Michelf\MarkdownExtra;
-
 echo "<h1>" . sprintf($data['l10n']->get('mgdschemas in %s'), midcom::get()->i18n->get_string($data['component'], $data['component'])) . "</h1>\n";
 
 if (!empty($data['mgdschemas'])) {
@@ -16,19 +14,17 @@ if (!empty($data['mgdschemas'])) {
         echo "            </tr>\n";
 
         foreach ($properties as $propname => $val) {
-            $proplink = "";
             $description = preg_replace('/ *\n */', "\n", $val['value']);
             if (   $val['link']
                 && $linked_component = midcom::get()->dbclassloader->get_component_for_class($val['link_name'])) {
                 $proplink = $data['router']->generate('help', ['component' => $linked_component, 'help_id' => 'mgdschemas']);
                 $proplink = "<a href='{$proplink}#{$val['link_name']}' title='{$linked_component}/{$val['link_name']}::{$val['link_target']}'>{$val['link_name']}:{$val['link_target']}</a>";
-                $classname = str_replace('_', '\\_', $val['link_name']);
-                $description .= "\n\n**This property links to {$classname}:{$val['link_target']}**";
+                $description .= "<div class='proplink'>This property links to {$proplink}</div>";
             }
 
             echo "            <tr>\n";
-            echo "                <td class='property'><span class='mgdtype'>{$val['midgard_type']}</span> {$propname}<br/>{$proplink}</td>\n";
-            echo "                <td>" . MarkdownExtra::defaultTransform($description) . "</td>\n";
+            echo "                <td class='property'><span class='mgdtype'>{$val['midgard_type']}</span> {$propname}</td>\n";
+            echo "                <td class='description'><p>" . trim($description) . "<p></td>\n";
             echo "            </tr>\n";
         }
         echo "        </tbody>\n";
