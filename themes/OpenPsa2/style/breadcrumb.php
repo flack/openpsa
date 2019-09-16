1d438729f67497885d2df697b5872c2a21dc829b
flack <flack@contentcontrol-berlin.de>
2019-09-16 20:13:52 +0200

    Make breadcrumb display more consistent

--- themes/OpenPsa2/style/breadcrumb.php
+++ themes/OpenPsa2/style/breadcrumb.php
@@ -1,6 +1,11 @@
 <div id="breadcrumb">
-<?php
-$nap = new midcom_helper_nav();
-echo $nap->get_breadcrumb_line(' &raquo; ', 'breadcrumb_link', 0, 'pagetitle');
-?>
+    <div class="breadcrumb-path">
+        <?php
+        $nap = new midcom_helper_nav();
+        echo $nap->get_breadcrumb_line(' &raquo; ', 'breadcrumb_link', 0, 'current-page');
+        ?>
+    </div>
+    <?php
+    echo '<h1>' . midcom_core_context::get()->get_key(MIDCOM_CONTEXT_PAGETITLE) . '</h1>';
+    ?>
 </div>
