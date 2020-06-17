a0477252a372ef6a4fc64d439cc7fbe8b58fcba9
flack <flack@contentcontrol-berlin.de>
2020-06-17 21:09:45 +0200

    Small cleanups

--- lib/net/nehmer/comments/handler/moderate.php
+++ lib/net/nehmer/comments/handler/moderate.php
@@ -16,34 +16,26 @@ use Symfony\Component\HttpFoundation\Request;
 class net_nehmer_comments_handler_moderate extends midcom_baseclasses_components_handler
 {
     /**
-     * Comment we are currently working with.
-     *
-     * @var net_nehmer_comments_comment
-     */
-    private $_comment;
-
-    /**
      * Marks comment as possible abuse
      */
     public function _handler_report_abuse(Request $request, string $guid)
     {
-        $this->load_comment($guid, false);
+        $comment = $this->load_comment($guid, false);
 
-        if ($this->_comment->status !== net_nehmer_comments_comment::MODERATED) {
-            if ($this->_comment->can_do('net.nehmer.comments:moderation')) {
+        if ($comment->status !== net_nehmer_comments_comment::MODERATED) {
+            if ($comment->can_do('net.nehmer.comments:moderation')) {
                 $status = net_nehmer_comments_comment::ABUSE;
             } else {
                 $status = net_nehmer_comments_comment::REPORTED_ABUSE;
             }
             $sudo = false;
-            if (!$this->_comment->can_do('midgard:update')) {
+            if (!$comment->can_do('midgard:update')) {
                 $sudo = midcom::get()->auth->request_sudo('net.nehmer.comments');
             }
 
-            if ($this->_comment->moderate($status, 'reported_abuse')) {
-                $moderators = $this->_config->get('moderators');
+            if ($comment->moderate($status, 'reported_abuse')) {
                 if ($moderators = $this->_config->get('moderators')) {
-                    $this->notify_moderators($moderators);
+                    $this->notify_moderators($moderators, $comment);
                 }
             }
             if ($sudo) {
@@ -54,13 +46,13 @@ class net_nehmer_comments_handler_moderate extends midcom_baseclasses_components
         return $this->reply($request);
     }
 
-    private function notify_moderators(string $moderators)
+    private function notify_moderators(string $moderators, net_nehmer_comments_comment $comment)
     {
         // Prepare notification message
         $message = [];
-        $message['title'] = sprintf($this->_l10n->get('comment %s reported as abuse'), $this->_comment->title);
+        $message['title'] = sprintf($this->_l10n->get('comment %s reported as abuse'), $comment->title);
         $message['content'] = '';
-        $logs = $this->_comment->get_logs();
+        $logs = $comment->get_logs();
         if (!empty($logs)) {
             $message['content'] .= $this->_l10n->get('moderation history').":\n\n";
             foreach ($logs as $time => $log) {
@@ -68,10 +60,10 @@ class net_nehmer_comments_handler_moderate extends midcom_baseclasses_components
                 $message['content'] .= $this->_l10n->get(sprintf('%s: %s by %s (from %s)', "$reported:\n", $this->_l10n->get($log['action']), $log['reporter'], $log['ip'])) . "\n\n";
             }
         }
-        $message['content'] = "\n\n" . midcom::get()->permalinks->create_permalink($this->_comment->objectguid);
+        $message['content'] = "\n\n" . midcom::get()->permalinks->create_permalink($comment->objectguid);
 
-        $message['abstract'] = sprintf($this->_l10n->get('comment %s reported as abuse'), $this->_comment->title);
-        $message['abstract'] .= " " . midcom::get()->permalinks->create_permalink($this->_comment->objectguid);
+        $message['abstract'] = sprintf($this->_l10n->get('comment %s reported as abuse'), $comment->title);
+        $message['abstract'] .= " " . midcom::get()->permalinks->create_permalink($comment->objectguid);
 
         // Notify moderators
         $moderator_guids = explode('|', $moderators);
@@ -85,32 +77,33 @@ class net_nehmer_comments_handler_moderate extends midcom_baseclasses_components
      */
     public function _handler_not_abuse(Request $request, string $guid)
     {
-        $this->load_comment($guid);
-        $this->_comment->moderate(net_nehmer_comments_comment::MODERATED, 'reported_not_abuse');
+        $comment = $this->load_comment($guid);
+        $comment->moderate(net_nehmer_comments_comment::MODERATED, 'reported_not_abuse');
         return $this->reply($request);
     }
 
     public function _handler_confirm_report(Request $request, string $action, string $guid)
     {
-        $this->load_comment($guid);
-        if ($this->_comment->status !== net_nehmer_comments_comment::MODERATED) {
+        $comment = $this->load_comment($guid);
+        if ($comment->status !== net_nehmer_comments_comment::MODERATED) {
             if ($action == 'confirm_abuse') {
-                $this->_comment->moderate(net_nehmer_comments_comment::ABUSE, 'confirmed_abuse');
+                $comment->moderate(net_nehmer_comments_comment::ABUSE, 'confirmed_abuse');
             } else {
-                $this->_comment->moderate(net_nehmer_comments_comment::JUNK, 'confirmed_junk');
+                $comment->moderate(net_nehmer_comments_comment::JUNK, 'confirmed_junk');
             }
         }
-        midcom::get()->indexer->delete($this->_comment->guid);
+        midcom::get()->indexer->delete([$comment->guid]);
         return $this->reply($request);
     }
 
-    private function load_comment(string $identifier, bool $require_moderation_privilege = true)
+    private function load_comment(string $identifier, bool $require_moderation_privilege = true) : net_nehmer_comments_comment
     {
-        $this->_comment = new net_nehmer_comments_comment($identifier);
+        $comment = new net_nehmer_comments_comment($identifier);
 
         if ($require_moderation_privilege) {
-            $this->_comment->require_do('net.nehmer.comments:moderation');
+            $comment->require_do('net.nehmer.comments:moderation');
         }
+        return $comment;
     }
 
     private function reply(Request $request) : midcom_response_relocate
