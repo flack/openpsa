5d6101b4ec799f15bb8e50eed1d0a5d81871eedc
flack <flack@contentcontrol-berlin.de>
2020-10-05 14:05:26 +0200

    Make sure resizing starts from a known-good value

--- lib/midgard/admin/asgard/style/ASGARD_ROOT.php
+++ lib/midgard/admin/asgard/style/ASGARD_ROOT.php
@@ -174,7 +174,11 @@ $extra_class = (!empty($data['asgard_toolbar']->items)) ? ' page-title-with-tool
             </span>
         </div>
         <script type="text/javascript">
-            $(window).trigger('resize');
+        	if (typeof midcom_grid_resize !== 'undefined') {
+                var nongrid_height = $('#siteinfo').height() + $('#content-text').offset().top;
+                $('#content-text').css('height', 'calc(100vh - ' + nongrid_height  + 'px - 4.7em)');
+	            $(window).trigger('resize');
+            }
         </script>
     </body>
 </html>
