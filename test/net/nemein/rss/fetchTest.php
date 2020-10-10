168c51aefdeeb2d62473d4ba91d735896b2cd7f0
flack <flack@contentcontrol-berlin.de>
2020-10-10 18:46:35 +0200

    Add php < 7.3 workaround

--- test/net/nemein/rss/fetchTest.php
+++ test/net/nemein/rss/fetchTest.php
@@ -43,7 +43,8 @@ class net_nemein_rss_fetchTest extends openpsa_testcase
 
         $this->assertEquals('import-test', $article->name);
         $this->assertEquals('Import Test', $article->title);
-        $this->assertEquals('Test Description <a rel="tag" title="tag title" href="http://openpsa2.org/news/tag-link" class="test">dummy</a>', $article->content);
+        // trim is a php 7.1 / 7.2 workaround
+        $this->assertEquals('Test Description <a rel="tag" title="tag title" href="http://openpsa2.org/news/tag-link" class="test">dummy</a>', trim($article->content));
         $this->assertEquals('http://openpsa2.org/news/no-such-entry/', $article->url);
         $this->assertEquals('|feed:' . md5($feed->url) . '|test category|test2|', $article->extra1);
 
