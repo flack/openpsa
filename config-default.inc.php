e46b198783bf484538d01be7006835a0b34186cb
flack <flack@contentcontrol-berlin.de>
2020-12-17 21:09:00 +0100

    Remove unneeded setting

--- config-default.inc.php
+++ config-default.inc.php
@@ -8,7 +8,6 @@
 
 require_once 'tools/bootstrap.php';
 $GLOBALS['midcom_config_local']['log_level'] = 5;
-$GLOBALS['midcom_config_local']['log_filename'] = dirname(midgard_connection::get_instance()->config->logfilename) . '/midcom.log';
 $GLOBALS['midcom_config_local']['midcom_root_topic_guid'] = openpsa_prepare_topics();
 $GLOBALS['midcom_config_local']['auth_backend_simple_cookie_secure'] = false;
 $GLOBALS['midcom_config_local']['toolbars_enable_centralized'] = false;
