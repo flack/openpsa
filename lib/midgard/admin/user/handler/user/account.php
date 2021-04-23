9b0237e45bed79eb06ebc4db2e0e39b17afbe419
flack <flack@contentcontrol-berlin.de>
2021-04-23 12:35:35 +0200

    Fix refactoring oversight

--- lib/midgard/admin/user/handler/user/account.php
+++ lib/midgard/admin/user/handler/user/account.php
@@ -152,7 +152,7 @@ class midgard_admin_user_handler_user_account extends midcom_baseclasses_compone
         if ($n <= 0 || $length <= 0) {
             return $this->_l10n->get('use positive numeric values');
         }
-        if ($n > $max_amount || $length > $length) {
+        if ($n > $max_amount || $length > $max_length) {
             return sprintf($this->_l10n->get('only up to %s passwords with maximum length of %s characters'), $max_amount, $max_length);
         }
 
