89fd0c89382f4f9ad8ffa4fd3c1246926eb01c98
flack <flack@contentcontrol-berlin.de>
2021-05-09 15:38:16 +0200

    Merge files & load only in the handler that needs them

--- lib/midcom/admin/help/help.php
+++ lib/midcom/admin/help/help.php
@@ -35,10 +35,6 @@ class midcom_admin_help_help extends midcom_baseclasses_components_plugin
         midcom::get()->skip_page_style = true;
         // doing this here as this component most probably will not be called by itself.
         midcom::get()->style->prepend_component_styledir('midcom.admin.help');
-
-        $this->add_stylesheet(MIDCOM_STATIC_URL . '/midcom.admin.help/style-editor.css');
-        midcom::get()->head->add_jsfile(MIDCOM_STATIC_URL . '/midcom.admin.help/twisty.js');
-        $this->add_stylesheet(MIDCOM_STATIC_URL . '/stock-icons/font-awesome-4.7.0/css/font-awesome.min.css');
     }
 
     /**
@@ -396,6 +392,9 @@ class midcom_admin_help_help extends midcom_baseclasses_components_plugin
             $this->read_schema_properties();
         }
         $data['html'] = $this->get_help_contents($help_id, $component);
+
+        midcom::get()->head->add_jsfile(MIDCOM_STATIC_URL . '/midcom.admin.help/twisty.js');
+        $this->add_stylesheet(MIDCOM_STATIC_URL . '/stock-icons/font-awesome-4.7.0/css/font-awesome.min.css');
 
         // Table of contents navi
         $data['view_title'] = sprintf(
