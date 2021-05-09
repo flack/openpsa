6d903565c1499d7cd1fc0653606d9169935e7253
flack <flack@contentcontrol-berlin.de>
2021-05-09 20:43:08 +0200

    Fix oversights

--- lib/org/openpsa/directmarketing/campaign/ruleresolver.php
+++ lib/org/openpsa/directmarketing/campaign/ruleresolver.php
@@ -158,19 +158,23 @@ class org_openpsa_directmarketing_campaign_ruleresolver
      */
     private function add_rules(array $rules, string $class) : bool
     {
-        if (!midcom::get()->dbclassloader->is_mgdschema_object($class)) {
+        if (midcom::get()->dbclassloader->is_midcom_db_object($class)) {
             $class = midcom::get()->dbclassloader->get_mgdschema_class_name_for_midcom_class($class);
         }
+
         if ($class == 'midgard_parameter') {
             return $this->add_parameter_rule($rules);
         }
         if (in_array($class, [midcom::get()->config->get('person_class'), 'midgard_person', 'org_openpsa_person'])) {
             return $this->apply_rules('person', $rules);
         }
+        if (in_array($class, ['midgard_group', 'org_openpsa_organization'])) {
+            return $this->apply_rules('group', $rules);
+        }
         if (in_array($class, ['midgard_member', 'org_openpsa_eventmember'])) {
             return $this->apply_rules('misc', $rules, $class, 'uid');
         }
-        if (in_array($class, ['midgard_member', 'org_openpsa_campaign_member', 'org_openpsa_campaign_message_receipt', 'org_openpsa_link_log'])) {
+        if (in_array($class, ['org_openpsa_campaign_member', 'org_openpsa_campaign_message_receipt', 'org_openpsa_link_log'])) {
             return $this->apply_rules('misc', $rules, $class, 'person');
         }
         debug_add("class " . $class . " not supported", MIDCOM_LOG_WARN);
