a7db4e6e195a564ce1d52fd70f150d6922b0c8eb
flack <flack@contentcontrol-berlin.de>
2021-06-02 11:47:23 +0200

    More robustness against incomplete records

--- lib/org/openpsa/documents/style/show-search-results-item.php
+++ lib/org/openpsa/documents/style/show-search-results-item.php
@@ -1,26 +1,21 @@
 <?php
 $view = $data['document_dm'];
-$att = $data['document_attachment'];
-$stat = $att->stat();
-
-$document_type = midcom_helper_misc::filesize_to_string($stat[7]) . ' ' . org_openpsa_helpers_fileinfo::render_type($att->mimetype);
+$icon = $document_type = '';
+if ($att = $data['document_attachment']) {
+    $stat = $att->stat();
+    $document_type = midcom_helper_misc::filesize_to_string($stat[7]) . ' ' . org_openpsa_helpers_fileinfo::render_type($att->mimetype);
+    $icon = midcom_helper_misc::get_mime_icon($att->mimetype);
+}
 $link = $data['router']->generate('document-view', ['guid' => $data['document']->guid]);
 $score = round($data['document_search']->score * 100);
 
 $url = $data['document_search']->topic_url . 'document/' . $data['document']->guid . '/';
-
-// MIME type
-$icon = midcom_helper_misc::get_mime_icon($att->mimetype);
 ?>
 <dt><a href="&(url);"><?php echo $view['title']; ?></a></dt>
 <dd>
 <?php if ($icon) { ?>
-    <div class="icon"><a style="text-decoration: none;" href="&(link);"><img src="&(icon);" <?php
-        if ($view['document']) {
-            echo 'title="' . $document_type . '" ';
-        } ?>style="border: 0;"/></a></div>
-    <?php
-} ?>
+    <div class="icon"><a href="&(link);"><img src="&(icon);" title="&(document_type);" /></a></div>
+<?php } ?>
 
 <ul>
     <li><?php printf($data['l10n']->get('score: %d%%'), $score); ?></li>
@@ -29,7 +24,7 @@ $icon = midcom_helper_misc::get_mime_icon($att->mimetype);
     }
 
     if ($data['document_search']->abstract) {
-        echo "<li>" . $data['document_search']->abstract . "</li>\n";
+        echo "<li>" . $data['document_search']->abstract . "</li>";
     }
     ?>
 </ul>
