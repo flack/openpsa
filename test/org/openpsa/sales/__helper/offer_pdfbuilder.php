42d0c7abf5a1c769f040dc7407e52f2c5fb47ded
flack <flack@contentcontrol-berlin.de>
2021-06-12 14:45:24 +0200

    Add basic smoketests

---
+++ test/org/openpsa/sales/__helper/offer_pdfbuilder.php
@@ -0,0 +1,12 @@
+<?php
+
+class test_offer_pdfbuilder implements org_openpsa_sales_interfaces_pdfbuilder
+{
+    public function __construct(org_openpsa_sales_salesproject_offer_dba $offer)
+    {
+    }
+
+    public function render(string $output_filename)
+    {
+    }
+}
---
+++ test/org/openpsa/sales/handler/offerTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package openpsa.test
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+/**
+ * OpenPSA testcase
+ *
+ * @package openpsa.test
+ */
+class org_openpsa_sales_handler_offerTest extends openpsa_testcase
+{
+    protected static $_person;
+    protected static $_salesproject;
+
+    public static function setUpBeforeClass() : void
+    {
+        require_once dirname(__DIR__) . '/__helper/offer_pdfbuilder.php';
+        self::$_person = self::create_user(true);
+        self::$_salesproject = self::create_class_object(org_openpsa_sales_salesproject_dba::class, ['customerContact' => self::$_person->id]);
+    }
+
+    public function testHandler_create()
+    {
+        $this->set_config('org.openpsa.sales', 'sales_pdfbuilder_class', test_offer_pdfbuilder::class);
+
+        $data = $this->run_handler('org.openpsa.sales', ['salesproject', 'offer', self::$_salesproject->guid]);
+        $this->assertEquals('create_offer', $data['handler_id']);
+    }
+
+    public function testHandler_edit()
+    {
+        $this->set_config('org.openpsa.sales', 'sales_pdfbuilder_class', test_offer_pdfbuilder::class);
+
+        $offer = $this->create_object(org_openpsa_sales_salesproject_offer_dba::class, [
+            'salesproject' => self::$_salesproject->id
+        ]);
+
+        $data = $this->run_handler('org.openpsa.sales', ['salesproject', 'offer', 'edit', $offer->guid]);
+        $this->assertEquals('edit_offer', $data['handler_id']);
+    }
+
+    public function testHandler_delete()
+    {
+        $offer = $this->create_object(org_openpsa_sales_salesproject_offer_dba::class, [
+            'salesproject' => self::$_salesproject->id
+        ]);
+
+        $data = $this->run_handler('org.openpsa.sales', ['salesproject', 'offer', 'delete', $offer->guid]);
+        $this->assertEquals('delete_offer', $data['handler_id']);
+    }
+}
