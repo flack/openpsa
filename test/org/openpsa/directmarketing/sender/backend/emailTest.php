49a2093b9351d4f27e60ae803cadfe3e31a3a9e3
flack <flack@contentcontrol-berlin.de>
2021-08-21 14:43:49 +0200

    Increase coverage

--- test/org/openpsa/directmarketing/sender/backend/emailTest.php
+++ test/org/openpsa/directmarketing/sender/backend/emailTest.php
@@ -10,6 +10,7 @@ namespace test\org\openpsa\directmarketing\sender\backend;
 
 use openpsa_testcase;
 use org_openpsa_directmarketing_campaign_dba;
+use org_openpsa_directmarketing_campaign_message_dba;
 use midcom_db_topic;
 use midcom\datamanager\datamanager;
 
@@ -20,22 +21,54 @@ use midcom\datamanager\datamanager;
  */
 class emailTest extends openpsa_testcase
 {
-    public function test_send()
+    private static $campaign;
+
+    public static function setUpBeforeClass() : void
     {
-        $topic = $this->create_object(midcom_db_topic::class, ['component' => 'org.openpsa.directmarketing']);
-        $campaign = $this->create_object(org_openpsa_directmarketing_campaign_dba::class, [
+        $topic = self::create_class_object(midcom_db_topic::class, [
+            'component' => 'org.openpsa.directmarketing'
+        ]);
+        self::$campaign = self::create_class_object(org_openpsa_directmarketing_campaign_dba::class, [
             'node' => $topic->id
         ]);
-        $message = $this->create_object(\org_openpsa_directmarketing_campaign_message_dba::class, [
-            'campaign' => $campaign->id
+
+    }
+
+    public function test_send()
+    {
+        $message = $this->create_object(org_openpsa_directmarketing_campaign_message_dba::class, [
+            'campaign' => self::$campaign->id
         ]);
 
         $datamanager = datamanager::from_schemadb('file:/org/openpsa/directmarketing/config/schemadb_default_message.inc')
             ->set_storage($message);
 
         $config = [
-            'dm_storage' => $datamanager->get_storage()
+            'dm_storage' => $datamanager->get_storage(),
+            'link_detector_address' => 'https://openpsa2.org/TOKEN'
         ];
+        $this->send($config, $message);
+    }
+
+    public function test_send_html()
+    {
+        $message = $this->create_object(org_openpsa_directmarketing_campaign_message_dba::class, [
+            'campaign' => self::$campaign->id,
+            'orgOpenpsaObtype' => org_openpsa_directmarketing_campaign_message_dba::EMAIL_HTML
+        ]);
+
+        $datamanager = datamanager::from_schemadb('file:/org/openpsa/directmarketing/config/schemadb_default_message.inc')
+            ->set_storage($message);
+
+        $config = [
+            'dm_storage' => $datamanager->get_storage(),
+            'link_detector_address' => 'https://openpsa2.org/TOKEN'
+        ];
+        $this->send($config, $message);
+    }
+
+    private function send(array $config, org_openpsa_directmarketing_campaign_message_dba $message)
+    {
         $sender = new \org_openpsa_directmarketing_sender_backend_email($config, $message);
 
         $person = new \org_openpsa_contacts_person_dba;
@@ -43,7 +76,7 @@ class emailTest extends openpsa_testcase
 
         $member = new \org_openpsa_directmarketing_campaign_member_dba;
         $member->person = $person->id;
-        $member->campaign = $campaign->id;
+        $member->campaign = self::$campaign->id;
 
         $sender->send($person, $member, 'test', 'test', 'test', 'from@openpsa2.org');
 
