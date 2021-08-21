2728db68402142d1dbdb2f13b62559c24ec9a8a8
flack <flack@contentcontrol-berlin.de>
2021-08-21 09:31:51 +0200

    Small rendering improvements

--- lib/org/openpsa/directmarketing/style/show-message-list-item.php
+++ lib/org/openpsa/directmarketing/style/show-message-list-item.php
@@ -3,11 +3,11 @@ $view = $data['message_array'];
 $formatter = $data['l10n']->get_formatter();
 $link = $data['router']->generate('message_view', ['guid' => $data['message']->guid]);
 
-echo "<div class=\"{$data['message_class']}\"><a href=\"{$link}\">{$view['title']}</a>\n";
-echo "<br />" . sprintf($data['l10n']->get('created on %s'), $formatter->datetime($data['message']->metadata->created)) . "\n";
+echo "<div class=\"{$data['message_class']}\"><a href=\"{$link}\">{$view['title']}</a><br />\n";
+printf($data['l10n']->get('created on %s'), $formatter->datetime($data['message']->metadata->created));
 
 if ($data['message']->sendStarted) {
-    echo ", " . sprintf($data['l10n']->get('sent on %s'), $formatter->datetime($data['message']->sendStarted)) . "\n";
+    echo "<br />\n" . sprintf($data['l10n']->get('sent on %s'), $formatter->datetime($data['message']->sendStarted));
 }
 
 echo "</div>\n";
