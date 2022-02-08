50f958ff8c7f4edb3cff2e33dd75989ac3f397af
pardoCCB <98949718+pardoCCB@users.noreply.github.com>
2022-02-08 14:13:28 +0100

    Add ui message to welcome email (#213)
    Add ui message to welcome email

--- lib/org/openpsa/user/handler/person/account.php
+++ lib/org/openpsa/user/handler/person/account.php
@@ -83,7 +83,11 @@ class org_openpsa_user_handler_person_account extends midcom_baseclasses_compone
         $person = new midcom_db_person($guid);
         $accounthelper = new org_openpsa_user_accounthelper($person);
         $accounthelper->welcome_email();
-
+        if ($accounthelper->welcome_email()) {
+            midcom::get()->uimessages->add($this->_l10n->get($this->_component), sprintf($this->_l10n->get('password reset and mail to %s sent'), $person->email ));
+        } else {
+            midcom::get()->uimessages->add($this->_l10n->get($this->_component), $accounthelper->errstr, 'error'); 
+        }
         return new midcom_response_relocate($this->router->generate('user_view', ['guid' => $guid]));
     }
 
