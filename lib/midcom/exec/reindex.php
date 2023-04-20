b67c6aa9441c79639300454f99578347189d5eb3
flack <flack@contentcontrol-berlin.de>
2023-04-20 12:06:21 +0200

    Convert reindex script to js for easier authentication & less timeout issues

--- lib/midcom/exec/reindex.php
+++ lib/midcom/exec/reindex.php
@@ -1,8 +1,4 @@
 <?php
-use GuzzleHttp\Client;
-use Psr\Http\Message\ResponseInterface;
-use GuzzleHttp\Exception\TransferException;
-
 /**
  * Reindex script.
  *
@@ -20,65 +16,53 @@ if (midcom::get()->config->get('indexer_backend') === false) {
     throw new midcom_error('No indexer backend has been defined. Aborting.');
 }
 
-$ip_sudo = midcom::get()->auth->require_admin_or_ip('midcom.services.indexer');
-
-//check if language is passed - if not take the current-one
-$language = midcom::get()->i18n->get_current_language();
-if (isset($_REQUEST['language'])) {
-    $language = $_REQUEST['language'];
-}
-
-debug_add('Disabling script abort through client.');
-ignore_user_abort(true);
-ob_implicit_flush();
-midcom::get()->disable_limits();
-$start = microtime(true);
-
 $nap = new midcom_helper_nav();
-$nodes = [];
-
-// Use this to check that indexer is online (and hope the root topic isn't a gigantic wiki)
-$root_node = $nap->get_node($nap->get_root_node());
-midcom::get()->indexer->query("__TOPIC_GUID:{$root_node[MIDCOM_NAV_OBJECT]->guid}");
-$node = $root_node;
-
-echo "<pre>\n";
-
-$reindex_topic_uri = midcom::get()->get_page_prefix() . 'midcom-exec-midcom/reindex_singlenode.php';
-
-$options = ['timeout' => 300];
-if (   !empty($_SERVER['PHP_AUTH_USER'])
-    && !empty($_SERVER['PHP_AUTH_PW'])) {
-    $options['auth'] = ['user' => $_SERVER['PHP_AUTH_USER'], 'password' => $_SERVER['PHP_AUTH_PW']];
-}
-$client = new Client($options);
+$node = $nap->get_node($nap->get_root_node());
+$nodes = $nodedata = [];
 
 while ($node !== null) {
-    // Reindex the node...
-    echo "Processing node #{$node[MIDCOM_NAV_ID]}, {$node[MIDCOM_NAV_FULLURL]}: ";
-    $uri = $reindex_topic_uri . "?" . http_build_query(['nodeid' => $node[MIDCOM_NAV_ID], 'language' => $language]);
-    $client->getAsync($uri)
-        ->then(function(ResponseInterface $response) use ($uri) {
-            if (!preg_match("#(\n|\r\n)Reindex complete for node http.*\s*</pre>\s*$#", $response->getBody())) {
-                echo "failure.\n   Background reindex returned unexpected data:\n---\n{$response->getBody()}\n---\n";
-                echo "Url: " . $uri . "\n\n";
-            } else {
-                echo "OK.\n";
-            }
-        }, function (TransferException $e) use ($uri) {
-            echo "failure.\n   Background processing failed, error: {$e->getMessage()}\n";
-            echo "Url: " . $uri . "\n";
-        })->wait();
-
+    $nodedata[$node[MIDCOM_NAV_ID]] = $node[MIDCOM_NAV_FULLURL];
     // Retrieve all child nodes and append them to $nodes:
     $nodes = array_merge($nodes, $nap->get_nodes($node[MIDCOM_NAV_ID]));
     $node = array_shift($nodes);
 }
-
-if ($ip_sudo) {
-    midcom::get()->auth->drop_sudo();
-}
 ?>
+<pre id="output"></pre>
+<script>
 
-Reindex complete. Time elapsed: <?php echo round(microtime(true) - $start, 2) . 's'; ?>
-</pre>
+    async function process_nodes() {
+        let start = new Date(),
+            endpoint = '<?= midcom::get()->get_page_prefix() ?>midcom-exec-midcom/reindex_singlenode.php',
+            language = '<?= $_REQUEST['language'] ?? midcom::get()->i18n->get_current_language() ?>',
+            nodes = <?= json_encode($nodedata)?>,
+            output = document.getElementById('output'),
+            url, response, body;
+
+        for (const [id, node_url] of Object.entries(nodes)) {
+            output.textContent += "Processing node #" + id + ", " + node_url + ": ";
+            url = endpoint + '?nodeid=' + id + '&language=' + language;
+            response = await fetch(url);
+            body = await response.text();
+            let parser = new DOMParser(),
+                parsed = parser.parseFromString(body, 'text/html');
+            if (!response.ok) {
+                output.textContent += "failure.\n   Background processing failed, error: " + parsed.getElementsByTagName('body')[0].innerHTML + "\n";
+                output.textContent += "URL: " + url + "\n";
+            } else {
+                if (!body.match(/(\n|\r\n)Reindex complete for node http.*\s*<\/pre>\s*$/)) {
+                    output.textContent += "failure.\n   Background reindex returned unexpected data:\n---\n" + parsed.getElementsByTagName('body')[0].innerHTML + "\n---\n";
+                    output.textContent += "URL: " + url + "\n";
+                } else {
+                    output.textContent += "OK.\n";
+                }
+            }
+        }
+        let end = new Date(),
+            duration = Math.round((end.getTime() - start.getTime()) / 10) / 100;
+
+        output.textContent += "\nReindex complete. Time elapsed: " + duration + "s";
+    }
+
+    process_nodes();
+
+</script>
