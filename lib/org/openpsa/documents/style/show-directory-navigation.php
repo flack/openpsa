8fb4499ae436100c0f451e77e6b39048a5fa1ffb
flack <flack@contentcontrol-berlin.de>
2024-02-22 13:42:32 +0100

    Remove unneeded style element (which was inaccessible due to an unrelated incompatibility)

--- lib/org/openpsa/documents/style/show-directory-navigation.php
+++ lib/org/openpsa/documents/style/show-directory-navigation.php
@@ -1,8 +1,6 @@
 <div class="area org_openpsa_helper_box">
 <h3><?php echo $data['l10n']->get('folders');?></h3>
 <?php
-midcom_show_style('show-search-form-simple');
-
 $nap = new midcom_helper_nav();
 $current_node = $nap->get_node($nap->get_current_node());
 $url = $current_node[MIDCOM_NAV_RELATIVEURL];
