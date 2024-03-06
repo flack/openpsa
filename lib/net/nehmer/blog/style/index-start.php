7d8adb641d87f3ead0c47dfd517b31718e01f817
duakayra <kahraman@contentcontrol-berlin.de>
2024-03-06 14:17:22 +0100

    remove unnecessary variable

--- lib/net/nehmer/blog/style/index-start.php
+++ lib/net/nehmer/blog/style/index-start.php
@@ -1,7 +1,4 @@
-<?php
-$workflow = new midcom\workflow\datamanager();
-if (array_key_exists('base_ajax_comments_url', $data)) {
-?>
+<?php if (array_key_exists('base_ajax_comments_url', $data)) { ?>
 <script type="text/javascript" charset="utf-8">
 var base_ajax_comments_url = MIDCOM_PAGE_PREFIX + '<?php echo $data['base_ajax_comments_url']; ?>';
 function showAjaxComments(element, guid)
