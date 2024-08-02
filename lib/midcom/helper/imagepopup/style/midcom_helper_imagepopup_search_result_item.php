3c403d0ca909a046d0890dac822e8c4109e95d11
flack <flack@contentcontrol-berlin.de>
2024-08-02 11:21:35 +0200

    replace deprecated attribute

--- lib/midcom/helper/imagepopup/style/midcom_helper_imagepopup_search_result_item.php
+++ lib/midcom/helper/imagepopup/style/midcom_helper_imagepopup_search_result_item.php
@@ -13,7 +13,7 @@ if (!in_array($item->mimetype, ['image/x-png', 'image/png', 'image/jpeg', 'image
 if ($item_type == "image") {
     ?>
     <a href='&(url);'>
-        <img src='&(url);' height='54' align='texttop' />
+        <img src='&(url);' height='54' />
     </a>
     <a href='&(url);'>
         <span title="name">&(item.name);</span>
@@ -22,7 +22,7 @@ if ($item_type == "image") {
 } else {
     $mime_icon = midcom_helper_misc::get_mime_icon($item->mimetype);
     ?>
-    <img src="&(mime_icon);" alt="&(item.mimetype);" align='texttop'/>
+    <img src="&(mime_icon);" alt="&(item.mimetype);" />
     <a href='&(url);'>
         <span title="name">&(item.name);</span>
     </a>
